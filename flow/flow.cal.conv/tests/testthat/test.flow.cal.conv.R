b398f088dba0992d9e540e894d210c58a4b3d479
visalakshi chundru <vchundru@battelleecology.org>
2020-01-22 15:45:12 -0700

    fixing the path

--- flow/flow.cal.conv/tests/testthat/test.flow.cal.conv.R
+++ flow/flow.cal.conv/tests/testthat/test.flow.cal.conv.R
@@ -4,13 +4,13 @@ test_that(
   "Calibration Conversion files first test",
   {
       DirEXTENSION <- "prt/2019/01/01"
-    DirIn <- file.path("C:/Users/vchundru/git/NEONScience/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_input/prt_calibration_filter/pfs", DirEXTENSION)
-    DirOut <- "C:/Users/vchundru/git/NEONScience/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_output/pfs/prt"
-    FileSchmData <- "C:/Users/vchundru/git/NEONScience/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_input/avro_schemas/dp0p/prt_calibrated.avsc"
-    FileSchmQf <- "C:/Users/vchundru/git/NEONScience/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_input/avro_schemas/dp0p/flags_validCal.avsc"
+    DirIn <- file.path("Users/vchundru/git/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_input/prt_calibration_filter/pfs", DirEXTENSION)
+    DirOut <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_output/pfs/prt"
+    FileSchmData <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_input/avro_schemas/dp0p/prt_calibrated.avsc"
+    FileSchmQf <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_input/avro_schemas/dp0p/flags_validCal.avsc"
     Term <- "resistance"
     NumDayExpiMax <- "NA"
-    FileUcrtFdas <- "C:/Users/vchundru/git/NEONScience/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_input/uncertainty_fdas/fdas_calibration_uncertainty_general.json"
+    FileUcrtFdas <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.cal.conv/tests/test_input/uncertainty_fdas/fdas_calibration_uncertainty_general.json"
     filter_calibration_conversion(DirIn,DirOut, FileSchmData, FileSchmQf, Term, NumDayExpiMax, TermUcrtFdas="resistance(R)", FileUcrtFdas=FileUcrtFdas)
     expect_that(file.exists(file.path(DirOut, DirEXTENSION)),is_true())
   }
