bd4d8764818f0c614e49c41f40a52cbbed44dd84
Robert Markel <rmarkel@battelleecology.org>
2020-01-29 15:03:39 -0700

    Updates to allow multiple directory inputs to directory filter and added water quality data sources.

--- modules/directory_filter/app.py
+++ modules/directory_filter/app.py
@@ -8,28 +8,44 @@ import lib.file_linker as file_linker
 import lib.target_path as target_path
 
 
-def filter_directory(in_path, filter_dir, out_path):
+def filter_directory(in_path, filter_dirs, out_path):
     """
     Link the target directory into the output directory.
     """
+    parsed_dirs = parse_dirs(filter_dirs)
     for r, d, f in os.walk(in_path):
         for name in d:
-            if not name.startswith('.') and name == filter_dir:
+            if not name.startswith('.') and name in parsed_dirs:
                 source = os.path.join(r, name)
                 destination = target_path.get_path(source, out_path)
                 file_linker.link(source, destination)
 
 
+def parse_dirs(filter_dirs):
+    """
+    Place filter directories into a List.
+    :param filter_dirs:
+    :return:
+    """
+    dirs = []
+    if ',' in filter_dirs:
+        dirs = filter_dirs.split(',')
+        return dirs
+    else:
+        dirs.append(filter_dirs)
+        return dirs
+
+
 def main():
     env = environs.Env()
     in_path = env('IN_PATH')
-    filter_dir = env('FILTER_DIR')
+    filter_dirs = env('FILTER_DIR')
     out_path = env('OUT_PATH')
     log_level = env('LOG_LEVEL')
     log_config.configure(log_level)
     log = structlog.get_logger()
-    log.debug(f'in_path: {in_path} sub_dir: {filter_dir} out_dir: {out_path}')
-    filter_directory(in_path, filter_dir, out_path)
+    log.debug(f'in_path: {in_path} filter_dirs: {filter_dirs} out_dir: {out_path}')
+    filter_directory(in_path, filter_dirs, out_path)
 
 
 if __name__ == '__main__':
