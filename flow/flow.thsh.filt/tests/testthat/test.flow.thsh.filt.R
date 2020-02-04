3f899af32bd32b36edd3ca45d6c6f266582e9c72
visalakshi chundru <vchundru@battelleecology.org>
2020-02-04 14:54:59 -0700

    Added test cases for threshold filter

--- flow/flow.thsh.filt/tests/testthat/test.flow.thsh.filt.R
+++ flow/flow.thsh.filt/tests/testthat/test.flow.thsh.filt.R
@@ -1,13 +1,33 @@
 library(testthat)
 
 test_that(
-  "Calibration Conversion files first test",
+   "Threshold Filter test expect 2 thresholds in the output files",
+   {
+     DirIn <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.thsh.filt/tests/test_input/pfs/threshold"
+     DirOut <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.thsh.filt/tests/test_output/pfs/out"
+     Term <- "temp"
+     Ctxt <- "aspirated-single"
+     OutPutFile <- file.path(DirOut, "thresholds.json")
+     filter_threshold(DirIn,DirOut, Term,Ctxt)
+     result <- fromJSON(file = OutPutFile)
+     print(result$thresholds)
+     expect_that(file.exists(file.path(DirOut, DirEXTENSION)),is_true())
+   }
+ )
+
+
+test_that(
+  "Threshold Filter test expect zero thresholds in the input file",
   {
-    DirIn <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.thsh.filt/tests/test_input/pfs/threshold/"
-    DirOut <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.thsh.filt/tests/test_output/pfs/out"
+    DirIn <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.thsh.filt/tests/test_input/pfs/threshold_fail"
+    DirOut <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.thsh.filt/tests/test_output/pfs/threshold_fail"
     Term <- "temp"
     Ctxt <- "aspirated-single"
+    OutPutFile <- file.path(DirOut, "thresholds.json")
     filter_threshold(DirIn,DirOut, Term,Ctxt)
+    result <- fromJSON(file = OutPutFile)
+    result <- base::do.call(base::rbind,result)
+    print(result$thresholds)
     expect_that(file.exists(file.path(DirOut, DirEXTENSION)),is_true())
   }
 )
