e3f2f3fea7cee07737b8f3057e56f9b46bea3d1f
visalakshi chundru <vchundru@battelleecology.org>
2020-02-04 09:30:02 -0700

    new test cases

---
+++ flow/flow.thsh.filt/tests/testthat/test.flow.thsh.filt.R
@@ -0,0 +1,13 @@
+library(testthat)
+
+test_that(
+  "Calibration Conversion files first test",
+  {
+    DirIn <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.thsh.filt/tests/test_input/threshold/"
+    DirOut <- "/Users/vchundru/git/NEON-IS-data-processing/flow/flow.thsh.filt/tests/test_output/pfs/out"
+    Term <- "temp"
+    Ctxt <- "aspirated-single"
+    filter_threshold(DirIn,DirOut, Term,Ctxt)
+    expect_that(file.exists(file.path(DirOut, DirEXTENSION)),is_true())
+  }
+)
