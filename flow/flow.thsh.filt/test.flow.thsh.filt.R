083bab87c6eabd1b7ed0d4ca0726237c05cd8325
visalakshi chundru <vchundru@battelleecology.org>
2020-02-06 10:39:19 -0700

    Added test cases to test threshold filter when only either term or context are passed as the inputs

--- flow/flow.thsh.filt/test.flow.thsh.filt.R
+++ flow/flow.thsh.filt/test.flow.thsh.filt.R
@@ -29,3 +29,31 @@ test_that(
     expect_true(length(result$thresholds) ==0)
   }
 )
+
+test_that(
+  "Threshold Filter test expect a thresholds with only Term and no Context in the input file",
+  {
+    DirIn <- "tests/test_input/pfs/threshold_with_only_term"
+    DirOut <- "tests/test_output/pfs/threshold_with_only_term"
+    Term <- "dewPoint"
+    Ctxt <- "aspirated-single"
+    OutPutFile <- file.path(DirOut, "thresholds.json")
+    filter_threshold(DirIn, DirOut, Term=Term)
+    result <- rjson::fromJSON(file = OutPutFile,simplify=TRUE)
+    expect_true(length(result$thresholds) ==3)
+  }
+)
+
+test_that(
+  "Threshold Filter test expect a thresholds with only context and no term in the input file",
+  {
+    DirIn <- "tests/test_input/pfs/threshold_with_only_context"
+    DirOut <- "tests/test_output/pfs/threshold_with_only_context"
+  # Term <- "temp"
+    Ctxt <- "aspirated-single"
+    OutPutFile <- file.path(DirOut, "thresholds.json")
+    filter_threshold(DirIn,DirOut,Ctxt = Ctxt)
+    result <- rjson::fromJSON(file = OutPutFile,simplify=TRUE)
+    expect_true(length(result$thresholds) ==2)
+  }
+)
