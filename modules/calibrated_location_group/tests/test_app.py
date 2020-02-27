a104b78afd3269c4934dbaf7e263a61c1380bf28
Robert Markel <rmarkel@battelleecology.org>
2020-02-27 16:52:40 -0700

    Updates.

--- modules/calibrated_location_group/tests/test_app.py
+++ modules/calibrated_location_group/tests/test_app.py
@@ -23,11 +23,13 @@ class AppTest(TestCase):
         calibrated_root = os.path.join(self.calibrated_path, self.metadata_path)
         data_path = os.path.join(calibrated_root, 'data', 'data.avro')
         flags_path = os.path.join(calibrated_root, 'flags', 'flags.avro')
-        uncertainty_coef_path = os.path.join(calibrated_root, 'uncertainty_coef', 'uncertaintyCoef.json')
+        uncertainty_path = os.path.join(calibrated_root, 'uncertainty', 'uncertainty.json')
+        test_extra_dir_path = os.path.join(calibrated_root, 'test', 'test_dir', 'test.json')
 
         self.fs.create_file(data_path)
         self.fs.create_file(flags_path)
-        self.fs.create_file(uncertainty_coef_path)
+        self.fs.create_file(uncertainty_path)
+        self.fs.create_file(test_extra_dir_path)
 
         #  Create location input file.
         locations_path = os.path.join(self.location_path, 'prt', '00001', 'locations.json')
@@ -54,9 +56,11 @@ class AppTest(TestCase):
         data_path = os.path.join(root_path, 'data', 'data.avro')
         flags_path = os.path.join(root_path, 'flags', 'flags.avro')
         locations_path = os.path.join(root_path, 'location', 'locations.json')
-        uncertainty_coef_path = os.path.join(root_path, 'uncertainty_coef', 'uncertaintyCoef.json')
+        uncertainty_path = os.path.join(root_path, 'uncertainty', 'uncertainty.json')
+        test_extra_dir_path = os.path.join(root_path, 'test', 'test_dir', 'test.json')
 
         self.assertTrue(os.path.lexists(data_path))
         self.assertTrue(os.path.lexists(flags_path))
         self.assertTrue(os.path.lexists(locations_path))
-        self.assertTrue(os.path.lexists(uncertainty_coef_path))
+        self.assertTrue(os.path.lexists(uncertainty_path))
+        self.assertTrue(os.path.lexists(test_extra_dir_path))
