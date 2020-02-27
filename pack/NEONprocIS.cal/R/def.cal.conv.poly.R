ea86cb6512631fa9a980daa993c4acae504ca361
Mija Choi <choim@battelleecology.org>
2020-02-27 09:55:43 -0700

    changing error to warn

--- pack/NEONprocIS.cal/R/def.cal.conv.poly.R
+++ pack/NEONprocIS.cal/R/def.cal.conv.poly.R
@@ -69,7 +69,7 @@ def.cal.conv.poly <- function(data = base::numeric(0),
   
   # If infoCal is NULL, return NA data
   if ((is.null(infoCal)) || any (is.na(unlist((infoCal))))) {
-    log$error('No calibration information supplied, returning NA values for converted data.')
+    log$warn('No calibration information supplied, returning NA values for converted data.')
     dataConv <- NA * data
     return(dataConv)
   }
