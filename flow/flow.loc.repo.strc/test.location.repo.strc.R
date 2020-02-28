eab7c548bfc111cc5d61d089e3f47b15832c3ad3
visalakshi chundru <vchundru@battelleecology.org>
2020-02-28 10:16:21 -0700

    more tests

--- flow/flow.loc.repo.strc/test.location.repo.strc.R
+++ flow/flow.loc.repo.strc/test.location.repo.strc.R
@@ -33,8 +33,40 @@ source("location_repo_strc.R")
 # )
 
 
+test_that(
+  "location repo strc where there are more than one locations",
+  {
+    DirEXTENSION <- "2019/01/04"
+    DirIn <- file.path("tests/test_input/pfs/prt", DirEXTENSION)
+    DirOut <- "tests/test_output/pfs/out"
+    DataFilePath <- file.path(DirOut,"2019/01/04/CFGLOC108440/data/prt_19963_2019-01-01.avro" )
+    FlagsFilePath <- file.path(DirOut,"2019/01/04/CFGLOC108440/flags/prt_19963_2019-01-01_flagsCal.avro" )
+    LocationFilePath <- file.path(DirOut,"2019/01/04/CFGLOC108440/location/prt_19963_locations.json" )
+    UncertaintyCoefFilePath <- file.path(DirOut,"2019/01/04/CFGLOC108440/uncertainty_coef/prt_19963_2019-01-01_uncertaintyCoef.json" )
+    UnCertaintyDataFilePath <- file.path(DirOut,"2019/01/04/CFGLOC108440/uncertainty_data/prt_19963_2019-01-01_uncertaintyData.avro" )
+
+    DataFilePath1 <- file.path(DirOut,"2019/01/04/CFGLOC108441/data/prt_19963_2019-01-01.avro" )
+    FlagsFilePath1 <- file.path(DirOut,"2019/01/04/CFGLOC108441/flags/prt_19963_2019-01-01_flagsCal.avro" )
+    LocationFilePath1 <- file.path(DirOut,"2019/01/04/CFGLOC108441/location/prt_19963_locations.json" )
+    UncertaintyCoefFilePath1 <- file.path(DirOut,"2019/01/04/CFGLOC108441/uncertainty_coef/prt_19963_2019-01-01_uncertaintyCoef.json" )
+    UnCertaintyDataFilePath1 <- file.path(DirOut,"2019/01/04/CFGLOC108441/uncertainty_data/prt_19963_2019-01-01_uncertaintyData.avro" )
+    restructure_location_repo(DirIn, DirOut, Comb=TRUE)
+    expect_true(file.exists(DataFilePath))
+    expect_true(file.exists(FlagsFilePath))
+    expect_true(file.exists(LocationFilePath))
+    expect_true(file.exists(UncertaintyCoefFilePath))
+    expect_true(file.exists(UnCertaintyDataFilePath))
+
+    expect_true(file.exists(DataFilePath1))
+    expect_true(file.exists(FlagsFilePath1))
+    expect_true(file.exists(LocationFilePath1))
+    expect_true(file.exists(UncertaintyCoefFilePath1))
+    expect_true(file.exists(UnCertaintyDataFilePath1))
+  }
+)
+
 # test_that(
-#   "location repo strc where there are more than one locations",
+#   "location repo strc when Comb variable is FALSE",
 #   {
 #     DirEXTENSION <- "2019/01/03"
 #     DirIn <- file.path("tests/test_input/pfs/prt", DirEXTENSION)
@@ -44,45 +76,13 @@ source("location_repo_strc.R")
 #     LocationFilePath <- file.path(DirOut,"2019/01/03/CFGLOC108440/location/prt_19963_locations.json" )
 #     UncertaintyCoefFilePath <- file.path(DirOut,"2019/01/03/CFGLOC108440/uncertainty_coef/prt_19963_2019-01-01_uncertaintyCoef.json" )
 #     UnCertaintyDataFilePath <- file.path(DirOut,"2019/01/03/CFGLOC108440/uncertainty_data/prt_19963_2019-01-01_uncertaintyData.avro" )
-#     
-#     DataFilePath1 <- file.path(DirOut,"2019/01/03/CFGLOC108441/data/prt_19963_2019-01-01.avro" )
-#     FlagsFilePath1 <- file.path(DirOut,"2019/01/03/CFGLOC108441/flags/prt_19963_2019-01-01_flagsCal.avro" )
-#     LocationFilePath1 <- file.path(DirOut,"2019/01/03/CFGLOC108441/location/prt_19963_locations.json" )
-#     UncertaintyCoefFilePath1 <- file.path(DirOut,"2019/01/03/CFGLOC108441/uncertainty_coef/prt_19963_2019-01-01_uncertaintyCoef.json" )
-#     UnCertaintyDataFilePath1 <- file.path(DirOut,"2019/01/03/CFGLOC108441/uncertainty_data/prt_19963_2019-01-01_uncertaintyData.avro" )
-#     restructure_location_repo(DirIn, DirOut, Comb=TRUE)
-#     expect_true(file.exists(DataFilePath))
-#     expect_true(file.exists(FlagsFilePath))
-#     expect_true(file.exists(LocationFilePath))
-#     expect_true(file.exists(UncertaintyCoefFilePath))
-#     expect_true(file.exists(UnCertaintyDataFilePath))
-#     
-#     expect_true(file.exists(DataFilePath1))
-#     expect_true(file.exists(FlagsFilePath1))
-#     expect_true(file.exists(LocationFilePath1))
-#     expect_true(file.exists(UncertaintyCoefFilePath1))
-#     expect_true(file.exists(UnCertaintyDataFilePath1))
+#     restructure_location_repo(DirIn, DirOut, Comb=FALSE)
+#     # expect_true(file.exists(DataFilePath))
+#     # expect_true(file.exists(FlagsFilePath))
+#     # expect_true(file.exists(LocationFilePath))
+#     # expect_true(file.exists(UncertaintyCoefFilePath))
+#     # expect_true(file.exists(UnCertaintyDataFilePath))
 #   }
 # )
-
-test_that(
-  "location repo strc when Comb variable is FALSE",
-  {
-    DirEXTENSION <- "2019/01/03"
-    DirIn <- file.path("tests/test_input/pfs/prt", DirEXTENSION)
-    DirOut <- "tests/test_output/pfs/out"
-    # DataFilePath <- file.path(DirOut,"2019/01/03/CFGLOC108440/data/prt_19963_2019-01-01.avro" )
-    # FlagsFilePath <- file.path(DirOut,"2019/01/03/CFGLOC108440/flags/prt_19963_2019-01-01_flagsCal.avro" )
-    # LocationFilePath <- file.path(DirOut,"2019/01/03/CFGLOC108440/location/prt_19963_locations.json" )
-    # UncertaintyCoefFilePath <- file.path(DirOut,"2019/01/03/CFGLOC108440/uncertainty_coef/prt_19963_2019-01-01_uncertaintyCoef.json" )
-    # UnCertaintyDataFilePath <- file.path(DirOut,"2019/01/03/CFGLOC108440/uncertainty_data/prt_19963_2019-01-01_uncertaintyData.avro" )
-    restructure_location_repo(DirIn, DirOut, Comb=FALSE)
-    # expect_true(file.exists(DataFilePath))
-    # expect_true(file.exists(FlagsFilePath))
-    # expect_true(file.exists(LocationFilePath))
-    # expect_true(file.exists(UncertaintyCoefFilePath))
-    # expect_true(file.exists(UnCertaintyDataFilePath))
-  }
-)
 
 
