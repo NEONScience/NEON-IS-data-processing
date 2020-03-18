a4f7519afb6f9051da160e1591380f495dcf2895
Robert Markel <rmarkel@battelleecology.org>
2020-03-18 15:24:59 -0600

    Update to add start and end dates to data gap filler.

--- modules/data_gap_filler/app.py
+++ modules/data_gap_filler/app.py
@@ -1,95 +1,34 @@
 import os
+from datetime import datetime
 
 import environs
 import structlog
 import pathlib
 
 import lib.log_config as log_config
-import lib.file_linker as file_linker
 import lib.file_crawler as file_crawler
+
+from data_gap_filler import gap_filler as gap_filler
 
 log = structlog.get_logger()
 
 
-def get_data_files(data_path, out_path):
-    keys = []
-    for file_path in file_crawler.crawl(data_path):
-        parts = file_path.parts
-        source_type = parts[3]
-        year = parts[4]
-        month = parts[5]
-        day = parts[6]
-        location_name = parts[7]
-        data_type = parts[8]
-        filename = parts[9]
-        target_root = os.path.join(out_path, source_type, year, month, day, location_name)
-        target_path = os.path.join(target_root, data_type, filename)
-        file_linker.link(file_path, target_path)
-        key = '/' + source_type + '/' + year + '/' + month + '/' + day + '/' + location_name
-        keys.append(key)
-    return keys
+def get_date_constraints():
+    date_format = '%Y-%m-%d'
+    try:
+        start_date = os.environ['START_DATE']
+        end_date = os.environ['END_DATE']
+    except KeyError:
+        print(f'Proceeding without start and end dates.')
+        return None
+    if start_date is not None:
+        start_date = datetime.strptime(start_date, date_format)
+    if end_date is not None:
+        end_date = datetime.strptime(end_date, date_format)
+    return {'start_date': start_date, 'end_date': end_date}
 
 
-def process_location_files(location_path, keys, out_path, output_directories,
-                           empty_data_path, empty_flags_path, empty_uncertainty_data_path):
-    for file_path in file_crawler.crawl(location_path):
-        parts = file_path.parts
-        source_type = parts[3]
-        year = parts[4]
-        month = parts[5]
-        day = parts[6]
-        named_location_name = parts[7]
-        log.debug(f'named location name: {named_location_name}')
-        filename = parts[8]
-        target_root = os.path.join(out_path, source_type, year, month, day, named_location_name)
-        # link the location file into the output directory
-        location_target = os.path.join(target_root, 'location', filename)
-        file_linker.link(file_path, location_target)
-        # create an empty calibration file in the target directory but do not overwrite
-        calibration_target = os.path.join(target_root, 'calibration')
-        os.makedirs(calibration_target, exist_ok=True)
-        # create key to find corresponding data for the sensor and date
-        key = '/' + source_type + '/' + year + '/' + month + '/' + day + '/' + named_location_name
-        if key not in keys:
-            # key not found, create empty directories and files
-            print(f'Key not found {key}')
-            for directory in output_directories:
-                target_dir = os.path.join(target_root, directory)
-                if directory == 'data':
-                    link_path(target_dir, empty_data_path, named_location_name, year, month, day)
-                elif directory == 'flags':
-                    link_path(target_dir, empty_flags_path, named_location_name, year, month, day)
-                elif directory == 'uncertainty_data':
-                    link_path(target_dir, empty_uncertainty_data_path, named_location_name, year, month, day)
-                elif directory == 'uncertainty_coef':
-                    os.makedirs(target_dir, exist_ok=True)
-
-
-def link_path(target_dir, empty_file_path, location_name, year, month, day):
-    file_name = pathlib.Path(empty_file_path).name
-    file_name = file_name.replace('location', location_name)
-    file_name = file_name.replace('year', year)
-    file_name = file_name.replace('month', month)
-    file_name = file_name.replace('day', day)
-    target_path = os.path.join(target_dir, file_name)
-    print(f'target_path: {target_path}')
-    file_linker.link(empty_file_path, target_path)
-
-
-def main():
-    env = environs.Env()
-    data_path = env('DATA_PATH')
-    location_path = env('LOCATION_PATH')
-    empty_files_path = env('EMPTY_FILES_PATH')
-    output_directories = env('OUTPUT_DIRECTORIES')
-    out_path = env('OUT_PATH')
-    log_level = env('LOG_LEVEL')
-    log_config.configure(log_level)
-
-    # Directory names to output should be a comma separated string.
-    if ',' in output_directories:
-        output_directories = output_directories.split(',')
-
+def get_empty_file_paths(empty_files_path):
     empty_data_path = None
     empty_flags_path = None
     empty_uncertainty_data_path = None
@@ -103,7 +42,6 @@ def main():
             empty_flags_path = file_path
         elif 'uncertainty_data' == directory_name:
             empty_uncertainty_data_path = file_path
-
     if empty_data_path is None:
         log.error('Empty data file not found.')
         exit(1)
@@ -113,10 +51,43 @@ def main():
     if empty_uncertainty_data_path is None:
         log.error('Empty uncertainty data file not found.')
         exit(1)
+    return {'empty_data_path': empty_data_path,
+            'empty_flags_path': empty_flags_path,
+            'empty_uncertainty_data_path': empty_uncertainty_data_path}
 
-    keys = get_data_files(data_path, out_path)
-    process_location_files(location_path, keys, out_path, output_directories,
-                           empty_data_path, empty_flags_path, empty_uncertainty_data_path)
+
+def main():
+    env = environs.Env()
+    data_path = env('DATA_PATH')
+    location_path = env('LOCATION_PATH')
+    empty_files_path = env('EMPTY_FILES_PATH')
+    output_directories = env('OUTPUT_DIRECTORIES')
+    out_path = env('OUT_PATH')
+    log_level = env('LOG_LEVEL')
+    log_config.configure(log_level)
+
+    # directory names to output should be a comma separated string.
+    if ',' in output_directories:
+        output_directories = output_directories.split(',')
+
+    # empty file paths
+    empty_files_paths = get_empty_file_paths(empty_files_path)
+    empty_data_path = empty_files_paths.get('empty_data_path')
+    empty_flags_path = empty_files_paths.get('empty_flags_path')
+    empty_uncertainty_data_path = empty_files_paths.get('empty_uncertainty_data_path')
+
+    date_constraints = get_date_constraints()
+    if date_constraints is not None:
+        start_date = date_constraints.get('start_date')
+        end_date = date_constraints.get('end_date')
+        keys = gap_filler.get_data_files(data_path, out_path, start_date=start_date, end_date=end_date)
+        gap_filler.process_location_files(location_path, keys, out_path, output_directories,
+                                          empty_data_path, empty_flags_path, empty_uncertainty_data_path,
+                                          start_date=start_date, end_date=end_date)
+    else:
+        keys = gap_filler.get_data_files(data_path, out_path)
+        gap_filler.process_location_files(location_path, keys, out_path, output_directories,
+                                          empty_data_path, empty_flags_path, empty_uncertainty_data_path)
 
 
 if __name__ == '__main__':
