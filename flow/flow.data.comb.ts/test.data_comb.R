f3af02fcf0d38656e41859e9e4c4502a815684dd
vchundru <vchundru@battelleecology.org>
2020-04-01 11:18:18 -0600

    bug fixes

--- flow/flow.data.comb.ts/test.data_comb.R
+++ flow/flow.data.comb.ts/test.data_comb.R
@@ -4,13 +4,13 @@ test_that(
 
    "Data Comb ts tests",
    {
-     DirIn <- "tests/test_input/pfs/waterQuality_exoconductivity_data_uncertainty_group"
+     DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group"
      DirOut <- "tests/test_output/pfs/out"
      FileSchmComb <- "tests/test_input/avro_schemas/dp01/waterQuality_exoconductivity_dp01_stats.avsc" 
-     DirComb <- "data|uncertainty_data"
+     DirComb <- c("data", "uncertainty_data")
      NameDirCombOut <- "stats"
      NameVarTime <- "readout_time"
-     ColKeep <- "readout_time|readout_time|specificConductance|specificConductance_ucrtExpn"
+     ColKeep <- c("readout_time", "readout_time", "specificConductance", "specificConductance_ucrtExpn")
      NameFileSufx <- "_basicStats_100"
      
      data_combine(DirIn = DirIn, DirOut = DirOut, FileSchmComb = FileSchmComb, DirComb = DirComb, NameDirCombOut = NameDirCombOut, NameVarTime = NameVarTime,
