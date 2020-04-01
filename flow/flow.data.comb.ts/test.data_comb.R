701bca7c94c930e2f3c1b10388b87f28b63f1ac2
vchundru <vchundru@battelleecology.org>
2020-04-01 14:08:49 -0600

    added 2nd test case

--- flow/flow.data.comb.ts/test.data_comb.R
+++ flow/flow.data.comb.ts/test.data_comb.R
@@ -2,7 +2,7 @@ library(testthat)
 source("data_comb.R")
 test_that(    
 
-   "Data Comb ts tests",
+   "Data Comb creates combined file",
    {
    
      DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group"
@@ -18,6 +18,27 @@ test_that(
        ColKeep = ColKeep, NameFileSufx = NameFileSufx)
      expect_that(file.exists(outputFile),is_true())
     
+   }
+)
+
+test_that(    
+   
+   "Data Comb creates combined file with shortest name",
+   {
+      
+      DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group/shortest"
+      DirOut <- "tests/test_output/pfs/out"
+      FileSchmComb <- "tests/test_input/avro_schemas/dp01/waterQuality_exoconductivity_dp01_stats.avsc" 
+      DirComb <- c("data", "uncertainty_data")
+      NameDirCombOut <- "stats"
+      NameVarTime <- "readout_time"
+      ColKeep <- c("readout_time", "readout_time", "specificConductance", "specificConductance_ucrtExpn")
+      NameFileSufx <- "_basicStats_100"
+      outputFile <-  file.path(DirOut,"01/02/water-quality-283/exoconductivity/CFGLOC110700/stats/316_uncert.avro_basicStats_100.avro" )
+      data_combine(DirIn = DirIn, DirOut = DirOut, FileSchmComb = FileSchmComb, DirComb = DirComb, NameDirCombOut = NameDirCombOut, NameVarTime = NameVarTime,
+                   ColKeep = ColKeep, NameFileSufx = NameFileSufx)
+      expect_that(file.exists(outputFile),is_true())
+      
    }
 )
 
