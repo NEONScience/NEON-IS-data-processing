878be7911f93bc17c065d294b189e78dbd02a4bf
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-04-01 13:53:14 -0600

    hard coded the output folder

--- flow/flow.data.comb.ts/test.data_comb.R
+++ flow/flow.data.comb.ts/test.data_comb.R
@@ -4,6 +4,7 @@ test_that(
 
    "Data Comb ts tests",
    {
+   
      DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group"
      DirOut <- "tests/test_output/pfs/out"
      FileSchmComb <- "tests/test_input/avro_schemas/dp01/waterQuality_exoconductivity_dp01_stats.avsc" 
@@ -12,9 +13,10 @@ test_that(
      NameVarTime <- "readout_time"
      ColKeep <- c("readout_time", "readout_time", "specificConductance", "specificConductance_ucrtExpn")
      NameFileSufx <- "_basicStats_100"
-     
+     outputFile <-  file.path(DirOut,"01/02/water-quality-283/exoconductivity/CFGLOC110700/stats/SENSOR000316_2019-01-02_basicStats_100.avro" )
      data_combine(DirIn = DirIn, DirOut = DirOut, FileSchmComb = FileSchmComb, DirComb = DirComb, NameDirCombOut = NameDirCombOut, NameVarTime = NameVarTime,
        ColKeep = ColKeep, NameFileSufx = NameFileSufx)
+     expect_that(file.exists(outputFile),is_true())
     
    }
 )
