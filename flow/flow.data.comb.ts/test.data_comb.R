33edc023ce6355d4f4c1be77572611bddd76820d
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-04-02 09:25:07 -0600

    added more test cases

--- flow/flow.data.comb.ts/test.data_comb.R
+++ flow/flow.data.comb.ts/test.data_comb.R
@@ -1,45 +1,71 @@
 library(testthat)
 source("data_comb.R")
-test_that(    
+ 
+  test_that(
 
-   "Data Comb creates combined file",
-   {
-   
-     DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group"
-     DirOut <- "tests/test_output/pfs/out"
-     FileSchmComb <- "tests/test_input/avro_schemas/dp01/waterQuality_exoconductivity_dp01_stats.avsc" 
-     DirComb <- c("data", "uncertainty_data")
-     NameDirCombOut <- "stats"
-     NameVarTime <- "readout_time"
-     ColKeep <- c("readout_time", "readout_time", "specificConductance", "specificConductance_ucrtExpn")
-     NameFileSufx <- "_basicStats_100"
-     outputFile <-  file.path(DirOut,"01/02/water-quality-283/exoconductivity/CFGLOC110700/stats/SENSOR000316_2019-01-02_basicStats_100.avro" )
-     data_combine(DirIn = DirIn, DirOut = DirOut, FileSchmComb = FileSchmComb, DirComb = DirComb, NameDirCombOut = NameDirCombOut, NameVarTime = NameVarTime,
-       ColKeep = ColKeep, NameFileSufx = NameFileSufx)
-     expect_that(file.exists(outputFile),is_true())
-    
-   }
-)
+    "Data Comb creates combined file",
+    {
 
-test_that(    
-   
-   "Data Comb creates combined file with shortest name",
-   {
-      
-      DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group/shortest"
+      DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group"
       DirOut <- "tests/test_output/pfs/out"
-      FileSchmComb <- "tests/test_input/avro_schemas/dp01/waterQuality_exoconductivity_dp01_stats.avsc" 
+      FileSchmComb <- "tests/test_input/avro_schemas/dp01/waterQuality_exoconductivity_dp01_stats.avsc"
       DirComb <- c("data", "uncertainty_data")
       NameDirCombOut <- "stats"
       NameVarTime <- "readout_time"
       ColKeep <- c("readout_time", "readout_time", "specificConductance", "specificConductance_ucrtExpn")
       NameFileSufx <- "_basicStats_100"
-      outputFile <-  file.path(DirOut,"01/02/water-quality-283/exoconductivity/CFGLOC110700/stats/316_uncert.avro_basicStats_100.avro" )
+      DirEXTENSION <- "01/02/water-quality-283/exoconductivity/CFGLOC110700/stats/SENSOR000316_2019-01-02_basicStats_100.avro"
+
       data_combine(DirIn = DirIn, DirOut = DirOut, FileSchmComb = FileSchmComb, DirComb = DirComb, NameDirCombOut = NameDirCombOut, NameVarTime = NameVarTime,
-                   ColKeep = ColKeep, NameFileSufx = NameFileSufx)
-      expect_that(file.exists(outputFile),is_true())
+        ColKeep = ColKeep, NameFileSufx = NameFileSufx)
+      expect_that(file.exists(file.path(DirOut, DirEXTENSION)),is_true())
+
+    }
+  )
+  
+  test_that(
+    
+    "No output directory when there are no datums",
+    {
       
-   }
-)
+      DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group"
+      DirOut <- "tests/test_output/pfs/out"
+      DirComb <- c("data", "uncertainty_data")
+      NameDirCombOut <- "stats"
+      NameVarTime <- "readout_time"
+      ColKeep <- c("readout_time", "readout_time", "specificConductance", "specificConductance_ucrtExpn")
+      NameFileSufx <- "_basicStats_100"
+   
+      
+      data_combine(DirIn = DirIn, DirOut = DirOut, DirComb = DirComb, NameDirCombOut = NameDirCombOut, NameVarTime = NameVarTime,
+                   ColKeep = ColKeep, NameFileSufx = NameFileSufx)
+      expect_that(file.exists(file.path(DirOut)),is_false())
+      
+    }
+  )
+
+ test_that(
+
+    "Data Comb creates combined file with shortest name",
+    {
+
+       DirIn <- "tests/test_input/waterQuality_exoconductivity_data_uncertainty_group/shortest"
+       DirOut <- "tests/test_output/pfs/out"
+       FileSchmComb <- "tests/test_input/avro_schemas/dp01/waterQuality_exoconductivity_dp01_stats.avsc"
+       DirComb <- c("data", "uncertainty_data")
+       NameDirCombOut <- "stats"
+       NameVarTime <- "readout_time"
+       ColKeep <- c("readout_time", "readout_time", "specificConductance", "specificConductance_ucrtExpn")
+       NameFileSufx <- "_basicStats_100"
+       DirEXTENSION <- "/01/02/water-quality-283/exoconductivity/CFGLOC110700/stats/316_uncert_basicStats_100.avro" 
+       
+       data_combine(DirIn = DirIn, DirOut = DirOut, FileSchmComb = FileSchmComb, DirComb = DirComb, NameDirCombOut = NameDirCombOut, NameVarTime = NameVarTime,
+                    ColKeep = ColKeep, NameFileSufx = NameFileSufx)
+       expect_that(file.exists(file.path(DirOut, DirEXTENSION)),is_true())
+       
+
+    }
+ )
+
 
 
