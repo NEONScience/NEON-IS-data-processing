d61aae514ed8ed4a3feb846c816b08276cb0fe84
covesturtevant <csturtevant@neoninc.org>
2020-04-09 13:02:12 -0600

    add compression and dictionary encoding options

--- pack/NEONprocIS.base/R/def.wrte.parq.R
+++ pack/NEONprocIS.base/R/def.wrte.parq.R
@@ -10,7 +10,7 @@
 #' be written as class character.
 
 #' @param data Data frame. Data to write to file.
-#' @param NameFile String. Name (including relative or absolute path) of AVRO file.
+#' @param NameFile String. Name (including relative or absolute path) of output parquet file.
 #' @param Schm Optional. Either a Parquet schema of class ArrowObject, or a Json formatted string 
 #' with an AVRO file schema. Example:\cr
 #' "{\"type\" : \"record\",\"name\" : \"ST\",\"namespace\" : \"org.neonscience.schema.device\",\"fields\" : [ {\"name\" :\"readout_time\",\"type\" : {\"type\" : \"long\",\"logicalType\" : \"timestamp-millis\"},\"doc\" : \"Timestamp of readout expressed in milliseconds since epoch\"}, {\"name\" : \"soilPRTResistance\",\"type\" : [ \"null\", \"float\" ],\"doc\" : \"DPID: DP0.00041.001 TermID: 1728 Units: ohm Description: Soil Temperature, Level 0\",\"default\" : null} ],\"__fastavro_parsed\" : true}"\cr
@@ -18,7 +18,10 @@
 #' (if not NULL) or auto-generated from the data frame.
 #' @param NameFileSchm String. Optional. A filename (include relative or aboslute path) of an avro 
 #' schema file (.avsc format). Defaults to NULL, in which case the schema will be constructed using 
-#' the Schm argument (if not NULL) or auto-generated from the data frame
+#' the Schm argument (if not NULL) or auto-generated from the input data.
+#' @param CompType String. Compression algorithm. Defaults to 'gzip'. Can also be NULL, in which case
+#' the arrow::write_parquet default will be used. 
+#' @param CompLvl Numeric. Compression level. See documentation for arrow::write_parquet for details.
 #' @param log A logger object as produced by NEONprocIS.base::def.log.init to produce structured log
 #' output. Defaults to NULL, in which the logger will be created and used within the function.
 
@@ -33,7 +36,8 @@
 #' data <- data.frame(x=c(1,2,3),y=c('one','two','three'),stringsAsFactors=FALSE)
 #' dataOut <- NEONprocIS.base::def.wrte.parq(data,NameFile='out.parquet')
 
-#' @seealso \link[NEONprocIS.base]{def.read.avro.deve}
+#' @seealso \link[NEONprocIS.base]{def.read.parq}
+#' @seealso \link[arrow]{write_parquet}
 
 #' @export
 
@@ -45,23 +49,38 @@ def.wrte.parq <- function(data,
                           NameFile,
                           Schm=NULL,
                           NameFileSchm=NULL,
+                          CompType='gzip',
+                          CompLvl=5,
+                          Dict=NULL,
                           log=NULL
 ){
   # initialize logging if necessary
   if (base::is.null(log)) {
     log <- NEONprocIS.base::def.log.init()
   }
+  
+  numVar <- base::length(data)
+  numRow <- base::nrow(data)
 
+  if(base::length(Dict) == 1){
+    Dict <- base::rep(Dict,numVar)
+  }
+  
+  # Error check
+  if(!base::is.null(Dict) && (base::length(Dict) != numVar || !is.logical(Dict))){
+    log$error(base::paste0('Input argument Dict must be NULL or a logical vector the same length as the number of columns in data.'))
+    stop()
+  }
+  
   # Convert data according to schema
   if(base::is.null(Schm) && base::is.null(NameFileSchm)){
     # Schema to be constructed from data frame. We done.
     log$debug('Auto-creating schema using data frame.')
-    rpt <- arrow::write_parquet(x=data,sink=NameFile) 
+
   } else {
     # Schema specified in inputs
     nameVarIn <- base::names(data)
-    numVarIn <- base::length(nameVarIn)
-    
+
     # Parquet Schema
     if('ArrowObject' %in% base::class(Schm)){
       
@@ -90,7 +109,7 @@ def.wrte.parq <- function(data,
     }
     
     # Rename the variables to match the schema
-    if(numVarIn != base::length(typeVar$name)){
+    if(numVar != base::length(typeVar$name)){
       log$error('Number of variables in the data does not match number of variables in the schema.')
       stop()
     } else {
@@ -98,7 +117,7 @@ def.wrte.parq <- function(data,
     }
     
     # Assign the data type for each column from the schema
-    for(idxVar in base::seq_len(base::length(typeVar$name))){
+    for(idxVar in base::seq_len(numVar)){
       
       # type indicated by schema
       typeIdx <- base::strsplit(typeVar$type[idxVar],",")[[1]]
@@ -107,7 +126,7 @@ def.wrte.parq <- function(data,
       if(base::any(c('timestamp-millis','timestamp[ms]') %in% typeIdx)){
         # Convert to POSIXct. Parquet writer handles this well.
         data[[idxVar]] <- base::as.POSIXct(data[[idxVar]],tz='GMT')
-      } else if('string' %in% typeIdx){
+      } else if(base::any(c('string','utf8') %in% typeIdx)){
         # Use as.character rather than class(..) <- 'character'. Converts factors to character without factors as attributes.
         data[[idxVar]] <- base::as.character(data[[idxVar]])
       } else if ('boolean' %in% typeIdx){
@@ -124,10 +143,24 @@ def.wrte.parq <- function(data,
       
     }
     
-    # Write the data
-    rpt <- arrow::write_parquet(x=data,sink=NameFile) 
   }
   
+  # Determine whether to use dictionary encoding for each variable
+  if(base::is.null(Dict)){
+    Dict <- base::rep(FALSE,numVar)
+    
+    # Run through each var. If the values repeat often, use dictionary encoding
+    for(idxVar in base::seq_len(numVar)){
+      if(base::length(base::unique(data[[idxVar]])) < 0.7*numRow){
+        Dict[idxVar] <- TRUE
+      }
+    }    
+  }
+
+  
+  # Write the data
+  rpt <- arrow::write_parquet(x=data,sink=NameFile,compression=CompType,compression_level=CompLvl,use_dictionary=Dict) 
+    
   log$debug(base::paste0('Wrote parquet file: ',NameFile))
   return(rpt)
 }
