09f5f0ddc86c388cff86144e6c3b14edfed59aed
vchundru <vchundru@battelleecology.org>
2020-04-22 16:36:11 -0600

    added more testcases

--- pack/NEONprocIS.base/tests/testthat/def.dir.in/test-def.dir.in.R
+++ pack/NEONprocIS.base/tests/testthat/def.dir.in/test-def.dir.in.R
@@ -11,23 +11,33 @@ source("R/def.dir.in.R")
 #             dirOut1 <- "tests/testthat/def.dir.in/test_input/pfs/27134/2019/01/01"
 #             dirOut2 <- "tests/testthat/def.dir.in/test_input/pfs/27134/2019/01/02/outer"
 #             testthat::expect_equal(c(dirOut1,dirOut2), dirIn)
-#          
+# 
 #           })
+# 
+# 
+# test_that("Input directory path is returned only if all the subDir are in the same directory",
+#           {
+#             dirBgn <- "tests/testthat/def.dir.in/test_input/pfs/27135"
+#             subDir <- c("data", "calibration")
+#             dirIn <- def.dir.in(DirBgn = dirBgn, nameDirSub =subDir)
+#             print("printing value of dirIn:")
+#             print(dirIn)
+#             testthat::expect_equal(0, length(dirIn))
+#           })
+# 
+# 
 
 
-test_that("Input directory path is returned only if all the subDir are in the same directory",
+
+test_that("When No subdirectories, then each terminal directory is a datum",
           {
             dirBgn <- "tests/testthat/def.dir.in/test_input/pfs/27135"
-            subDir <- c("data", "calibration")
+            subDir <- NULL
             dirIn <- def.dir.in(DirBgn = dirBgn, nameDirSub =subDir)
             print("printing value of dirIn:")
             print(dirIn)
-            testthat::expect_equal(0, length(dirIn))
+            dirOut1 <- "tests/testthat/def.dir.in/test_input/pfs/27135/outer/data"
+            dirOut2 <- "tests/testthat/def.dir.in/test_input/pfs/27135/outer/inner/calibration"
+            testthat::expect_equal(c(dirOut1,dirOut2), dirIn)
           })
-
-
-
-
-
-
 
