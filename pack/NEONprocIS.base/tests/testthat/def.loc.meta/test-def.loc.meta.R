4b1cc69247a606a94103e4b5533709566faa1419
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-04-28 12:13:29 -0600

    adding test cases

--- pack/NEONprocIS.base/tests/testthat/def.loc.meta/test-def.loc.meta.R
+++ pack/NEONprocIS.base/tests/testthat/def.loc.meta/test-def.loc.meta.R
@@ -2,21 +2,25 @@ library(testthat)
 source("R/def.loc.meta.R")
 
 
-test_that("When No subdirectories, then each terminal directory is a datum",
+test_that("When No restriction, get all locations",
           {
-            nameFile <- "tests/testthat/def.loc.meta/test_input/pfs/prt_calibrated_location_group/prt/2019/01/01/16247"
-          
-            if (file.exists(file.path(nameFile))) {
-              cat(
-                "\n file exists====|\n"
-              )
-            
-            }
-           
+            nameFile <- "tests/testthat/def.loc.meta/test_input/pfs/prt_calibrated_location_group/prt/2019/01/01/16247/prt_16247_location.json"
+
             locationMetaData <- def.loc.meta(NameFile = nameFile)
-            print("printing value of dirIn:")
-            print(dirIn)
-            
-            testthat::expect_equal(c(dirOut1,dirOut2), dirIn)
+            expect_true (length(locationMetaData$site) > 0)
+
           })
+
+
+test_that("When restricted to location, then return only that locaiton",
+          {
+            nameFile <- "tests/testthat/def.loc.meta/test_input/pfs/prt_calibrated_location_group/prt/2019/01/01/16247/prt_16247_location.json"
+            nameLoc <- "CFGLOC101663"
+            locationMetaData <- def.loc.meta(NameFile = nameFile, NameLoc = nameLoc)
+            expect_true (length(locationMetaData$site) == 1)
+            expect_true (locationMetaData[1], equals("POSE"))
+          
+          })
+
+
 
