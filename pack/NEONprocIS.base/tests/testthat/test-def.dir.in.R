59481917025b2f95ef0ab0b9ecc2f1da1d6135b9
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-04-29 14:34:01 -0600

    test cases

--- pack/NEONprocIS.base/tests/testthat/test-def.dir.in.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.dir.in.R
@@ -1,31 +1,28 @@
-library(testthat)
-source("R/def.dir.in.R")
+test_that("Data directory multiple input paths",
+          {
+            dirBgn <- "tests/testthat/def.dir.in/test_input/pfs"
+            subDir <- c("data", "calibration")
+            dirIn <-  NEONprocIS.base::def.dir.in(DirBgn = dirBgn, nameDirSub =subDir)
+            print("printing value of dirIn:")
+            print(dirIn)
+            dirOut1 <- "tests/testthat/def.dir.in/test_input/pfs/27134/2019/01/01"
+            dirOut2 <- "tests/testthat/def.dir.in/test_input/pfs/27134/2019/01/02/outer"
+            testthat::expect_equal(c(dirOut1,dirOut2), dirIn)
 
-# test_that("Data directory multiple input paths",
-#           {
-#             dirBgn <- "tests/testthat/def.dir.in/test_input/pfs"
-#             subDir <- c("data", "calibration")
-#             dirIn <- def.dir.in(DirBgn = dirBgn, nameDirSub =subDir)
-#             print("printing value of dirIn:")
-#             print(dirIn)
-#             dirOut1 <- "tests/testthat/def.dir.in/test_input/pfs/27134/2019/01/01"
-#             dirOut2 <- "tests/testthat/def.dir.in/test_input/pfs/27134/2019/01/02/outer"
-#             testthat::expect_equal(c(dirOut1,dirOut2), dirIn)
-# 
-#           })
-# 
-# 
-# test_that("Input directory path is returned only if all the subDir are in the same directory",
-#           {
-#             dirBgn <- "tests/testthat/def.dir.in/test_input/pfs/27135"
-#             subDir <- c("data", "calibration")
-#             dirIn <- def.dir.in(DirBgn = dirBgn, nameDirSub =subDir)
-#             print("printing value of dirIn:")
-#             print(dirIn)
-#             testthat::expect_equal(0, length(dirIn))
-#           })
-# 
-# 
+          })
+
+
+test_that("Input directory path is returned only if all the subDir are in the same directory",
+          {
+            dirBgn <- "tests/testthat/def.dir.in/test_input/pfs/27135"
+            subDir <- c("data", "calibration")
+            dirIn <-  NEONprocIS.base::def.dir.in(DirBgn = dirBgn, nameDirSub =subDir)
+            print("printing value of dirIn:")
+            print(dirIn)
+            testthat::expect_equal(0, length(dirIn))
+          })
+
+
 
 
 
@@ -33,7 +30,7 @@ test_that("When No subdirectories, then each terminal directory is a datum",
           {
             dirBgn <- "tests/testthat/def.dir.in/test_input/pfs/27135"
             subDir <- NULL
-            dirIn <- def.dir.in(DirBgn = dirBgn, nameDirSub =subDir)
+            dirIn <-  NEONprocIS.base::def.dir.in(DirBgn = dirBgn, nameDirSub =subDir)
             print("printing value of dirIn:")
             print(dirIn)
             dirOut1 <- "tests/testthat/def.dir.in/test_input/pfs/27135/outer/data"
