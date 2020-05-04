26bb0995d4b8b3e520005e8edd4953aac5fbf636
Robert Markel <rmarkel@battelleecology.org>
2020-05-04 11:51:39 -0600

    Updates to genavro version in pipelines.

--- modules/calibrated_location_group/app.py
+++ modules/calibrated_location_group/app.py
@@ -12,33 +12,46 @@ import lib.file_crawler as file_crawler
 log = structlog.get_logger()
 
 
-def group(calibrated_path, location_path, out_path):
+def group(calibrated_path, location_path, out_path, source_type_index, year_index,
+          month_index, day_index, source_id_index, data_type_index):
     """
     Write calibrated data and location files into the output path.
 
-    :param calibrated_path: The input path for calibrated files.
+    :param calibrated_path: Path to the calibration files to process.
     :type calibrated_path: str
-    :param location_path: The input path for location files.
+    :param location_path: Path to the calibration files to process.
     :type location_path: str
-    :param out_path: The output path for writing grouped files.
+    :param out_path: Directory path to write output.
     :type out_path: str
+    :param source_type_index: The source type index in the calibrated file path.
+    :type source_type_index: int
+    :param year_index: The year index in the calibrated file path.
+    :type year_index: int
+    :param month_index: The month index in the calibrated file path.
+    :type month_index: int
+    :param day_index: The day index in the calibrated file path.
+    :type day_index: int
+    :param source_id_index: The source ID index in the calibrated file path.
+    :type source_id_index: int
+    :param data_type_index: The data type index in the calibrated file path.
+    :type data_type_index: int
     :return:
     """
     i = 0
     for file_path in file_crawler.crawl(calibrated_path):
         parts = file_path.parts
-        source_type = parts[3]
-        year = parts[4]
-        month = parts[5]
-        day = parts[6]
-        source_id = parts[7]
-        data_type = parts[8]
+        source_type = parts[source_type_index]
+        year = parts[year_index]
+        month = parts[month_index]
+        day = parts[day_index]
+        source_id = parts[source_id_index]
+        data_type = parts[data_type_index]
         log.debug(f'year: {year}  month: {month}  day: {day}')
         log.debug(f'source type: {source_type} source_id: {source_id} data type: {data_type}')
         target_root = os.path.join(out_path, source_type, year, month, day, source_id)
-        if i == 0:  # Only link location once.
+        if i == 0:  # only link location once
             link_location(location_path, target_root)
-        # Grab all directories and files under the common path (after the data type).
+        # pass remainder of file path after the data type
         target = os.path.join(target_root, data_type, *parts[9:])
         log.debug(f'target: {target}')
         file_linker.link(file_path, target)
@@ -63,14 +76,21 @@ def link_location(location_path, target_root):
 
 def main():
     env = environs.Env()
-    calibrated_path = env('CALIBRATED_PATH')
-    location_path = env('LOCATION_PATH')
-    out_path = env('OUT_PATH')
-    log_level = env('LOG_LEVEL')
+    calibrated_path = env.str('CALIBRATED_PATH')
+    location_path = env.str('LOCATION_PATH')
+    out_path = env.str('OUT_PATH')
+    log_level = env.str('LOG_LEVEL', 'INFO')
+    source_type_index = env.int('SOURCE_TYPE_INDEX')
+    year_index = env.int('YEAR_INDEX')
+    month_index = env.int('MONTH_INDEX')
+    day_index = env.int('DAY_INDEX')
+    source_id_index = env.int('SOURCE_ID_INDEX')
+    data_type_index = env.int('DATA_TYPE_INDEX')
     log_config.configure(log_level)
     log.debug(f'calibrated_dir: {calibrated_path} '
               f'location_dir: {location_path} out_dir: {out_path}')
-    group(calibrated_path, location_path, out_path)
+    group(calibrated_path, location_path, out_path, source_type_index, year_index,
+          month_index, day_index, source_id_index, data_type_index)
 
 
 if __name__ == '__main__':
--- modules/calibrated_location_group/tests/test_app.py
+++ modules/calibrated_location_group/tests/test_app.py
@@ -39,13 +39,34 @@ class AppTest(TestCase):
         #  Create output dir
         self.fs.create_dir(self.out_path)
 
+        self.source_type_index = '3'
+        self.year_index = '4'
+        self.month_index = '5'
+        self.day_index = '6'
+        self.source_id_index = '7'
+        self.data_type_index = '8'
+
     def test_group(self):
-        app.group(self.calibrated_path, self.location_path, self.out_path)
+        app.group(self.calibrated_path,
+                  self.location_path,
+                  self.out_path,
+                  int(self.source_type_index),
+                  int(self.year_index),
+                  int(self.month_index),
+                  int(self.day_index),
+                  int(self.source_id_index),
+                  int(self.data_type_index))
         self.check_output()
 
     def test_main(self):
         os.environ['CALIBRATED_PATH'] = self.calibrated_path
         os.environ['LOCATION_PATH'] = self.location_path
+        os.environ['SOURCE_TYPE_INDEX'] = self.source_type_index
+        os.environ['YEAR_INDEX'] = self.year_index
+        os.environ['MONTH_INDEX'] = self.month_index
+        os.environ['DAY_INDEX'] = self.day_index
+        os.environ['SOURCE_ID_INDEX'] = self.source_id_index
+        os.environ['DATA_TYPE_INDEX'] = self.data_type_index
         os.environ['OUT_PATH'] = self.out_path
         os.environ['LOG_LEVEL'] = 'DEBUG'
         app.main()
