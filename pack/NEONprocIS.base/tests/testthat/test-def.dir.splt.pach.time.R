90d3dff71b1ee7e6f8c323cf5e39f49a97f9d12e
vchundru <vchundru@battelleecology.org>
2020-05-06 10:17:05 -0600

    adding test cases for def.dir.splt.pach.time.R script

--- pack/NEONprocIS.base/tests/testthat/test-def.dir.splt.pach.time.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.dir.splt.pach.time.R
@@ -2,22 +2,42 @@ library(testthat)
 source("R/def.dir.splt.pach.time.R")
 
 
-test_that("When No restriction, get all locations",
+test_that("correct date format",
           {
             nameFile <- "tests/testthat/def.loc.meta/test_input/pfs/prt_calibrated_location_group/prt/2019/01/01/16247/prt_16247_location.json"
-            
-            locationMetaData <- NEONprocIS.base::def.loc.meta(NameFile = nameFile)
-            expect_true (length(locationMetaData$site) > 0)
-            
+
+            rpt <- NEONprocIS.base::def.dir.splt.pach.time(nameFile)
+            timeInPath = base::as.POSIXct('2019-01-01 00:00:00', tz = 'GMT')
+            print(typeof(rpt))
+            expect_true (length(rpt) == 5)
+            testthat::expect_true  (is.list(rpt))
+
+            if (!(length(rpt) == 0)) {
+              testthat::expect_true (rpt$dirSplt[1] == 'tests')
+              testthat::expect_true (rpt$repo[1] == 'prt_calibrated_location_group')
+              testthat::expect_true (rpt$dirRepo[1] == "/prt/2019/01/01/16247/prt_16247_location.json")
+              testthat::expect_true (rpt$time[1] == timeInPath)
+            }
+
           })
 
-
-test_that("When restricted to location, then return only that locaiton",
+test_that(" without date in the directory structue",
           {
-            nameFile <- "tests/testthat/def.loc.meta/test_input/pfs/prt_calibrated_location_group/prt/2019/01/01/16247/prt_16247_location.json"
-            nameLoc <- "CFGLOC101663"
-            locationMetaData <- NEONprocIS.base::def.loc.meta(NameFile = nameFile, NameLoc = nameLoc)
-            expect_true (length(locationMetaData$site) == 1)
-            # expect_true (locationMetaData[1], equals("POSE"))
+            nameFile <-
+              "tests/testthat/def.dir.splt.pach.time/test_input/pfs/testFolder/prt/prt_16247_location.json"
+            
+            rpt <- NEONprocIS.base::def.dir.splt.pach.time(nameFile)
+            print(typeof(rpt))
+            expect_true (length(rpt) == 5)
+            testthat::expect_true  (is.list(rpt))
+            
+            if (!(length(rpt) == 0)) {
+              testthat::expect_true (rpt$dirSplt[2] == 'testthat')
+              testthat::expect_true (rpt$repo[1] == 'testFolder')
+              testthat::expect_true (rpt$dirRepo[1] == "/prt/prt_16247_location.json")
+              testthat::expect_null(rpt$time[1])
+            }
+            
+            
             
           })
