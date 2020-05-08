8d41baeb9ba03386e0eda06e5eb1c0d5f5fcd9fc
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-05-08 11:46:12 -0600

    bug fixes

--- pack/NEONprocIS.base/tests/testthat/test-def.df.renm.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.df.renm.R
@@ -1,12 +1,18 @@
-library(testthat)
-source("R/def.df.renm.R")
+#library(testthat)
+#source("R/def.df.renm.R")
 
 test_that("Renaming the dataframe",
           {
             original_df <- data.frame(X=c(1,2,3),Y=c(4,5,6))
             mappNameVar <- data.frame(nameVarIn=c('X','Y'), nameVarOut=c('NewA','NewB'), stringsAsFactors=FALSE)
             returned_df <- def.df.renm(df = original_df, mappNameVar = mappNameVar)
-            assert_colnames(returned_df, c('NewA','NewB'))
+            testthat::expect_true(is.list(returned_df))
+            if (!(length(returned_df) == 0)) {
+              testthat::equals (returned_df$NewA[1], 1)
+              testthat::equals (returned_df$NewB[3], 6)
+              
+            }
+            
           }
 
 )
--- pack/NEONprocIS.base/tests/testthat/test-def.dir.crea.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.dir.crea.R
@@ -1,20 +1,9 @@
-library(testthat)
-source("R/def.dir.crea.R")
 
 test_that("Data directory created without subpath",
           {
             dirBgn <- "tests/testthat/def.dir.crea/test_output"
             dirSub <- "dirCreaTest"
             NEONprocIS.base::def.dir.crea(DirBgn = dirBgn, DirSub =dirSub)
-            testthat::expect_true(dir.exists("tests/testthat/def.dir.crea/test_output/dirCreaTest"))
-       if (dir.exists("tests/testthat/def.dir.crea/test_output/dirCreaTest")) {  unlink("tests/testthat/def.dir.crea/test_output/dirCreaTest")}
-            
-            
+            testthat::expect_true(dir.exists("def.dir.crea/test_output/dirCreaTest"))
+            if (file.exists("tests/testthat/def.dir.crea/test_output/dirCreaTest")) { file.remove("tests/testthat/def.dir.crea/test_output/dirCreaTest")}
           })
-
-
-
-
-
-
-
