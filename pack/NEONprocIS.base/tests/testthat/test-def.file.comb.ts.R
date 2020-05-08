7849b0a4e39fa299d783730b442ff58f1c59970d
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-05-08 10:40:53 -0600

    adding test cases for "def.file.comb.ts.R"

--- pack/NEONprocIS.base/tests/testthat/test-def.file.comb.ts.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.file.comb.ts.R
@@ -5,18 +5,18 @@ source("R/def.file.comb.ts.R")
 test_that("valid files to mergee",
           {
             file <- c('tests/testthat/def.file.comb.ts/validFiles/testdata.parquet', 'tests/testthat/def.file.comb.ts/validFiles/testflagsdata.parquet')
-            nameFile <- "tests/testthat/def.loc.meta/test_input/pfs/prt_calibrated_location_group/prt/2019/01/01/16247/prt_16247_location.json"
-
-            NEONprocIS.base::def.file.comb.ts(file = file, nameVarTime='readout_time')
+           
+            returnedData <- def.file.comb.ts(file = file, nameVarTime='readout_time')
             
-
-            if (!(length(rpt) == 0)) {
-              testthat::expect_true (rpt$dirSplt[1] == 'tests')
-              testthat::expect_true (rpt$repo[1] == 'prt_calibrated_location_group')
-              testthat::expect_true (rpt$dirRepo[1] == "/prt/2019/01/01/16247/prt_16247_location.json")
-              testthat::expect_true (rpt$time[1] == timeInPath)
+            expect_true (length(returnedData) == 6)
+            testthat::expect_true(is.list(returnedData))
+            if (!(length(returnedData) == 0)) {
+              testthat::expect_true (returnedData$source_id[2] == '16247')
+              testthat::equals (returnedData$temp[5], 0.007209014)
+              testthat::equals (returnedData$validCalQF[1], 0)
+              #testthat::expect_null(returnedData$time[1])
             }
-
+            
           })
 
 # test_that(" without date in the directory structue",
