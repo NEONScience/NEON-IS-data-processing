a9f7b67ab873636d6cbbea1f909d852bbd59c44b
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-05-11 15:05:34 -0600

    more test cases

--- pack/NEONprocIS.base/tests/testthat/test-def.file.comb.ts.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.file.comb.ts.R
@@ -26,3 +26,22 @@ test_that("wrong file format",
             
           })
 
+test_that("duplicate columns in the input files",
+          {
+            file <- c('def.file.comb.ts/invalidFiles/testdata.parquet', 'def.file.comb.ts/invalidFiles/testflagsdatadup.parquet')
+            
+            returnedData <- def.file.comb.ts(file = file, nameVarTime='readout_time')
+            
+            expect_true (length(returnedData) == 6)
+            testthat::expect_true(is.list(returnedData))
+            if (!(length(returnedData) == 0)) {
+              testthat::expect_true (returnedData$source_id[2] == '16247')
+              testthat::equals (returnedData$temp[5], 0.007209014)
+              testthat::equals (returnedData$validCalQF[1], 0)
+              #testthat::expect_null(returnedData$time[1])
+            }
+            
+          })
+
+
+
