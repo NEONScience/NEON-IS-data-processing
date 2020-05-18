67989f43a9a0c7c7d2eee9a8146d650adc596cac
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-05-18 15:34:29 -0600

    bug fixes

--- pack/NEONprocIS.base/tests/testthat/test-def.file.name.out.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.file.name.out.R
@@ -1,10 +1,10 @@
 # library(testthat)
 # source("R/def.file.name.out.R")
 
-test_that("Output is no",
+test_that("Output path with prefix filename suffix and extension",
           {
 
-            output_filename <- def.file.name.out(nameFileIn='myFileName.json',prfx='Prefix_',sufx='_Suffix',ext='txt')
+            output_filename <- NEONprocIS.base::def.file.name.out(nameFileIn='myFileName.json',prfx='Prefix_',sufx='_Suffix',ext='txt')
             testthat::equals(output_filename, 'Prefix_myFileName_Suffix.txt')
           }
 
@@ -13,7 +13,7 @@ test_that("Output is no",
 test_that("Output when there is no extension in the nameFileIn",
           {
 
-            output_filename <- def.file.name.out(nameFileIn='myFileName',prfx='Prefix_',sufx='_Suffix',ext='txt')
+            output_filename <- NEONprocIS.base::def.file.name.out(nameFileIn='myFileName',prfx='Prefix_',sufx='_Suffix',ext='txt')
             testthat::equals(output_filename, 'Prefix_myFileName_Suffix.txt')
           }
 
@@ -22,7 +22,7 @@ test_that("Output when there is no extension in the nameFileIn",
 test_that("Output when there is no suffix",
           {
 
-            output_filename <- def.file.name.out(nameFileIn='myFileName',prfx='Prefix_',ext='txt')
+            output_filename <- NEONprocIS.base::def.file.name.out(nameFileIn='myFileName',prfx='Prefix_',ext='txt')
             testthat::equals(output_filename, 'Prefix_myFileName.txt')
           }
 
@@ -31,7 +31,7 @@ test_that("Output when there is no suffix",
 test_that("Output when there is no prefix and suffix",
           {
 
-            output_filename <- def.file.name.out(nameFileIn='myFileName',ext='txt')
+            output_filename <- NEONprocIS.base::def.file.name.out(nameFileIn='myFileName',ext='txt')
             testthat::equals(output_filename, 'Prefix_myFileName.txt')
           }
 
@@ -40,7 +40,7 @@ test_that("Output when there is no prefix and suffix",
 test_that("Output when there are multiple periods in the filenameIn",
           {
             
-            output_filename <- def.file.name.out(nameFileIn='myFileName.ext1.ext2.ext3', prfx='Prefix_',sufx='_Suffix', ext='txt')
+            output_filename <- NEONprocIS.base::def.file.name.out(nameFileIn='myFileName.ext1.ext2.ext3', prfx='Prefix_',sufx='_Suffix', ext='txt')
             testthat::equals(output_filename, 'Prefix_myFileName.ext1.ext2_Suffix.txt')
           }
           
