6c079da8082ddac454cd672cf4a12371a325e682
Robert Markel <rmarkel@battelleecology.org>
2020-05-21 14:13:52 -0600

    Updates for linking empty directories.

--- modules/date_gap_filler_linker/app.py
+++ modules/date_gap_filler_linker/app.py
@@ -1,16 +1,85 @@
 #!/usr/bin/env python3
-import os
 import structlog
 import environs
 import glob
 from pathlib import Path
 
 import lib.log_config as log_config
-from lib.file_crawler import crawl
-from lib.file_linker import link
-
 
 log = structlog.get_logger()
+
+
+class Linker(object):
+
+    def __init__(self, in_path, out_path, relative_path_index, empty_file_suffix):
+        """
+        Constructor.
+
+        :param in_path: The root input path.
+        :type in_path: Path
+        :param out_path: The output path for linking files and directories.
+        :type out_path: Path
+        :param relative_path_index: Trim input paths up to this index.
+        :type relative_path_index: int
+        :param empty_file_suffix: The file extension for empty files.
+        :type empty_file_suffix: str
+        """
+        self.in_path = in_path
+        self.out_path = out_path
+        self.relative_path_index = relative_path_index
+        self.empty_file_suffix = empty_file_suffix
+
+    def link(self):
+        """
+        Link input files and directories into the output path.
+
+        :return:
+        """
+        paths = self.in_path.glob('**/*')
+        for path in paths:
+            log.debug(f'path: {path}')
+            parts = path.parts
+            if path.is_dir():
+                # add the metadata directories to the output path in case they are empty
+                dir_path = Path(self.out_path, *parts[self.relative_path_index:])
+                log.debug(f'dir_path: {dir_path}')
+                dir_path.mkdir(parents=True)
+            elif path.is_file():
+                # check for empty file
+                if path.suffix == self.empty_file_suffix:
+                    # real paths do not have the empty file suffix, strip it off with stem
+                    real_path = Path(*parts[:len(parts) - 1], path.stem)
+                    log.debug(f'real_path: {real_path}')
+                    # if a real file exists, do not link the empty file
+                    if real_path.exists():
+                        log.debug('real path exists')
+                        continue
+                    else:
+                        # no real file exists, trim the suffix to link the empty file
+                        # with the expected real file name
+                        link_path = Path(self.out_path, *parts[self.relative_path_index:len(parts) - 1], path.stem)
+                        log.debug(f'linking {link_path} to {path}')
+                        link_path.symlink_to(path)
+                else:
+                    if self.location_is_active(parts):
+                        link_path = Path(self.out_path, *parts[self.relative_path_index:])
+                        log.debug(f'linking {link_path} to {path}')
+                        link_path.symlink_to(path)
+
+    def location_is_active(self, path_parts):
+        """
+        Check to see if the directory contains empty files indicating the location was active.
+
+        :param path_parts:
+        :return:
+        """
+        glob_path = Path(self.in_path, *path_parts[self.relative_path_index:len(path_parts) - 2])
+        # glob on the data type and filename
+        glob_pattern = f'{glob_path}/*/*{self.empty_file_suffix}'
+        log.debug(f'glob_pattern: {glob_pattern}')
+        if glob.glob(glob_pattern):
+            return True
+        return False
 
 
 def main():
@@ -18,37 +87,13 @@ def main():
     in_path = env.path('IN_PATH')
     out_path = env.path('OUT_PATH')
     log_level = env.log_level('LOG_LEVEL', 'INFO')
-    source_type_index = env.int('SOURCE_TYPE_INDEX')
-    year_index = env.int('YEAR_INDEX')
-    month_index = env.int('MONTH_INDEX')
-    day_index = env.int('DAY_INDEX')
-    location_index = env.int('LOCATION_INDEX')
-    data_type_index = env.int('DATA_TYPE_INDEX')
-    filename_index = env.int('FILENAME_INDEX')
+    relative_path_index = env.int('RELATIVE_PATH_INDEX')
+    empty_file_suffix = env.str('EMPTY_FILE_SUFFIX')
+
     log_config.configure(log_level)
 
-    suffix = '.empty'
-
-    for file in crawl(in_path):
-        parts = Path(file).parts
-        root = os.path.join(*parts[:source_type_index])
-        source = parts[source_type_index]
-        year = parts[year_index]
-        month = parts[month_index]
-        day = parts[day_index]
-        location = parts[location_index]
-        data_type = parts[data_type_index]
-        filename = parts[filename_index]
-        if file.suffix == suffix:
-            real_data_path = os.path.join(root, source, year, month, day, location, data_type, file.stem)
-            if os.path.exists(real_data_path):
-                continue  # a real file exists, do not link the empty file
-            else:
-                filename = filename.replace(suffix, '')  # no real file exists, trim the suffix
-        if glob.glob(f'{root}/{source}/{year}/{month}/{day}/{location}/*/*{suffix}'):
-            link_path = os.path.join(out_path, source, year, month, day, location, data_type, filename)
-            log.debug(f'file: {file} link: {link_path}')
-            link(file, link_path)
+    linker = Linker(in_path, out_path, relative_path_index, empty_file_suffix)
+    linker.link()
 
 
 if __name__ == '__main__':
--- modules/date_gap_filler_linker/tests/test_app.py
+++ modules/date_gap_filler_linker/tests/test_app.py
@@ -12,37 +12,37 @@ class AppTest(TestCase):
         self.setUpPyfakefs()
 
         self.out_path = os.path.join('/', 'pfs', 'out')
-        self.in_path = os.path.join('/', 'pfs', 'repo')
+        self.in_path = os.path.join('/', 'pfs', 'in')
         self.fs.create_dir(self.out_path)
         self.fs.create_dir(self.in_path)
 
-        self.metadata_1 = os.path.join('prt', '2019', '01', '06', 'CFGLOC113499')
-        self.metadata_2 = os.path.join('prt', '2019', '01', '07', 'CFGLOC113499')
-        self.metadata_3 = os.path.join('prt', '2019', '01', '08', 'CFGLOC113499')
+        self.metadata_1 = os.path.join('prt', '2019', '01', '06', 'location')
+        self.metadata_2 = os.path.join('prt', '2019', '01', '07', 'location')
+        self.metadata_3 = os.path.join('prt', '2019', '01', '08', 'location')
 
         # empty files
-        data_1 = os.path.join(self.in_path, self.metadata_1, 'data', 'prt_CFGLOC113499_2019-01-06.parquet.empty')
+        data_1 = os.path.join(self.in_path, self.metadata_1, 'data', 'prt_location_2019-01-06.parquet.empty')
         flags_1 = os.path.join(self.in_path, self.metadata_1, 'flags',
-                               'prt_CFGLOC113499_2019-01-06_flagsCal.parquet.empty')
-        location_1 = os.path.join(self.in_path, self.metadata_1, 'location', 'CFGLOC113499.json')
-        calibration_1 = os.path.join(self.in_path, self.metadata_1, 'calibration', 'calibration.xml')
+                               'prt_location_2019-01-06_flagsCal.parquet.empty')
+        location_1 = os.path.join(self.in_path, self.metadata_1, 'location', 'location.json')
+        calibration_1 = os.path.join(self.in_path, self.metadata_1, 'calibration')
         uncertainty_1 = os.path.join(self.in_path, self.metadata_1, 'uncertainty_data',
-                                     'prt_CFGLOC113499_2019-01-06_uncertaintyData.parquet.empty')
+                                     'prt_location_2019-01-06_uncertaintyData.parquet.empty')
         self.fs.create_file(data_1)
         self.fs.create_file(flags_1)
         self.fs.create_file(location_1)
-        self.fs.create_file(calibration_1)
+        self.fs.create_dir(calibration_1)
         self.fs.create_file(uncertainty_1)
 
         # real data
-        data_2 = os.path.join(self.in_path, self.metadata_2, 'data', 'prt_CFGLOC113499_2019-01-07.parquet')
-        data_2_e = os.path.join(self.in_path, self.metadata_2, 'data', 'prt_CFGLOC113499_2019-01-07.parquet.empty')
+        data_2 = os.path.join(self.in_path, self.metadata_2, 'data', 'prt_location_2019-01-07.parquet')
+        data_2_e = os.path.join(self.in_path, self.metadata_2, 'data', 'prt_location_2019-01-07.parquet.empty')
         flags_2 = os.path.join(self.in_path, self.metadata_2, 'flags',
-                               'prt_CFGLOC113499_2019-01-07_flagsCal.parquet.empty')
-        location_2 = os.path.join(self.in_path, self.metadata_2, 'location', 'CFGLOC113499.json')
+                               'prt_location_2019-01-07_flagsCal.parquet.empty')
+        location_2 = os.path.join(self.in_path, self.metadata_2, 'location', 'location.json')
         calibration_2 = os.path.join(self.in_path, self.metadata_2, 'calibration', 'calibration.xml')
         uncertainty_2 = os.path.join(self.in_path, self.metadata_2, 'uncertainty_data',
-                                     'prt_CFGLOC113499_2019-01-07_uncertaintyData.parquet.empty')
+                                     'prt_location_2019-01-07_uncertaintyData.parquet.empty')
         self.fs.create_file(data_2)
         self.fs.create_file(data_2_e)
         self.fs.create_file(flags_2)
@@ -51,71 +51,58 @@ class AppTest(TestCase):
         self.fs.create_file(uncertainty_2)
 
         # real data, no empty file (location not active)
-        data_3 = os.path.join(self.in_path, self.metadata_3, 'data', 'prt_CFGLOC113499_2019-01-08.parquet')
-        flags_3 = os.path.join(self.in_path, self.metadata_3, 'flags', 'prt_CFGLOC113499_2019-01-08_flagsCal.parquet')
-        location_3 = os.path.join(self.in_path, self.metadata_3, 'location', 'CFGLOC113499.json')
+        data_3 = os.path.join(self.in_path, self.metadata_3, 'data', 'prt_location_2019-01-08.parquet')
+        flags_3 = os.path.join(self.in_path, self.metadata_3, 'flags', 'prt_location_2019-01-08_flagsCal.parquet')
+        location_3 = os.path.join(self.in_path, self.metadata_3, 'location', 'location.json')
         calibration_3 = os.path.join(self.in_path, self.metadata_3, 'calibration', 'calibration.xml')
         uncertainty_3 = os.path.join(self.in_path, self.metadata_3, 'uncertainty_data',
-                                     'prt_CFGLOC113499_2019-01-08_uncertaintyData.parquet')
+                                     'prt_location_2019-01-08_uncertaintyData.parquet')
         self.fs.create_file(data_3)
         self.fs.create_file(flags_3)
         self.fs.create_file(location_3)
         self.fs.create_file(calibration_3)
         self.fs.create_file(uncertainty_3)
 
-        # path indices
-        self.source_type_index = '3'
-        self.year_index = '4'
-        self.month_index = '5'
-        self.day_index = '6'
-        self.location_index = '7'
-        self.data_type_index = '8'
-        self.filename_index = '9'
-
     def test_main(self):
         os.environ['IN_PATH'] = self.in_path
         os.environ['OUT_PATH'] = self.out_path
-        os.environ['LOG_LEVEL'] = 'DEBUG'
-        os.environ['SOURCE_TYPE_INDEX'] = self.source_type_index
-        os.environ['YEAR_INDEX'] = self.year_index
-        os.environ['MONTH_INDEX'] = self.month_index
-        os.environ['DAY_INDEX'] = self.day_index
-        os.environ['LOCATION_INDEX'] = self.location_index
-        os.environ['DATA_TYPE_INDEX'] = self.data_type_index
-        os.environ['FILENAME_INDEX'] = self.filename_index
+        os.environ['LOG_LEVEL'] = 'INFO'
+        os.environ['RELATIVE_PATH_INDEX'] = '3'
+        os.environ['EMPTY_FILE_SUFFIX'] = '.empty'
         app.main()
         self.check_output()
 
     def check_output(self):
         # empty files
         self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_1,
-                                                     'data', 'prt_CFGLOC113499_2019-01-06.parquet')))
+                                                     'data', 'prt_location_2019-01-06.parquet')))
         self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_1, 'flags',
-                                                     'prt_CFGLOC113499_2019-01-06_flagsCal.parquet')))
-        self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_1, 'location', 'CFGLOC113499.json')))
-        self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_1, 'calibration', 'calibration.xml')))
+                                                     'prt_location_2019-01-06_flagsCal.parquet')))
+        self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_1, 'location', 'location.json')))
+        # empty directory test
+        self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_1, 'calibration')))
         self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_1, 'uncertainty_data',
-                                                     'prt_CFGLOC113499_2019-01-06_uncertaintyData.parquet')))
+                                                     'prt_location_2019-01-06_uncertaintyData.parquet')))
 
         # real data
         self.assertTrue(os.path.lexists(
-            os.path.join(self.out_path, self.metadata_2, 'data', 'prt_CFGLOC113499_2019-01-07.parquet')))
+            os.path.join(self.out_path, self.metadata_2, 'data', 'prt_location_2019-01-07.parquet')))
         self.assertFalse(os.path.lexists(
-            os.path.join(self.out_path, self.metadata_2, 'data', 'prt_CFGLOC113499_2019-01-07.parquet.empty')))
+            os.path.join(self.out_path, self.metadata_2, 'data', 'prt_location_2019-01-07.parquet.empty')))
         self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_2, 'flags',
-                                                     'prt_CFGLOC113499_2019-01-07_flagsCal.parquet')))
-        self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_2, 'location', 'CFGLOC113499.json')))
+                                                     'prt_location_2019-01-07_flagsCal.parquet')))
+        self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_2, 'location', 'location.json')))
         self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_2, 'calibration', 'calibration.xml')))
         self.assertTrue(os.path.lexists(os.path.join(self.out_path, self.metadata_2, 'uncertainty_data',
-                                                     'prt_CFGLOC113499_2019-01-07_uncertaintyData.parquet')))
+                                                     'prt_location_2019-01-07_uncertaintyData.parquet')))
 
         # real data, no empty file (location not active)
         self.assertFalse(os.path.lexists(
-            os.path.join(self.out_path, self.metadata_3, 'data', 'prt_CFGLOC113499_2019-01-08.parquet')))
+            os.path.join(self.out_path, self.metadata_3, 'data', 'prt_location_2019-01-08.parquet')))
         self.assertFalse(os.path.lexists(
-            os.path.join(self.out_path, self.metadata_3, 'flags', 'prt_CFGLOC113499_2019-01-08_flagsCal.parquet')))
-        self.assertFalse(os.path.lexists(os.path.join(self.out_path, self.metadata_3, 'location', 'CFGLOC113499.json')))
+            os.path.join(self.out_path, self.metadata_3, 'flags', 'prt_location_2019-01-08_flagsCal.parquet')))
+        self.assertFalse(os.path.lexists(os.path.join(self.out_path, self.metadata_3, 'location', 'location.json')))
         self.assertFalse(
             os.path.lexists(os.path.join(self.out_path, self.metadata_3, 'calibration', 'calibration.xml')))
         self.assertFalse(os.path.lexists(os.path.join(self.out_path, self.metadata_3, 'uncertainty_data',
-                                                      'prt_CFGLOC113499_2019-01-08_uncertaintyData.parquet')))
+                                                      'prt_location_2019-01-08_uncertaintyData.parquet')))
