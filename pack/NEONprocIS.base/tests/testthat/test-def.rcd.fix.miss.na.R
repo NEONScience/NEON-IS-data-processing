a0e6a8c4211c28eb07ed4d67d99cb921d4e305d0
Visala Chundru <vchundru@den-devissom.ci.neoninternal.org>
2020-05-22 10:21:40 -0600

    adding test cases for def.rcd.fix.miss.na script

--- pack/NEONprocIS.base/tests/testthat/test-def.rcd.fix.miss.na.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.rcd.fix.miss.na.R
@@ -1,21 +1,17 @@
-# # library(testthat)
-# # source("R/def.rcd.fix.miss.na.R")
-# 
-# test_that("Output path with prefix filename suffix and extension",
-#           {
-#              
-#                                    
-#             readout_time = as.Date(c(base::as.POSIXct('2019-06-12 00:10:20', tz = 'GMT'),
-#                                      base::as.POSIXct("2012-01-01", tz = 'GMT'),
-#                                      base::as.POSIXct("2013-09-23", tz = 'GMT'),
-#                                      base::as.POSIXct("2014-11-15", tz = 'GMT'), 
-#                                      base::as.POSIXct("2015-03-27", tz = 'GMT')))
-#             output_filename <- NEONprocIS.base::def.rcd.fix.miss.na(nameFileIn='myFileName.json',prfx='Prefix_',sufx='_Suffix',ext='txt')
-#             testthat::equals(output_filename, 'Prefix_myFileName_Suffix.txt')
-#           }
-# 
-# )
-# 
+library(testthat)
+source("R/def.rcd.fix.miss.na.R")
+
+test_that("fixing the badtime values",
+          {
+            data <- NEONprocIS.base::def.read.parq(NameFile ="~/git/NEON-IS-data-processing/pack/NEONprocIS.base/tests/testthat/def.rcd.fix.miss.na/testdata.parquet")
+            inputfilepaths <- c('~/git/NEON-IS-data-processing/pack/NEONprocIS.base/tests/testthat/def.rcd.fix.miss.na/testdata.parquet', '~/git/NEON-IS-data-processing/pack/NEONprocIS.base/tests/testthat/def.rcd.miss.na/valid_files/testflagsdata.parquet')
+            returnList <-NEONprocIS.base::def.rcd.miss.na(fileData=inputfilepaths)
+            output_filename <- NEONprocIS.base::def.rcd.fix.miss.na(data=data, timeBad=returnList$timeBad,valuBad='test')
+            expect_true (length(output_filename$readout_time) == 6)
+            testthat::equals(length(output_filename$suspectCalQF[6]), 'test')
+          }
+
+)
 # test_that("Output when there is no extension in the nameFileIn",
 #           {
 # 
@@ -45,10 +41,10 @@
 # 
 # test_that("Output when there are multiple periods in the filenameIn",
 #           {
-#             
+# 
 #             output_filename <- NEONprocIS.base::def.rcd.fix.miss.na(nameFileIn='myFileName.ext1.ext2.ext3', prfx='Prefix_',sufx='_Suffix', ext='txt')
 #             testthat::equals(output_filename, 'Prefix_myFileName.ext1.ext2_Suffix.txt')
 #           }
-#           
+# 
 # )
 # 
