a2ee0c8007ab380d3d6612da77db6c998c0314f9
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-05-27 14:31:14 -0600

    bug fixes

--- pack/NEONprocIS.base/tests/testthat/test-def.rcd.fix.miss.na.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.rcd.fix.miss.na.R
@@ -3,12 +3,12 @@
 
 test_that("fixing the badtime values",
           {
-            data <- NEONprocIS.base::def.read.parq(NameFile ="tests/testthat/def.rcd.fix.miss.na/testdata.parquet")
-            inputfilepaths <- c('tests/testthat/def.rcd.fix.miss.na/testdata.parquet', '~/git/NEON-IS-data-processing/pack/NEONprocIS.base/tests/testthat/def.rcd.miss.na/valid_files/testflagsdata.parquet')
+            data <- NEONprocIS.base::def.read.parq(NameFile ="def.rcd.fix.miss.na/testdata.parquet")
+            inputfilepaths <- c('def.rcd.fix.miss.na/testdata.parquet', 'def.rcd.miss.na/valid_files/testflagsdata.parquet')
             returnList <-NEONprocIS.base::def.rcd.miss.na(fileData=inputfilepaths)
             output_filename <- NEONprocIS.base::def.rcd.fix.miss.na(data=data, timeBad=returnList$timeBad,valuBad='test')
             expect_true (length(output_filename$readout_time) == 6)
-            testthat::equals(length(output_filename$suspectCalQF[6]), 'test')
+            testthat::equals(length(output_filename$suspectCalQF[6]), "test")
           }
 
 )
