37d826946f7a35c6ac146ee66e715386843d9f58
covesturtevant <csturtevant@battelleecology.org>
2020-06-18 15:37:07 -0600

    test script update

--- flow/flow.test.pach/flow.test.pach.R
+++ flow/flow.test.pach/flow.test.pach.R
@@ -1,13 +1,21 @@
 # Specify directory paths
 dirIn <- Sys.getenv("DIR_IN") # Input path placed into the container for each datum. DIR_IN is the input repo name.
 dirOutBase <- '/pfs/out' # Base output path - MUST be /pfs/out for Pachyderm to recognize it as output
-dirOut <- paste0(dirOutBase,'/',dirIn) # Specific output path for each datum
 
-# Create some text for each datum path, and print it to screen
-text <- paste('Hello. The datum path is',dirIn) 
-print(text)
+dirDatm <- NEONprocIS.base::def.dir.in(DirBgn = dirIn,nameDirSub='data')
 
-# Write an output file in the output path
-dir.create(dirOut,recursive = TRUE) # Create the output directory in the container
-nameFile <- paste0(dirOut,'/output.txt') # Name of the output file (full path)
-write(text,nameFile)  # Write the output file
+for (idxDatm in dirDatm){
+  
+  InfoDirIn <- NEONprocIS.base::def.dir.splt.pach.time(idxDatm)
+  
+  dirOut <- paste0(dirOutBase,InfoDirIn$dirRepo) # Specific output path for each datum
+  
+  # Create some text for each datum path, and print it to screen
+  text <- paste('Hello. The datum path is',InfoDirIn$dirRepo) 
+  print(text)
+  
+  # Write an output file in the output path
+  dir.create(dirOut,recursive = TRUE) # Create the output directory in the container
+  nameFile <- paste0(dirOut,'/output.txt') # Name of the output file (full path)
+  write(text,nameFile)  # Write the output file
+}
