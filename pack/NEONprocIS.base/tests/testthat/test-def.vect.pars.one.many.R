3c89b7b6f9e717a4970790a9da0e6ee2591b363d
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-06-18 12:38:53 -0600

    more test cases for "def.vect.pars.one.many.R"

---
+++ pack/NEONprocIS.base/tests/testthat/test-def.vect.pars.one.many.R
@@ -0,0 +1,79 @@
+library(testthat)
+source("R/def.vect.pars.one.many.R")
+
+# test_that("when listVect is not a list, throw an error",
+#           {
+#             listVect <- 'key1'
+#             NameList <- c('MyKeys','MyValues')
+#             Type <- c('character','numeric')
+#             rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
+#             testthat::expect_true((class(rpt)[1] == "try-error"))
+# 
+#           })
+
+test_that("when listVect length is less than 2, throw an error",
+          {
+            listVect <- list(c('key1'))
+            NameList <- c('MyKeys','MyValues')
+            Type <- c('character','numeric')
+            rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
+            testthat::expect_true((class(rpt)[1] == "try-error"))
+            
+          })
+
+# 
+# 
+#           })
+# 
+# test_that("when length of ValueDflt is not 1, throw an error",
+#           {
+#             vect <- c('key1','1.5')
+#             KeyExpc <- c('key1','key2','key3')
+#             ValuDflt <- c(3,4)
+#             NameCol <- c('MyKey', 'MyValu')
+#             Type <- c('character','numeric')
+#             report <- try(NEONprocIS.base::def.vect.pars.pair(vect=vect,KeyExp=KeyExpc,ValuDflt=ValuDflt,NameCol=NameCol,Type=Type), silent = TRUE)
+#             testthat::expect_true((class(report)[1] == "try-error"))
+# 
+#           })
+# 
+# 
+# test_that("when type is not 2, throw an error",
+#           {
+#             vect <- c('key1','1.5')
+#             KeyExpc <- c('key1','key2')
+#             ValuDflt <- 3
+#             NameCol <- c('MyKey', 'MyKey2')
+#             Type <- c('character')
+#             rptt <- try(NEONprocIS.base::def.vect.pars.pair(vect=vect,KeyExp=KeyExpc,ValuDflt=ValuDflt,NameCol=NameCol,Type=Type), silent = TRUE)
+#             testthat::expect_true((class(rptt)[1] == "try-error"))
+# 
+# 
+#           })
+# 
+# test_that("when vect lenght is 1 and keyExpc is 0, throw an error",
+#           {
+#             vect <- c('key1')
+#             ValuDflt <- 3
+#             NameCol <- c('MyKey', 'MyKey2')
+#             Type <- c('character', 'numeric')
+#             rptt <- try(NEONprocIS.base::def.vect.pars.pair(vect=vect,ValuDflt=ValuDflt,NameCol=NameCol,Type=Type), silent = TRUE)
+#             testthat::expect_true((class(rptt)[1] == "try-error"))
+# 
+# 
+#           })
+# 
+# test_that("when vect lenght is 1 and keyExpc is 1, return value",
+#           {
+#             vect <- c('value1')
+#             ValuDflt <- 3
+#             KeyExpc <- c('key1')
+#             NameCol <- c('MyKey', 'MyKey2')
+#             Type <- c('character', 'character')
+#             rptt <- try(NEONprocIS.base::def.vect.pars.pair(vect=vect, KeyExp=KeyExpc, ValuDflt=ValuDflt,NameCol=NameCol,Type=Type), silent = TRUE)
+#             testthat::expect_equal(2, length(rptt))
+#             testthat::expect_equal(rptt$MyKey[1], "key1")
+#             testthat::expect_equal(rptt$MyKey2[1], "value1")
+#             
+#             
+#           })
