153f679c98737157130ddbf0ec268c3e40b874a8
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-06-18 15:59:29 -0600

    added more test cases for def.vect.pars.one.many.R

--- pack/NEONprocIS.base/tests/testthat/test-def.vect.pars.one.many.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.vect.pars.one.many.R
@@ -1,36 +1,46 @@
-library(testthat)
-source("R/def.vect.pars.one.many.R")
+#library(testthat)
+#source("R/def.vect.pars.one.many.R")
 
-# test_that("when listVect is not a list, throw an error",
-#           {
-#             listVect <- 'key1'
-#             NameList <- c('MyKeys','MyValues')
-#             Type <- c('character','numeric')
-#             rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
-#             testthat::expect_true((class(rpt)[1] == "try-error"))
-# 
-#           })
+test_that("when listVect is not a list, throw an error",
+          {
+            listVect <- 'key1'
+            NameList <- c('MyKeys','MyValues')
+            Type <- c('character','numeric')
+            rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
+            testthat::expect_true((class(rpt)[1] == "try-error"))
 
-# test_that("when listVect length is less than 2, throw an error",
-#           {
-#             listVect <- list(c('key1'))
-#             NameList <- c('MyKeys','MyValues')
-#             Type <- c('character','numeric')
-#             rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
-#             testthat::expect_true((class(rpt)[1] == "try-error"))
-#             
-#           })
+          })
+
+test_that("when listVect length is less than 2, throw an error",
+          {
+            listVect <- list(c('key1'))
+            NameList <- c('MyKeys','MyValues')
+            Type <- c('character','numeric')
+            rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
+            testthat::expect_true((class(rpt)[1] == "try-error"))
+
+          })
+
+test_that("when type is not 2, throw an error",
+          {
+            listVect <- c('key1','1.5')
+            NameList <- c('MyKeys','MyValues')
+            Type <- c('character')
+            rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
+            testthat::expect_true((class(rpt)[1] == "try-error"))
+            
+          })
 
 
-# test_that("when NameList length is not 2, log an error but proceeds",
-#            {
-#               listVect <- list(c('key1', 'key2'))
-#               NameList <- c('MyKeys','MyValues', 'wrongvalue')
-#               Type <- c('character','numeric')
-#               rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
-#               testthat::expect_false((class(rpt)[1] == "try-error"))
-# 
-#            })
+test_that("when NameList length is not 2, log an error but proceeds",
+           {
+              listVect <- list(c('key1', 'key2'))
+              NameList <- c('MyKeys','MyValues', 'wrongvalue')
+              Type <- c('character','numeric')
+              rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
+              testthat::expect_true((class(rpt)[1] == "try-error"))
+
+           })
 
 test_that("when all the values are passed, returns values",
           {
@@ -39,7 +49,10 @@ test_that("when all the values are passed, returns values",
               Type <- c('character','numeric')
               rpt <- NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type)
               testthat::expect_equal(2, length(rpt))
-             # testthat::expect_equal(rpt$key1[1], 1.5 3.0 4.0 5.0)
-              testthat::expect_equal(rpt$key2[1], 3)
-              
+              testthat::expect_equal(rpt[[1]]$MyKeys, 'key1')
+              testthat::expect_equal(rpt[[1]]$MyValues[1], 1.5)
+              testthat::expect_equal(rpt[[1]]$MyValues[4], 5.0)
+              testthat::expect_equal(rpt[[2]]$MyKeys, 'key2')
+              testthat::expect_equal(rpt[[2]]$MyValues[1], 3.0)
+
           })
