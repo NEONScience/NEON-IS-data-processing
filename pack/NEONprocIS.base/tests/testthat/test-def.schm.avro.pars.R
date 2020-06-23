c2bb76f438ecf84ba2698fb122d7a47dec7226f9
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-06-23 16:28:08 -0600

    added testcases for def.schm.avro.pars.R

--- pack/NEONprocIS.base/tests/testthat/test-def.schm.avro.pars.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.schm.avro.pars.R
@@ -1,11 +1,23 @@
-library(testthat)
-source("R/def.schm.avro.pars.R")
+#library(testthat)
+#source("R/def.schm.avro.pars.R")
 
-test_that("when length of vector is not multiple of 2, throw an error",
+test_that("when fileSchm and schm are both null, throw an exceptionr",
+          {
+            rpt <- try(NEONprocIS.base::def.schm.avro.pars(FileSchm = NULL, Schm = NULL), silent = TRUE)
+            testthat::expect_true((class(rpt)[1] == "try-error"))
+
+          })
+
+test_that("when avro schm is pass, return all the elements in the schema",
           {
             FileSchm <- "tests/testthat/def.wrte.parq/prt_calibrated.avsc"
             rpt <- try(NEONprocIS.base::def.schm.avro.pars(FileSchm = FileSchm), silent = TRUE)
-            testthat::expect_true((class(rpt)[1] == "try-error"))
-
+            testthat::expect_false((class(rpt)[1] == "try-error"))
+            testthat::expect_true(is.list(rpt))
+            expect_true (length(rpt$schmJson) == 1)
+            expect_true (length(rpt$schmList) == 5)
+            expect_true (length(rpt$var) == 3)
+            expect_true (rpt$var[1]$name[1] == 'source_id')
+            expect_true (rpt$var[3]$doc[3] == 'NA')
           })
 
