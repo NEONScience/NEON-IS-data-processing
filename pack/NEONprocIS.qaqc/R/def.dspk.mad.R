0383427e5f65b47b8de560b372ad4059f20a0a7e
covesturtevant <csturtevant@battelleecology.org>
2020-06-23 13:49:48 -0600

    method A complete

--- pack/NEONprocIS.qaqc/R/def.dspk.mad.R
+++ pack/NEONprocIS.qaqc/R/def.dspk.mad.R
@@ -11,7 +11,7 @@
 
 #' @param data a numeric vector
 #' @param Meth Character string. 
-#' @param Mad Value
+#' @param ThshMad Value
 #' @param Wndw Value. Must be odd
 #' @param WndwStep Value
 #' @param NumPtsGrp Value
@@ -42,21 +42,21 @@
 def.dspk.mad <-
   function(data,
            Meth=c('A','B')[1],
-           Mad,
+           ThshMad,
            Wndw,
-           WndwStep,
-           NumPtsGrp,
-           NaFracMax,
+           WndwStep=1,
+           NumGrp=4,
+           NaFracMax=1,
            log = NULL) {
     # initialize logging if necessary
     if (base::is.null(log)) {
       log <- NEONprocIS.base::def.log.init()
     }
     
-    # Validate the data frame & numeric data only
-    if (!NEONprocIS.base::def.validate.dataframe(dfIn = data,
-                                                 TestNumc = TRUE,
-                                                 log = log)) {
+    # Validate numeric data only
+    if (!NEONprocIS.base::def.validate.vector(vectIn = data,
+                                              TestNumc = TRUE,
+                                              log = log)) {
       stop()
     }
     
@@ -65,46 +65,77 @@ def.dspk.mad <-
     # Meth only A or B
     # Wndw integer & odd for Method A
     # WndwStep only applied to method B and must be less than Wndw/2
-
+    # NumGrp ,ust be > 1
     
-    MultMad <- 1.4826 # Scale factor for MAD equal to reciprocal of the quantile function at probability of 75%
-     <- 
+    # Scale factor to use MAD as a consistent estimation of std
+    # equal to reciprocal of the quantile function at probability of 75%
+    CorStd <- 1.4826 
     
     # Setup
-    numData <- base::nrow(data)
+    numData <- base::length(data)
+    dmmyVect <- rep(NA,numData)
     
     # Method A
     if (Meth == 'A'){
       
+      numDataWndw<-dmmyVect
+      med <- dmmyVect
+      mad <- dmmyVect
+      qf <- rep(-1,numData)
+      
       # Run through each window, stepping forward 1 value each time, evaluating spike test for the central value
       for(idxBgn in 1:(numData-Wndw+1)){
-        
+      
         setMad <- idxBgn:(idxBgn+Wndw-1) # set of indices for the window
         idxMid <- idxBgn+(Wndw-1)/2 # mid point of the window
         dataWndw <- data[setMad]
-        valuMid <- data[idxMid]
-        
+
         # Get rid of NAs
-        data <- data[!base::is.na(data)]
-        numDataWndw <- base::length(data)
+        #dataWndw <- dataWndw[!is.na(dataWndw)]
+        numDataWndw[idxMid] <- sum(!is.na(dataWndw))
           
         # Calculate median absolute deviation over the window
-        med <- stats::median(dataWndw)
-        diffAbs <-base::abs(dataWndw-med)
-        mad <- stats::median(diffAbs) # Median absolute deviation from median
-        
-        numObs <- sum()
-        
-        # Evaluate mad at central point
-        mad > 
-        
-        
-        
-        
+        med[idxMid] <- median(dataWndw,na.rm=TRUE)
+        diffAbs <-abs(dataWndw-med[idxMid])
+        mad[idxMid] <- median(diffAbs,na.rm=TRUE) # Median absolute deviation from median
         
       }
-    
-      # Behavior at the edges (-1?? - yes, bc that's what timeseries padding does)
+      
+      # Compute correction factor to reduce bias with low window size
+      corWndw <- numDataWndw/(numDataWndw-0.8)
+      if(base::min(numDataWndw,na.rm=TRUE) < 10){
+        corWndw[numDataWndw == 9] <- 1.107
+        corWndw[numDataWndw == 8] <- 1.129
+        corWndw[numDataWndw == 7] <- 1.140
+        corWndw[numDataWndw == 6] <- 1.200
+        corWndw[numDataWndw == 5] <- 1.206
+        corWndw[numDataWndw == 4] <- 1.363
+      }
+        
+      # Adjust for window size, stardard deviation approx, and threshold factor
+      madAdj <- mad*corWndw*CorStd*ThshMad 
+      
+      # Evaluate central point of each window for spike
+      setEval <- numDataWndw >= 4 & numDataWndw >= (1-NaFracMax)*Wndw # center indices with enough non-NA data points in the window
+      setSpkFail <- setEval & data < med-madAdj | data > med+madAdj
+      setSpkPass <- setEval & !setSpkFail
+      
+      # Set the pass & fail flags (all others are -1)
+      qf[setSpkFail] <- 1
+      qf[setSpkPass] <- 0
+
+      # Set a fail to a pass if there are more than NumGrp consective fails (suggesting a real data pattern)
+      diff(qf,differences=NumGrp)
+      
+      qfGrp <- utils::head(qf,-(NumGrp-1))
+      for(idx in 2:NumGrp){
+       qfGrp <- qfGrp + qf[idx:(numData-NumGrp+idx)]
+      }
+      
+      setFix <- which(qfGrp == NumGrp)
+      for(idxFix in setFix){
+        qf[idxFix:min(idxFix+NumGrp-1,numData)] <- 0
+      }
       
     } # End Method A
     
@@ -112,3 +143,7 @@ def.dspk.mad <-
     
     return(qf)
   }
+
+# timeBgn <- Sys.time()
+# X <- def.dspk.mad(data=data,Meth='A',ThshMad=7,Wndw=3601,WndwStep=1,NumGrp=4,NaFracMax=1,log=log)
+# print(Sys.time()-timeBgn)
