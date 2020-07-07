975598200bca2eb4611f0dd98b9a662f4744ce65
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-07-07 15:57:21 -0600

    Added test cases for "def.data.mapp.schm.avro.R"

--- pack/NEONprocIS.base/tests/testthat/test-def.data.mapp.schm.avro.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.data.mapp.schm.avro.R
@@ -1,40 +1,55 @@
-library(testthat)
-source("R/def.data.mapp.schm.avro.R")
+#library(testthat)
+#source("R/def.data.mapp.schm.avro.R")
 
-test_that("make sure missing data column is added",
+test_that("make sure missing data column are added",
           {
-            
-            inputfilepaths <- c('def.rcd.miss.na/valid_files/testdata.parquet', 'def.rcd.miss.na/valid_files/testflagsdata.parquet')
-            
-            returnList <-NEONprocIS.base::def.data.mapp.schm.avro(fileData=inputfilepaths)
-            testthat::expect_true(is.list(returnList))
-            expect_true (length(returnList) == 2)
-            testthat::equals(length(returnList$timeAll$readout_time), 6)
-            testthat::equals(returnList$timeBad$readout_time[1], "2019-01-01 07:00:05 GMT")
-            
+            data <- data.frame("source_id"=c(1,2,3), "site_id"=c('one','two','three'), stringsAsFactors=FALSE)
+            FileSchm <- "def.schm.avro.pars/prt_calibrated.avsc"
+            schm <- base::paste0(base::readLines(FileSchm),collapse='')
+
+            returnData <- NEONprocIS.base::def.data.mapp.schm.avro(data = data, schm = paste(unlist(schm), collapse=''))
+            testthat::expect_true(is.list(returnData))
+            testthat::expect_true(length(returnData) == 4)
+            testthat::equals(colnames(returnData), c("source_id","site_id", "readout_time", "temp"))
+
           }
-          
 )
 
 test_that("Data type is converted when ConvType is sent as true",
           {
-            
-            inputfilepaths <- c('def.rcd.miss.na/second_test/textformat.txt', 'def.rcd.miss.na/valid_files/testflagsdata.parquet')
-            returnList <-try(NEONprocIS.base::def.data.mapp.schm.avro(fileData=inputfilepaths),
-                             silent = TRUE)
-            testthat::expect_true((class(returnList)[1] == "try-error"))
-          }
-          
+
+            timestamp1 = base::as.POSIXct('2019-01-01',tz='GMT')
+            timestamp2 = base::as.POSIXct('2019-01-02',tz='GMT')
+            timestamp3 = base::as.POSIXct('2019-01-03',tz='GMT')
+            data <- data.frame("source_id"=c(1,2,3), "site_id"=c('one','two','three'), "temp" = c("10", "20", "30"),
+                               "readout_time" = c(timestamp1, timestamp2, timestamp3), stringsAsFactors=FALSE)
+            FileSchm <- "def.schm.avro.pars/prt_calibrated.avsc"
+            schm <- base::paste0(base::readLines(FileSchm),collapse='')
+
+            returnData <- NEONprocIS.base::def.data.mapp.schm.avro(data = data, schm = schm, ConvType=TRUE)
+            testthat::expect_true(is.list(returnData))
+            testthat::expect_true(length(returnData) == 4)
+            testthat::equals(colnames(returnData), c("source_id","site_id", "readout_time", "temp"))
+            testthat::equals(typeof(returnData$source_id, "character"))
+            testthat::equals(class(returnData$temp, "numeric"))
+            testthat::equals(class(returnData$readout_time, "long"))
+
+           }
 )
 
 test_that("make sure data is not converted when ConvType is not passed as a parameter",
           {
+            data <- data.frame("source_id"=c(1,2,3), "site_id"=c('one','two','three'), "temp" = c("10", "20", "30"), stringsAsFactors=FALSE)
+            FileSchm <- "def.schm.avro.pars/prt_calibrated.avsc"
+            schm <- base::paste0(base::readLines(FileSchm),collapse='')
             
-            inputfilepaths <- c('def.rcd.miss.na/second_test/textformat.txt', 'def.rcd.miss.na/valid_files/testflagsdata.parquet')
-            returnList <-try(NEONprocIS.base::def.data.mapp.schm.avro(fileData=inputfilepaths),
-                             silent = TRUE)
-            testthat::expect_true((class(returnList)[1] == "try-error"))
+            returnData <- NEONprocIS.base::def.data.mapp.schm.avro(data = data, schm = paste(unlist(schm), collapse=''))
+            testthat::expect_true(is.list(returnData))
+            testthat::expect_true(length(returnData) == 4)
+            testthat::equals(colnames(returnData), c("source_id","site_id", "readout_time", "temp"))
+            testthat::equals(typeof(returnData$source_id, "numeric"))
+            testthat::equals(class(returnData$temp, "character"))
           }
-          
+
 )
 
