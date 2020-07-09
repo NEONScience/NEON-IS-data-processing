2f42c2d27413813e835855218eba8b01ebfcb4e4
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-07-09 10:36:01 -0600

    more test cases

--- pack/NEONprocIS.base/tests/testthat/test-def.read.avro.deve.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.read.avro.deve.R
@@ -1,55 +1,24 @@
-library(testthat)
-source("R/def.read.avro.deve.R")
+#library(testthat)
+#source("R/def.read.avro.deve.R")
 
 test_that("when more than one input is sent as an input, consider just the first one",
            {
-             NameFile <- c("/home/NEON/vchundru/gitRepo/NEON-IS-data-processing/pack/NEONprocIS.base/tests/testthat/def.schm.avro.pars/prt_calibrated.avsc", "~/gitRepo/NEON-IS-data-processing/pack/NEONprocIS.base/tests/testthat/def.read.avro.deve/prt_calibrated_2.avsc")
-             rpt <- try(NEONprocIS.base::def.read.avro.deve(NameFile=NameFile, NameLib = "~/gitRepo/NEON-IS-data-processing/pack/NEONprocIS.base/ravro.so"), silent = TRUE)
+             NameFile <- c("tests/testthat/def.read.avro.deve/prt_test.avro", "tests/testthat/def.read.avro.deve/prt_calibrated_2.avsc")
+             rpt <- try(NEONprocIS.base::def.read.avro.deve(NameFile=NameFile, NameLib = "ravro.so"), silent = TRUE)
              testthat::expect_false((class(rpt)[1] == "try-error"))
+             testthat::equals(length(rpt), 4 )
 
            })
 
-# test_that("when listVect length is less than 2, throw an error",
-#           {#             listVect <- list(c('key1'))
-#             NameList <- c('MyKeys','MyValues')
-#             Type <- c('character','numeric')
-#             rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
-#             testthat::expect_true((class(rpt)[1] == "try-error"))
-# 
-#           })
-# 
-# test_that("when type is not 2, throw an error",
-#           {
-#             listVect <- c('key1','1.5')
-#             NameList <- c('MyKeys','MyValues')
-#             Type <- c('character')
-#             rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
-#             testthat::expect_true((class(rpt)[1] == "try-error"))
-# 
-#           })
-# 
-# 
-# test_that("when NameList length is not 2, log an error but proceeds",
-#            {
-#               listVect <- list(c('key1', 'key2'))
-#               NameList <- c('MyKeys','MyValues', 'wrongvalue')
-#               Type <- c('character','numeric')
-#               rpt <- try(NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type), silent = TRUE)
-#               testthat::expect_true((class(rpt)[1] == "try-error"))
-# 
-#            })
-# 
-# test_that("when all the values are passed, returns values",
-#           {
-#               listVect <- list(c('key1','1.5','3','4','5'),c('key2','3'))
-#               NameList <- c('MyKeys','MyValues')
-#               Type <- c('character','numeric')
-#               rpt <- NEONprocIS.base::def.vect.pars.one.many(listVect=listVect,NameList=NameList,Type=Type)
-#               testthat::expect_equal(2, length(rpt))
-#               testthat::expect_equal(rpt[[1]]$MyKeys, 'key1')
-#               testthat::expect_equal(rpt[[1]]$MyValues[1], 1.5)
-#               testthat::expect_equal(rpt[[1]]$MyValues[4], 5.0)
-#               testthat::expect_equal(rpt[[2]]$MyKeys, 'key2')
-#               testthat::expect_equal(rpt[[2]]$MyValues[1], 3.0)
-# 
-#           })
+ test_that("check data types of the reutrn list",
+           {
+             NameFile <- c("~/gitRepo/NEON-IS-data-processing/pack/NEONprocIS.base/tests/testthat/def.read.avro.deve/prt_test.avro", "tests/testthat/def.read.avro.deve/prt_calibrated_2.avsc")
+             rpt <- try(NEONprocIS.base::def.read.avro.deve(NameFile=NameFile, NameLib = "ravro.so"), silent = TRUE)
+             testthat::expect_false((class(rpt)[1] == "try-error"))
+             testthat::equals(length(rpt), 4 )
+             #testthat::equals(class(rpt$source_id), "character" )
+             testthat::equals(class(rpt$source_id), "string" )
+             testthat::equals(class(rpt$readout_time), "POSIXct" )
+             testthat::equals(class(rpt$resistance), "numeric" )
+
+     })
