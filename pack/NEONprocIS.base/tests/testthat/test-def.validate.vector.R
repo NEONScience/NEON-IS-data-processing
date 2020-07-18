bc50a1ba9245e5df0004a9267c3815b6ab1faac5
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-07-18 13:24:10 -0600

    bug fix

--- pack/NEONprocIS.base/tests/testthat/test-def.validate.vector.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.validate.vector.R
@@ -4,27 +4,27 @@
 test_that("if not vector, return falseh",
           {
             inputList <- list("a" = "2.5", "b" = TRUE, "c" = 1:3)
-            returnValue <- def.validate.vector(vectIn = inputList, TestEmpty=FALSE, TestNumc=FALSE)
+            returnValue <- NEONprocIS.base::def.validate.vector(vectIn = inputList, TestEmpty=FALSE, TestNumc=FALSE)
             testthat::expect_false(returnValue)
           })
 
 test_that("vector elements are not numeric and TestNumc is sent as True, return false",
           {
             inputList <- c("test1", "test2", "test3" )
-            returnValue <- def.validate.vector(vectIn = inputList, TestEmpty=FALSE, TestNumc=TRUE)
+            returnValue <- NEONprocIS.base::def.validate.vector(vectIn = inputList, TestEmpty=FALSE, TestNumc=TRUE)
             testthat::expect_false(returnValue)
           })
 
 test_that("empty vector and TestEmpty is sent as true, return false",
           {
             inputVect <- numeric(0)
-            returnValue <- def.validate.vector(vectIn = inputVect, TestEmpty=TRUE, TestNumc=FALSE)
+            returnValue <- NEONprocIS.base::def.validate.vector(vectIn = inputVect, TestEmpty=TRUE, TestNumc=FALSE)
             testthat::expect_false(returnValue)
           })
 
 test_that("Non empty vector and TestEmpty is sent as true and TestNumc is sent as true, return true",
           {
             inputVect <- c(1, 2, 3)
-            returnValue <- def.validate.vector(vectIn = inputVect, TestEmpty=TRUE, TestNumc=TRUE)
+            returnValue <- NEONprocIS.base::def.validate.vector(vectIn = inputVect, TestEmpty=TRUE, TestNumc=TRUE)
             testthat::expect_true(returnValue)
           })
