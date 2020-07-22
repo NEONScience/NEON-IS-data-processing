9f6aea05a8124d523a6e30dc42823e6f803e7db6
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-07-22 13:31:19 -0600

    Added test cases for "def.validate.xml.schema.R"

--- pack/NEONprocIS.base/tests/testthat/test-def.validate.xml.schema.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.validate.xml.schema.R
@@ -1,67 +1,23 @@
-library(testthat)
-source("R/def.validate.xml.schema.R")
+#library(testthat)
+#source("R/def.validate.xml.schema.R")
 
-test_that("retrun extra timestamp in the second file",
+test_that("valid xml should return true",
           {
-
-            inputfilepaths <- c('def.rcd.miss.na/valid_files/testdata.parquet', 'def.rcd.miss.na/valid_files/testflagsdata.parquet')
-
-            returnList <-NEONprocIS.base::def.rcd.miss.na(fileData=inputfilepaths)
-            testthat::expect_true(is.list(returnList))
-            expect_true (length(returnList) == 2)
-            testthat::equals(length(returnList$timeAll$readout_time), 6)
-            testthat::equals(returnList$timeBad$readout_time[1], "2019-01-01 07:00:05 GMT")
-
+            xmlpath <- 'def.validate.xml.schema/calibration.xml'
+            schemapath <- 'def.validate.xml.schema/calibration.xsd'
+            returnData <- NEONprocIS.base::def.validate.xml.schema(xmlIn = xmlpath, xmlSchemaIn = schemapath)
+            testthat::expect_true(returnData)
           }
 
 )
 
-test_that("wrong file format",
+test_that("invalid xml should return false",
           {
-
-            inputfilepaths <- c('def.rcd.miss.na/second_test/textformat.txt', 'def.rcd.miss.na/valid_files/testflagsdata.parquet')
-            returnList <-try(NEONprocIS.base::def.rcd.miss.na(fileData=inputfilepaths),
-                             silent = TRUE)
-            testthat::expect_true((class(returnList)[1] == "try-error"))
+            xmlpath <- 'def.validate.xml.schema/invalid_calibration.xml'
+            schemapath <- 'def.validate.xml.schema/calibration.xsd'
+            returnData <- NEONprocIS.base::def.validate.xml.schema(xmlIn = xmlpath, xmlSchemaIn = schemapath)
+            testthat::expect_false(returnData)
           }
 
 )
 
-test_that("missing readout_time in one of the files",
-          {
-
-            inputfilepaths <- c('def.rcd.miss.na/third_test/incorrecttestdata.parquet', 'def.rcd.miss.na/valid_files/testflagsdata.parquet')
-            returnList <-try(NEONprocIS.base::def.rcd.miss.na(fileData=inputfilepaths),
-                                    silent = TRUE)
-            testthat::expect_true((class(returnList)[1] == "try-error"))
-          }
-)
-
-test_that("extra timestampe in the second file",
-          {
-
-            inputfilepaths <- c( 'def.rcd.miss.na/valid_files/testflagsdata.parquet', 'def.rcd.miss.na/valid_files/testdata.parquet')
-
-            returnList <-NEONprocIS.base::def.rcd.miss.na(fileData=inputfilepaths)
-            testthat::expect_true(is.list(returnList))
-            expect_true (length(returnList) == 2)
-            testthat::equals(length(returnList$timeAll$readout_time), 6)
-            testthat::equals(returnList$timeBad$readout_time[1], "2019-01-01 07:00:05 GMT")
-
-          }
-)
-
-test_that("NA in one of the columns",
-           {
-
-             inputfilepaths <- c( 'def.rcd.miss.na/NA_test/testdataWithNA.parquet', 'def.rcd.miss.na/valid_files/testdata.parquet')
- 
-             returnList <-NEONprocIS.base::def.rcd.miss.na(fileData=inputfilepaths)
-             testthat::expect_true(is.list(returnList))
-             expect_true (length(returnList) == 2)
-             testthat::equals(length(returnList$timeAll$readout_time), 6)
-             testthat::equals(returnList$timeBad$readout_time[1], "2019-01-01 00:00:02 GMT")
- 
-           }
-
-)
