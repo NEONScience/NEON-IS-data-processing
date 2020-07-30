a05939a7640388bd592e58c071bf9fa50cc97e05
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-07-30 13:26:33 -0600

    improved test coverage

--- pack/NEONprocIS.base/tests/testthat/test-def.time.bin.diff.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.time.bin.diff.R
@@ -1,27 +1,27 @@
-library(testthat)
-source("R/def.time.bin.diff.R")
+#library(testthat)
+#source("R/def.time.bin.diff.R")
 
-# test_that("retrun extra timestamp in the second file",
-#           {
-#             WndwBin <- base::as.difftime(30,units="mins")
-#             WndwTime <- base::as.difftime(1,units='days')
-#           
-#             returnList <-NEONprocIS.base::def.time.bin.diff(WndwBin = WndwBin,
-#                                                             WndwTime = WndwTime)
-#             testthat::expect_true(is.list(returnList))
-#             expect_true (length(returnList) == 2)
-#             testthat::equals(returnList$timeBgnDiff[1], 0)
-#             testthat::equals(returnList$timeBgnDiff[48], 84600)
-#             testthat::equals(length(returnList$timeBgnDiff), 48)
-#             testthat::equals(returnList$timeEndDiff[1], 30)
-#             testthat::equals(returnList$timeEndDiff[24], 720)
-#             testthat::equals(length(returnList$timeEndDiff), 48)
-#             
-#           }
-# 
-# )
+test_that("divide one day into 30 minutes",
+          {
+            WndwBin <- base::as.difftime(30,units="mins")
+            WndwTime <- base::as.difftime(1,units='days')
 
-test_that("retrun extra timestamp in the second file",
+            returnList <-NEONprocIS.base::def.time.bin.diff(WndwBin = WndwBin,
+                                                            WndwTime = WndwTime)
+            testthat::expect_true(is.list(returnList))
+            expect_true (length(returnList) == 2)
+            testthat::equals(returnList$timeBgnDiff[1], 0)
+            testthat::equals(returnList$timeBgnDiff[48], 84600)
+            testthat::equals(length(returnList$timeBgnDiff), 48)
+            testthat::equals(returnList$timeEndDiff[1], 30)
+            testthat::equals(returnList$timeEndDiff[24], 720)
+            testthat::equals(length(returnList$timeEndDiff), 48)
+
+          }
+
+)
+
+test_that("divide one day into 1 hour bins",
           {
             WndwBin <- base::as.difftime(1,units="hours")
             WndwTime <- base::as.difftime(1,units='days')
@@ -40,4 +40,17 @@ test_that("retrun extra timestamp in the second file",
 
           }
 
+)
+
+test_that("divide one day into 37 minutes bins",
+          {
+            WndwBin <- base::as.difftime(37,units="mins")
+            WndwTime <- base::as.difftime(1,units='days')
+            
+            returnList <- try(NEONprocIS.base::def.time.bin.diff(WndwBin = WndwBin,
+                                                            WndwTime = WndwTime), silent = TRUE)
+            testthat::expect_true((class(returnList)[1] == "try-error"))
+            
+          }
+          
 )
