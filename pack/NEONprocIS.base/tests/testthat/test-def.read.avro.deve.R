cf7bba81fd54d4eebdf1704c311a00f1a02c14f5
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-08-04 16:19:26 -0600

    bug fixes

--- pack/NEONprocIS.base/tests/testthat/test-def.read.avro.deve.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.read.avro.deve.R
@@ -1,23 +1,22 @@
-library(testthat)
-source("R/def.read.avro.deve.R")
+#library(testthat)
+#source("R/def.read.avro.deve.R")
 
 test_that("when more than one input is sent as an input, consider just the first one",
            {
-             NameFile <- c("tests/testthat/def.read.avro.deve/prt_test.avro", "tests/testthat/def.read.avro.deve/prt_calibrated_2.avsc")
-             rpt <- try(NEONprocIS.base::def.read.avro.deve(NameFile=NameFile, NameLib = "ravro.so"), silent = TRUE)
+             nameFile <- c('tests/testthat/def.read.avro.deve/prt_test.avro', 'def.read.avro.deve/prt_calibrated_2.avsc')
+             rpt <- try(NEONprocIS.base::def.read.avro.deve(NameFile = nameFile, NameLib = "ravro.so"), silent = TRUE)
              testthat::expect_false((class(rpt)[1] == "try-error"))
-             testthat::equals(length(rpt), 4 )
-
+            
            })
 
  test_that("check data types of the reutrn list",
            {
-             NameFile <- c("tests/testthat/def.read.avro.deve/prt_test.avro")
-             rpt <- try(NEONprocIS.base::def.read.avro.deve(NameFile=NameFile, NameLib = "ravro.so"), silent = TRUE)
+             nameFile <- c('tests/testthat/def.read.avro.deve/prt_test.avro')
+             rpt <- try(NEONprocIS.base::def.read.avro.deve(NameFile = nameFile, NameLib = "ravro.so"), silent = TRUE)
              testthat::expect_false((class(rpt)[1] == "try-error"))
              testthat::equals(length(rpt), 4 )
-             #testthat::equals(class(rpt$source_id), "character" )
-             testthat::equals(class(rpt$source_id), "string" )
+             testthat::equals(class(rpt$source_id), "character" )
+             testthat::equals(class(rpt$site_id), "character" )
              testthat::equals(class(rpt$readout_time), "POSIXct" )
              testthat::equals(class(rpt$resistance), "numeric" )
 
