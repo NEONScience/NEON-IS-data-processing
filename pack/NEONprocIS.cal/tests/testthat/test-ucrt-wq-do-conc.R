c4e5a06365c37e6bcc7c3c060b7f8e370eb1a563
Mija Choi <choim@battelleecology.org>
2020-08-05 11:58:50 -0600

    final check in

--- pack/NEONprocIS.cal/tests/testthat/test-ucrt-wq-do-conc.R
+++ pack/NEONprocIS.cal/tests/testthat/test-ucrt-wq-do-conc.R
@@ -2,7 +2,7 @@
 #' @title Unit test of def.ucrt.wq.do.conc.R
 
 #' @description
-#' Run unit tests for def.ucrt.wq.do.conc.R, 
+#' Run unit tests for def.ucrt.wq.do.conc.R,
 #'            "Uncertainty for dissolved oxygen (DO) concentration (mg/L) as part of the water"
 #' The tests include positive and negative scenarios.
 #' The positive test is for a case when all the params to the function are valid
@@ -21,7 +21,7 @@
 #' created and used within the function.
 
 #' @return dataframe with L0 uncertatinty column(s) [dataframe]
-#' 
+#'
 #' Written to potentially plug in to def.cal.conv.R
 #' ucrt <- def.ucrt.wq.do.conc(data = data, cal = NULL)
 
@@ -38,7 +38,7 @@
 #' To run with testthat:
 #' devtools::test(pkg="<path>/NEON-IS-data-processing/pack/NEONprocIS.cal")
 #' an example, devtools::test(pkg="C:/projects/NEON-IS-data-processing/pack/NEONprocIS.cal")
-#' 
+#'
 
 # changelog and author contributions / copyrights
 #   Mija Choi (2020-08-03)
@@ -49,16 +49,16 @@ context("\n                       Unit test of def.ucrt.wq.do.conc.R\n")
 
 # Unit test of def.ucrt.wq.do.conc.R
 test_that("Unit test of def.ucrt.wq.do.conc.R", {
-  # Happy Path 
+  # Happy Path
   
   ### output = 0.01 if DO is > 0 & <= 20 mg/l according to the manual
   ### output = 0.05 if DO is >20 mg/l & < 50 mg/l according to the manual
-
+  
   data = c(-5, 25, 20, 30, 10, 35)
   out_Data = c(0.01, 0.05, 0.01, 0.05, 0.01, 0.05)
   
   col_List = c("ucrtPercent", "ucrtMeas")
- 
+  
   outputDF_returned <-
     NEONprocIS.cal::def.ucrt.wq.do.conc (data = data, log = NULL)
   
@@ -66,13 +66,14 @@ test_that("Unit test of def.ucrt.wq.do.conc.R", {
                  !(is.null(outputDF_returned)))
   # columns returned are ucrtPercent and ucrtMeas
   expect_true (all (names(outputDF_returned) == col_List) &&
-                  all(outputDF_returned$ucrtMeas == out_Data*data))
+                 all(outputDF_returned$ucrtMeas == out_Data * data))
   
   # Sad path - data is NULL
   
   # The error will be returned since empty data is not a valid input to def.ucrt.wq.do.conc.R
- 
+  
   data = c()
-  outputDF_returned <- try (NEONprocIS.cal::def.ucrt.wq.do.conc (data = data), silent = TRUE) 
-  testthat::expect_true((class(outputDF_returned)[1] == "try-error")) 
+  outputDF_returned <-
+    try (NEONprocIS.cal::def.ucrt.wq.do.conc (data = data), silent = TRUE)
+  testthat::expect_true((class(outputDF_returned)[1] == "try-error"))
 })
