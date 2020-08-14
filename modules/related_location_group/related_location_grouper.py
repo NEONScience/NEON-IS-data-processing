1ff234691684baa61268820d93a177326d637496
Robert Markel <rmarkel@battelleecology.org>
2020-08-14 15:53:22 -0600

    Modified to include directories without files in the output.

--- modules/related_location_group/related_location_grouper.py
+++ modules/related_location_group/related_location_grouper.py
@@ -19,8 +19,14 @@ class RelatedLocationGrouper:
     def group_files(self) -> None:
         """Link related data and location files into the output path."""
         for path in self.data_path.rglob('*'):
-            if path.is_file():
-                source_type, year, month, day, group, location, data_type, remainder = self.data_path_parser.parse(path)
+            # create data type directories as they may be empty
+            if path.is_dir() and len(path.parts) - 1 == self.data_path_parser.max_value:
+                source_type, year, month, day, group, location, data_type = self.data_path_parser.parse_dir(path)
+                output_path = Path(self.out_path, year, month, day, group, source_type, location, data_type)
+                output_path.mkdir(parents=True, exist_ok=True)
+            elif path.is_file():
+                source_type, year, month, day, group, location, data_type, remainder = \
+                    self.data_path_parser.parse_file(path)
                 link_path = Path(self.out_path, year, month, day, group, source_type, location, data_type, *remainder)
                 link_path.parent.mkdir(parents=True, exist_ok=True)
                 if not link_path.exists():
