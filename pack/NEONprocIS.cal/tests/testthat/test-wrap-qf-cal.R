f711881b9334fbcc7423cfdb135d208935e0c311
Mija Choi <choim@battelleecology.org>
2020-09-02 09:38:36 -0600

    unit test for wrap-qf-cal.R 1st check in

---
+++ pack/NEONprocIS.cal/tests/testthat/test-wrap-qf-cal.R
@@ -0,0 +1,125 @@
+##############################################################################################
+#' @title Unit test for wrap-qf-cal.R, wrapper for computing calibration flags for all variables and time ranges
+#' 
+#' @author
+#' Mija Choi \email{choim@battelleecology.org}
+#' 
+#' @description
+#' wrap-qf-cal.R: Wrapper function. Compute valid calibration and suspect calibration flags for NEON L0 data. 
+#' 
+#' @param data Data frame of L0 data. Must include POSIXct time variable readout_time.  
+#' @param calSlct A named list of data frames, list element corresponding to the variable for which
+#' uncertainty coefficients are to be compiled. The data frame in each list element holds 
+#' information about the calibration files and time periods that apply to the variable, as returned 
+#' from NEONprocIS.cal::def.cal.slct. See documentation for that function. 
+#' @param DirCal Character string. Relative or absolute path (minus file name) to the main calibration
+#' directory. Nested within this directory are directories for each variable in calSlct, each holding
+#' calibration files for that variable. Defaults to "./"
+#' @param mappNameVar A data frame with in/out variable name mapping as produced by 
+#' NEONprocIS.base::def.var.mapp.in.out. See documentation for that function.   
+#' @param log A logger object as produced by NEONprocIS.base::def.log.init to produce structured log
+#' output. Defaults to NULL, in which the logger will be created and used within the function.
+#' 
+#' @return A named list of qfExpi and qfSusp, each holding data frames with the same dimension as data, 
+#' with the exception that the variable readout_time is removed.\cr
+#' \code{qfExpi} Integer. The expired/valid calibration flag. 0 = valid, non expired calibration available; 
+#' 1 = no calibration or expired calibration available. \cr
+#' \code{qfSusp} Integer. The suspect calibration flag. 0 = calibration not suspect, 1 = calibration suspect, 
+#' -1 = no cal to evaluate 
+
+#' @references Currently none
+
+#' @keywords Currently none
+
+#' @examples Currently none
+
+#' @seealso \link[NEONprocIS.cal]{def.cal.slct}
+#' @seealso \link[NEONprocIS.cal]{def.read.cal.xml}
+#' @seealso \link[NEONprocIS.cal]{def.qf.cal.susp}
+#' 
+#' @examples
+#' To run with testthat:
+#' devtools::test(pkg="<path>/NEON-IS-data-processing/pack/NEONprocIS.cal")
+#' an example, devtools::test(pkg="C:/projects/NEON-IS-data-processing/pack/NEONprocIS.cal")
+#' 
+# changelog and author contributions / copyrights
+#   Mija Choi (2020-00-02)
+#     Original Creation
+##############################################################################################
+# Define test context
+context("\n                       Unit test of def-qf-cal-susp.R\n")
+
+# Unit test of def-qf-cal-susp.R
+test_that("Unit test of def-qf-cal-susp.R", {
+   testDir = "testdata/"
+   
+   testData = "L0_data.csv"
+   testDataPath <- paste0(testDir, testData)
+   
+   data0 <- read.csv(testDataPath, sep = ",", header = TRUE)
+   
+   data <- data0$resistance
+   
+   # Happy path 1 - Check the CVALR1 coefficient in the calibration file. If it is present,
+   # set the suspect calibration flag for all data values to the value of the coefficient
+   # (1=bad,0=good).
+   
+   testFileCal = "calibration2.xml"
+   testFileCalPath <- paste0(testDir, testFileCal)
+   
+   infoCal <- NEONprocIS.cal::def.read.cal.xml (testFileCalPath, Vrbs = TRUE)
+   
+   DirCal = "./calibrations"
+   NameVarExpc = character(0)
+   
+   # Test 1 There are calibration files available within the time range
+   TimeBgn = base::as.POSIXct('2019-06-12 00:10:20', tz = 'GMT')
+   TimeEnd = base::as.POSIXct('2019-07-07 00:18:28', tz = 'GMT')
+   
+   varCal <- base::unique(c(NameVarExpc, base::dir(DirCal)))
+   values <- c(10, 13)
+   
+   NumDayExpiMax <- data.frame(var = varCal, NumDayExpiMax = values, stringsAsFactors = FALSE)
+   #
+   calSlct <-
+      NEONprocIS.cal::wrap.cal.slct (
+         DirCal = DirCal,
+         NameVarExpc = character(0),
+         TimeBgn = TimeBgn,
+         TimeEnd = TimeEnd,
+         NumDayExpiMax = NumDayExpiMax,
+         log = NULL
+      )
+   
+   
+   qfSusp <- NEONprocIS.cal::def.qf.cal.susp(data, infoCal)
+   
+   expect_true (all(qfSusp == 1))
+   
+   # Happy path 2 -  If the CVALR1 coefficient is not present, set the suspect calibration flag
+   # to 0.
+   
+   testFileCal = "calibration4.xml"
+   testFileCalPath <- paste0(testDir, testFileCal)
+   
+   infoCal <- NEONprocIS.cal::def.read.cal.xml (testFileCalPath, Vrbs = TRUE)
+   
+   qfSusp <- NEONprocIS.cal::def.qf.cal.susp(data, infoCal)
+   
+   expect_true (all(qfSusp) == 0)
+   
+   # Happy path 3 - If no calibration information is available, set the flag to -1.
+   
+   qfSusp <- NEONprocIS.cal::def.qf.cal.susp(data)
+   
+   expect_true (all(qfSusp == -1))
+ 
+   # Happy path 4 - If data is not a vector, errors out.
+   
+   data <- list(data)
+   
+   qfSusp <- try(NEONprocIS.cal::def.qf.cal.susp(data, infoCal), silent = TRUE)
+   
+   expect_true(class(qfSusp)[1] == "try-error")
+   
+})
