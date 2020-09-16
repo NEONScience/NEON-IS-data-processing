36ead193cc6fbe7d1e3ddfba6ab22a7eebe58024
visalachundru <vchundru@den-devissom.ci.neoninternal.org>
2020-09-16 12:49:00 -0600

    adding test data and test cases for def.thsh.slct.R

--- pack/NEONprocIS.qaqc/tests/testthat/test-def.thsh.slct.R
+++ pack/NEONprocIS.qaqc/tests/testthat/test-def.thsh.slct.R
@@ -33,12 +33,11 @@ test_that("when there are two thresholds, just consider first one",
             rpt <- NEONprocIS.qaqc::def.thsh.slct(thsh = inputThsh, Time = time1, Site="ABBY", Term="TFPrecipBulk", NameLoc="ABBY", RptThsh = FALSE)
             testthat::expect_false(is.list(rpt))
             testthat::equals(rpt, 1)
-           
-          }
-          
-)
 
-test_that("when there are two thresholds, just consider first one",
+          }
+
+)
+test_that("For SENSOR & DOY-specific when there are two thresholds, just consider first one",
           {
             #thsh, Time, Term, Ctxt = NULL, Site=NULL, NameLoc=NULL, RptThsh = TRUE, log = NULL
             inputThsh <- NEONprocIS.qaqc::def.read.thsh.qaqc.list(NameFile = 'def.read.thsh.qaqc.df/thresholds2.json')
@@ -46,7 +45,66 @@ test_that("when there are two thresholds, just consider first one",
             rpt <- NEONprocIS.qaqc::def.thsh.slct(thsh = inputThsh, Time = time1, Site="ABBY", Term="TFPrecipBulk", NameLoc="ABBY", RptThsh = FALSE)
             testthat::expect_false(is.list(rpt))
             testthat::equals(rpt, 1)
-            
+
           }
-          
+
+)
+test_that("For SENSOR-specific",
+          {
+            #thsh, Time, Term, Ctxt = NULL, Site=NULL, NameLoc=NULL, RptThsh = TRUE, log = NULL
+            inputThsh <- NEONprocIS.qaqc::def.read.thsh.qaqc.list(NameFile = 'def.read.thsh.qaqc.df/thresholds2.json')
+            time1 <- base::as.POSIXct('2000-01-10 00:00:00', tz = 'GMT')
+            rpt <- NEONprocIS.qaqc::def.thsh.slct(thsh = inputThsh, Time = time1, Site="ABBY", Term="rawVSWC7", NameLoc="REALM")
+            testthat::expect_true(is.list(rpt))
+            expect_true(length(rpt) == 1)
+            testthat::equals(rpt[1]$start_date, "2000-01-01T00:00:00Z")
+            testthat::equals(rpt[1]$location_name, "REALM")
+            testthat::equals(rpt[1]$number_value, 1.2)
+
+          }
+)
+
+test_that("For SENSOR-specific, when there are more than 2 thresholds, use first one",
+          {
+            #thsh, Time, Term, Ctxt = NULL, Site=NULL, NameLoc=NULL, RptThsh = TRUE, log = NULL
+            inputThsh <- NEONprocIS.qaqc::def.read.thsh.qaqc.list(NameFile = 'def.read.thsh.qaqc.df/thresholds2.json')
+            time1 <- base::as.POSIXct('2000-01-10 00:00:00', tz = 'GMT')
+            rpt <- NEONprocIS.qaqc::def.thsh.slct(thsh = inputThsh, Time = time1, Term="rawVSWC9", NameLoc="REALM")
+            testthat::expect_true(is.list(rpt))
+            expect_true(length(rpt) == 1)
+            testthat::equals(rpt[1]$start_date, "2000-01-01T00:00:00Z")
+            testthat::equals(rpt[1]$location_name, "REALM")
+            testthat::equals(rpt[1]$number_value, 1.5)
+
+          }
+)
+test_that("For SITE & DOY-specific",
+          {
+            #thsh, Time, Term, Ctxt = NULL, Site=NULL, NameLoc=NULL, RptThsh = TRUE, log = NULL
+            inputThsh <- NEONprocIS.qaqc::def.read.thsh.qaqc.list(NameFile = 'def.read.thsh.qaqc.df/thresholds3.json')
+            time1 <- base::as.POSIXct('2000-01-10 00:00:00', tz = 'GMT')
+            rpt <- NEONprocIS.qaqc::def.thsh.slct(thsh = inputThsh, Time = time1, Term="TBPrecipBulk", Site="PUMA")
+            testthat::expect_true(is.list(rpt))
+            expect_true(length(rpt) == 1)
+            testthat::equals(rpt[1]$start_date, "2000-01-01T00:00:00Z")
+            testthat::equals(rpt[1]$location_name, "PUMA")
+            testthat::equals(rpt[1]$number_value, 20)
+            testthat::equals(rpt[1]$end_day_of_year, 15)
+
+          }
+)
+test_that("For SITE specific, when there are more than 2 thresholds, use first one",
+          {
+            #thsh, Time, Term, Ctxt = NULL, Site=NULL, NameLoc=NULL, RptThsh = TRUE, log = NULL
+            inputThsh <- NEONprocIS.qaqc::def.read.thsh.qaqc.list(NameFile = 'def.read.thsh.qaqc.df/thresholds3.json')
+            time1 <- base::as.POSIXct('2000-01-10 00:00:00', tz = 'GMT')
+            rpt <- NEONprocIS.qaqc::def.thsh.slct(thsh = inputThsh, Time = time1, Term="TCPrecipBulk", Site="CEPR")
+            testthat::expect_true(is.list(rpt))
+            expect_true(length(rpt) == 1)
+            testthat::equals(rpt[1]$start_date, "2000-01-01T00:00:00Z")
+            testthat::equals(rpt[1]$location_name, "CPER")
+            testthat::equals(rpt[1]$number_value, NULL)
+            testthat::equals(rpt[1]$string_value, "test")
+
+          }
 )
