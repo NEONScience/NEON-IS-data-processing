214d8d52f4d95fce397b9a671274cf9595d66263
Mija Choi <choim@battelleecology.org>
2020-10-20 19:58:03 -0600

    test-wq-abs-corr updated and calibration3 updated and one more parquet added

--- pack/NEONprocIS.wq/tests/testthat/test-wq-abs-corr.R
+++ pack/NEONprocIS.wq/tests/testthat/test-wq-abs-corr.R
@@ -40,36 +40,60 @@ context("\n                       Unit test of def.wq.abs.corr.R\n")
 
 # Unit test of def.wq.abs.corr.R
 test_that("Unit test of def.wq.abs.corr.R", {
+  
+  testDir = "testdata/"
+  testFile = "sunav2_File5_01-02.parquet"
+  testFilesPath <- paste0(testDir, testFile)
+  # Get the filenames without path information
+  # nameFileCal <- base::unlist(base::lapply(strsplit(fileCal,'/'),utils::tail,n=1))
+  
+  # calibration has all the required elements including CalibrationTable 
 
-# Validate sunav2 calibration against the xsd  
+  # Validate sunav2 calibration against the xsd
   
   xsd1 <- system.file("extdata", "sunav2_calibration.xsd", package = "NEONprocIS.wq")
   
   calDir = "calibrations/"
-  testFileCal = "sunav2_calibration1.xml"
+  testFileCal = "sunav2_calibration2.xml"
   testFileCalPath <- paste0(calDir, testFileCal)
   NameFile = testFileCalPath
   
-  xmlchk <-
-    try(NEONprocIS.base::def.validate.xml.schema(NameFile, xsd1),
-        silent = TRUE)
+  xmlchk <- try(NEONprocIS.base::def.validate.xml.schema(NameFile, xsd1),silent = TRUE)
   
   if (xmlchk != TRUE) {
     log$error(
-      base::paste0(
-        " ====== def.read.cal.xml will not run due to the error in xml,  ",
-        NameFile
-      )
-    )
-    
+      base::paste0(" ====== def.read.cal.xml will not run due to the error in xml,  ",NameFile))
     base::stop()
   }
   
-  testDir = "testdata/"
-  testFile = "sunav2_File4.parquet"
-  testFilesPath <- paste0(testDir, testFile)
-  # Get the filenames without path information
- # nameFileCal <- base::unlist(base::lapply(strsplit(fileCal,'/'),utils::tail,n=1))
-  NEONprocIS.wq::def.wq.abs.corr(sunav2Filenames=testFilesPath,sunav2CalFilenames=testFileCalPath,log = NULL)
+  wq_abs_corr_returned <- NEONprocIS.wq::def.wq.abs.corr(
+      sunav2Filenames = testFilesPath,
+      sunav2CalFilenames = testFileCalPath,
+      log = NULL
+    )
   
-  })
+  col_List = c("readout_time","Abs_ex","Abs_em","ucrt_A_ex","ucrt_A_em","spectrumCount","fDOMAbsQF")
+  expect_true ((is.data.frame(wq_abs_corr_returned)) && !(is.null(wq_abs_corr_returned)))
+  # columns returned are readout_time, abs_ex, abs_em, ucrt_A_ex, ucrt_A_em, spectrumCount, fDOMAbsQF 
+  expect_true (all (names(wq_abs_corr_returned) ==  col_List))
+  
+  # test case 2 - calibration does not have CalibrationTable
+  testFileCal = "sunav2_noCalTbl_calibration.xml"
+  testFileCalPath <- paste0(calDir, testFileCal)
+  NameFile = testFileCalPath
+  
+  xmlchk <- try(NEONprocIS.base::def.validate.xml.schema(NameFile, xsd1),silent = TRUE)
+  
+  if (xmlchk != TRUE) {
+    log$error(
+      base::paste0(" ====== def.read.cal.xml will not run due to the error in xml,  ",NameFile))
+    base::stop()
+  }
+  
+  wq_abs_corr_returned <- try(NEONprocIS.wq::def.wq.abs.corr(
+      sunav2Filenames = testFilesPath,
+      sunav2CalFilenames = testFileCalPath,
+      log = NULL),silent = FALSE)
+  
+  expect_true(class(wq_abs_corr_returned)[1] == "try-error")
+})
