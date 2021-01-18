e1a5d010c16a8936ac2b95988fa7616b3135302e
Mija Choi <choim@battelleecology.org>
2021-01-18 10:12:12 -0700

    tests dir moved up by 2 levels

--- flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
+++ flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
@@ -10,11 +10,9 @@ context("\n                       Unit test of wrap.loc.repo.strc.R\n")
 # Unit test of wrap.loc.repo.strc.R
 test_that("Unit test of wrap.loc.repo.strc.R", {
 
-  source('C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/wrap.loc.repo.strc.R')
-#  source('../../flow.loc.repo.strc/wrap.loc.repo.strc.R')
+#  source('C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/wrap.loc.repo.strc.R')
+  source('../../wrap.loc.repo.strc.R')
    
-#  source('./flow.loc.repo.strc/wrap.loc.repo.strc.R')
-  
   wlrs_returned <-
     wrap.loc.repo.strc(DirIn="/pfs/tempSoil_context_group/prt/2018/01/01",DirOutBase="/pfs/out",Comb=TRUE)
   
