18f2c3c0983f78a9587ebc62b1493f82e929ad04
Mija Choi <choim@battelleecology.org>
2021-01-18 18:58:42 -0700

    add test locations.json and the test modified

--- flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
+++ flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
@@ -1,4 +1,4 @@
-
+#
 # unit test Flow scripts
 # changelog and author contributions / copyrights
 #   Mija Choi (2021-01-11)
@@ -9,15 +9,18 @@ context("\n                       Unit test of wrap.loc.repo.strc.R\n")
 
 # Unit test of wrap.loc.repo.strc.R
 test_that("Unit test of wrap.loc.repo.strc.R", {
-
-#  source('C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/wrap.loc.repo.strc.R')
-  source('../../wrap.loc.repo.strc.R')
-   
-  wlrs_returned <-
-    wrap.loc.repo.strc(DirIn="/pfs/tempSoil_context_group/prt/2018/01/01",DirOutBase="/pfs/out",Comb=TRUE)
+  #  source('C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/wrap.loc.repo.strc.R')
   
-}
-)
-
-
-
+  source('../../wrap.loc.repo.strc.R')
+  
+  #testInputDir = "/scratch/pfs/prt_location_filter/2019/01/01/14491"
+  
+  testInputDir= "C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/testst/testthat/pfs/prt/2019/01/01/3119"
+  
+  testOutputDir = "pfs/out"
+  
+  wlrs_returned <- wrap.loc.repo.strc(DirIn = testInputDir,
+                       DirOutBase = testOutputDir,
+                       Comb = TRUE)
+  
+})
