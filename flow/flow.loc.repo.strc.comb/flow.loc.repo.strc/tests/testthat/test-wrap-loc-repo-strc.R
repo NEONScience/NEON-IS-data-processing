2e44cf990a7327707c4f03e11719f03864a55cc5
Mija Choi <choim@battelleecology.org>
2021-01-19 16:04:07 -0700

    test-wrap-loc-repo-strc.R modified

--- flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
+++ flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
@@ -10,16 +10,33 @@ context("\n                       Unit test of wrap.loc.repo.strc.R\n")
 # Unit test of wrap.loc.repo.strc.R
 test_that("Unit test of wrap.loc.repo.strc.R", {
   source('../../wrap.loc.repo.strc.R')
-  
+  library(stringr)
   testInputDir = "C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/pfs/prt/2019/01/01/3119"
+  dirInLoc <- base::paste0(testInputDir,'/location')
+  fileLoc <- base::dir(dirInLoc)
+  numFileLoc <- base::length(fileLoc)
+ 
+  # Load in the location json and get the location name to verify the test
+  loc <- NEONprocIS.base::def.loc.meta(NameFile=base::paste0(dirInLoc,'/',fileLoc))
+  nameLoc <-loc$name
+  sourceId <- loc$source_id
+  install_date <- loc$install_date
   
   testOutputDir = "pfs/out"
+  
+  #
   # Test scenario 1::
   # if Comb = FALSE, default when Comb is not passed in to wrap.loc.repo.strc,
   # then pfs/prt/2019/01/01/3119 copied to pfs/out/2019/01/01/CFGLOC100241/3119/location/
   
   wrap.loc.repo.strc(DirIn = testInputDir, DirOutBase = testOutputDir)
-  # clean out the test output dirs and file recursively
+
+  installdate <- str_replace_all(install_date, "-", "/")
+  testOutputDirPath <- base::paste0(testOutputDir,"/", installdate, collapse='/')
+  testOutputDirnamedLoc <- base::paste0(testOutputDirPath, "/", nameLoc, "/", sourceId, "/location")
+  expect_true (dir.exists(testOutputDirnamedLoc))
+ 
+   # clean out the test output dirs and file recursively
   if (dir.exists(testOutputDir)) {
     unlink(testOutputDir, recursive = TRUE)
   }
@@ -31,12 +48,15 @@ test_that("Unit test of wrap.loc.repo.strc.R", {
   
   wrap.loc.repo.strc(DirIn = testInputDir, DirOutBase = testOutputDir, Comb = TRUE)
   
+  testOutputDirSourceIdLoc <- base::paste0(testOutputDirPath, "/", nameLoc, "/location")
+  expect_true (dir.exists(testOutputDirSourceIdLoc))
   # clean out the test output dirs and file recursively
   #
   if (dir.exists(testOutputDir))  {
     unlink(testOutputDir, recursive = TRUE)
   }
   
+  # Test scenario 3::
   # If there is no location file, skip
   testInputDir = "C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/pfs/prt_noFiles/2019/01/01/3119"
   
