cafaf06e136f573273d396a6447b85fa192c0b01
Mija Choi <choim@battelleecology.org>
2021-01-19 08:56:33 -0700

    clean up the test outout dir and files

--- flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
+++ flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
@@ -9,18 +9,20 @@ context("\n                       Unit test of wrap.loc.repo.strc.R\n")
 
 # Unit test of wrap.loc.repo.strc.R
 test_that("Unit test of wrap.loc.repo.strc.R", {
-  #  source('C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/wrap.loc.repo.strc.R')
-  
+
   source('../../wrap.loc.repo.strc.R')
-  
-  #testInputDir = "/scratch/pfs/prt_location_filter/2019/01/01/14491"
   
   testInputDir= "C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/pfs/prt/2019/01/01/3119"
   
   testOutputDir = "pfs/out"
+  # Comb = FALSE
+  wrap.loc.repo.strc(DirIn = testInputDir, DirOutBase = testOutputDir)
   
-  wlrs_returned <- wrap.loc.repo.strc(DirIn = testInputDir,
-                       DirOutBase = testOutputDir,
-                       Comb = TRUE)
+  if (file.exists("pfs/out/2019/01/01/CFGLOC100241/3119/location/prt_3119_locations.json")) 
+    { unlink(testOutputDir, recursive = TRUE)}
+
+  wrap.loc.repo.strc(DirIn = testInputDir, DirOutBase = testOutputDir,Comb = TRUE)
   
-})
+  if (file.exists("pfs/out/2019/01/01/CFGLOC100241/location/prt_3119_locations.json")) 
+  { unlink(testOutputDir, recursive = TRUE)}
+  })
