d73ac81aa3fd065b2ae5066bc59901b25357177b
Mija Choi <choim@battelleecology.org>
2021-02-24 16:53:31 -0700

    add negative scenario and the test data

--- pack/NEONprocIS.cal/tests/testthat/test-ucrt-meas-rh-dew-frst-pt.R
+++ pack/NEONprocIS.cal/tests/testthat/test-ucrt-meas-rh-dew-frst-pt.R
@@ -74,12 +74,54 @@ test_that("Unit test of def.ucrt.meas.rh.dew.frst.pt.R", {
   file = "testdata/dewPoint/30000000000080_WO29705_157556.xml",id = 157556, expi= FALSE))
   
   ucrt <- NEONprocIS.cal::def.ucrt.meas.rh.dew.frst.pt(data=data,calSlct=calSlct) 
+  testthat::expect_true(is.data.frame(ucrt) && is.numeric(ucrt$ucrtMeas))
+  
+# #  Sad Path 1,  Identify rows with temperature above 0 degrees C
+# # 
+  data_tempAbove0 <- data.frame(dew_point=c(1,-1,5,4,4.5),temperature=c(32,33,36,38,35),relative_humidity=c(1,6,7,0,10),
+                     readout_time=as.POSIXct(c('2019-01-01 02:00','2019-01-01 04:01','2019-01-01 06:02','2019-01-01 08:01','2019-01-01 10:02'),tz='GMT'))
+
+  ucrt <- NEONprocIS.cal::def.ucrt.meas.rh.dew.frst.pt(data=data_tempAbove0,calSlct=calSlct)
   expect_true(is.data.frame(ucrt) && is.numeric(ucrt$ucrtMeas))
-  
-  #  Sad Path 1, if 
-  data_outOfRange <- data.frame(dew_point=c(1,-1,5,4,4.5),temperature=c(2,-3,6,8,5),relative_humidity=c(1,6,7,0,10),
-                     readout_time=as.POSIXct(c('2021-01-01 02:00','2021-01-01 04:01','2021-01-01 06:02','2021-01-01 08:01','2021-01-01 10:02'),tz='GMT'))
-  
-  ucrt <- NEONprocIS.cal::def.ucrt.meas.rh.dew.frst.pt(data=data_outOfRange,calSlct=calSlct)
+# # 
+#  Sad Path 2,  Move on if no data points fall within this cal window when readout_time is out of range
+#
+  data_rtOutOfRange <- data.frame(dew_point=c(1,-1,5,4,4.5),temperature=c(2,3,6,8,5),relative_humidity=c(1,6,7,0,10),
+                                  readout_time=as.POSIXct(c('2021-05-01 02:00','2021-05-01 04:01','2021-05-01 06:02','2021-05-01 08:01','2021-05-01 10:02'),tz='GMT'))
+
+  calSlct_out=list("temperature"= data.frame(timeBgn=as.POSIXct("2021-01-01",tz="GMT"),
+                                         timeEnd=as.POSIXct("2021-01-02",tz="GMT"),file = "testdata/temperature/30000000000080_WO29705_157555.xml",id = 157555),
+               "relative_humidity"= data.frame(timeBgn=as.POSIXct("2021-01-01",tz="GMT"),
+                                               timeEnd=as.POSIXct("2021-01-02",tz="GMT"),file = "testdata/relHumidity/30000000000080_WO29705_157554.xml",id = 157554),
+               "dew_point"= data.frame(timeBgn=as.POSIXct("2021-01-01",tz="GMT"),timeEnd=as.POSIXct("2021-01-02",tz="GMT"),
+                                       file = "testdata/dewPoint/30000000000080_WO29705_157556.xml",id = 157556))
+
+
+  ucrt <- NEONprocIS.cal::def.ucrt.meas.rh.dew.frst.pt(data=data_rtOutOfRange,calSlct=calSlct_out)
   expect_true(is.data.frame(ucrt) && is.na(ucrt$ucrtMeas))
+#   
+#   Sad Path 3, Issue warning if more than one matching uncertainty coefficient was found
+#   
+  calSlct_two=list("temperature"= data.frame(timeBgn=as.POSIXct("2019-01-01",tz="GMT"),
+                                         timeEnd=as.POSIXct("2019-01-02",tz="GMT"),file = "testdata/temperature/temp_two_U_CVALA1s.xml",id = 157555, expi= FALSE),
+               "relative_humidity"= data.frame(timeBgn=as.POSIXct("2019-01-01",tz="GMT"),
+                                               timeEnd=as.POSIXct("2019-01-02",tz="GMT"),file = "testdata/relHumidity/rh_two_U_CVALA1s.xml",id = 157554, expi= FALSE),
+               "dew_point"= data.frame(timeBgn=as.POSIXct("2019-01-01",tz="GMT"),timeEnd=as.POSIXct("2019-01-02",tz="GMT"),
+                                       file = "testdata/dewPoint/30000000000080_WO29705_157556.xml",id = 157556, expi= FALSE))
+
+  ucrt <- NEONprocIS.cal::def.ucrt.meas.rh.dew.frst.pt(data=data,calSlct=calSlct_two)
+  testthat::expect_true(is.data.frame(ucrt) && is.na(all(ucrt$ucrtMeas)))
+
+  #   Sad Path 4,  Check format of infoCalRh, no U_CVALA1
+  #   
+  calSlct_no=list("temperature"= data.frame(timeBgn=as.POSIXct("2019-01-01",tz="GMT"),
+                                             timeEnd=as.POSIXct("2019-01-02",tz="GMT"),file = "testdata/temperature/temp_no_U_CVALA1.xml",id = 157555, expi= FALSE),
+                   "relative_humidity"= data.frame(timeBgn=as.POSIXct("2019-01-01",tz="GMT"),
+                                                   timeEnd=as.POSIXct("2019-01-02",tz="GMT"),file = "testdata/relHumidity/rh_no_U_CVALA1.xml",id = 157554, expi= FALSE),
+                   "dew_point"= data.frame(timeBgn=as.POSIXct("2019-01-01",tz="GMT"),timeEnd=as.POSIXct("2019-01-02",tz="GMT"),
+                                           file = "testdata/dewPoint/30000000000080_WO29705_157556.xml",id = 157556, expi= FALSE))
+  
+  ucrt <- try(NEONprocIS.cal::def.ucrt.meas.rh.dew.frst.pt(data=data,calSlct=calSlct_no), silent = TRUE)
+  testthat::expect_true((class(ucrt)[1] == "try-error"))
+  
    })
