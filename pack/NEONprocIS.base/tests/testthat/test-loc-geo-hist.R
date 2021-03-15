4ac98fc8370c139aa5c0863d171bcb110ae754b8
Mija Choi <choim@battelleecology.org>
2021-03-15 17:42:25 -0600

    test-loc-geo-hist.R initial checkin

---
+++ pack/NEONprocIS.base/tests/testthat/test-loc-geo-hist.R
@@ -0,0 +1,69 @@
+##############################################################################################
+#' @title Unit test of def.loc.geo.hist.R, read sensor locations json file and return the geolocation history of
+#' all the configured locations within the file
+#' 
+#' @author
+#' Mija Choi \email{choim@batelleEcology.org}
+
+#' @description
+#' Run unit tests for def.loc.geo.hist.R, Read sensor locations json file and return the geolocation history of
+#' all the configured locations within the file
+#' 
+#' @param NameFile Filename (including relative or absolute path). Must be json format.
+#' @param log A logger object as produced by NEONprocIS.base::def.log.init to produce structured log
+#' output. Defaults to NULL, in which the logger will be created and used within the function.
+
+#' @return 
+#' A list of configured locations found in the file. Nested within the list element for each 
+#' configured location is a list of geolocation history, one list element per location change. 
+#' Further nested within the list element for each geolocation is a variable list of properties 
+#' of that geolocation. Each geolocation property may also be a list of elements.
+#' 
+#' @references
+#' License: (example) GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+#' NEON.DOC.000785 TIS Calibrated Measurements and Level 1 Data Products Uncertainty Budget Plan
+
+#' @keywords Currently none
+
+#' @examples
+#' To run with testthat:
+#' devtools::test(pkg="<path>/NEON-IS-data-processing/pack/NEONprocIS.base")
+#' an example, devtools::test(pkg="C:/projects/NEON-IS-data-processing/pack/NEONprocIS.base")
+#' 
+#' To run the test
+#' NameFile <- '~/pfs/aquatroll200_23614_locations.json'
+#' locGeoHist <- NEONprocIS.base::def.loc.geo.hist(NameFile=NameFile)
+
+#' @seealso \link[NEONprocIS.base]
+#'
+#' @export
+
+# changelog and author contributions / copyrights
+#   Mija Choi (2021-03-15)
+#     original creation
+##############################################################################################
+library(rapportools)
+
+# Define test context
+#context("\n       | Read sensor locations json file and return the geolocation history of all the configured locations within the file \n")
+
+# Read sensor locations json file and return the geolocation history of all the configured locations within the file
+test_that("   Read sensor locations json file and return the geolocation history of all the configured locations within the file ", {
+  ##########
+
+  testDir = "testdataJson/"
+  testFile = 'locations.json'
+  NameFileIn <- paste0(testDir, testFile)
+  locMeta <- NEONprocIS.base::def.loc.meta(NameFile=NameFileIn)
+  
+  # Happy path #1:Read sensor locations json file and return the geolocation history of all the configured locations within the file
+
+  cat("\n       |=====================================   Test Summary   ====================================|\n")
+  
+  locGeoHist <- NEONprocIS.base::def.loc.geo.hist(NameFile=NameFileIn)
+  testthat::expect_true (is.list(locGeoHist))
+  testthat::expect_match(names(locGeoHist), locMeta$name) 
+  
+# Happy path #2: 
+}
+)
