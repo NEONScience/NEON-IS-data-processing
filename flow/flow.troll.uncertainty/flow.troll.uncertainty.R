62768c0116d19d842b956f2d994182a56e26e117
ncatolico <ncatolico@battelleecology.org>
2021-03-23 11:50:52 -0600

    progress

--- flow/flow.troll.uncertainty/flow.troll.uncertainty.R
+++ flow/flow.troll.uncertainty/flow.troll.uncertainty.R
@@ -1,18 +1,93 @@
-Sys.setenv(DIR_IN='/home/NEON/ncatolico/pfs/groundwaterPhysical_qaqc_data_group')
-log <- NEONprocIS.base::def.log.init(Lvl = "debug")
-arg <- c("DirIn=$DIR_IN","DirOut=~/pfs/out","WndwInst=005","WndwAgr=030")
-
-#' 4. "WndwAgr=value", where value is the aggregation interval for which to compute statistics. It is formatted 
-#' as a 3 character sequence, typically representing the number of minutes over which to compute statistics. 
+##############################################################################################
+#' @title Workflow for Level Troll 500 and Aqua Troll 200 Science Computations
+#' 
+#' @author
+#' Nora Catolico \email{ncatolico@battelleecology.org}
+#' 
+#' @description Workflow. Calculate elevation and derive uncertainty  for surface and groundwater troll data products.
+#' 
+#' The arguments are: 
+#' 
+#' 1. "DirIn=value", where value is the  path to input data directory (see below)
+#' The input path is structured as follows: #/pfs/BASE_REPO/#/yyyy/mm/dd/#, where # indicates any number of 
+#' parent and child directories of any name, so long as they are not 'pfs', the same name as subdirectories 
+#' expected at the terminal directory (see below), or recognizable as the 'yyyy/mm/dd' structure 
+#' which indicates the 4-digit year, 2-digit month, and 2-digit day of the data contained in the folder.
+#' 
+#' Nested within this path are the folders:
+#'         /sensor/yyyy/mm/dd/namedLocation/data
+#'         /sensor/yyyy/mm/dd/namedLocation/location
+#'         /sensor/yyyy/mm/dd/namedLocation/uncertainty_coef
+#'         /sensor/yyyy/mm/dd/namedLocation/uncertainty_data
+#'         
+#'        
+#' 2. "DirOut=value", where the value is the output path that will replace the #/pfs/BASE_REPO portion 
+#' of DirIn.
+#' 
+#' 3. "WndwAgr=value", (optional) where value is the aggregation interval for which to compute unceratainty. It is 
+#' formatted as a 3 character sequence, typically representing the number of minutes over which to compute unceratainty 
 #' For example, "WndwAgr=001" refers to a 1-minute aggregation interval, while "WndwAgr=030" refers to a 
 #' 30-minute aggregation interval. Multiple aggregation intervals may be specified by delimiting with a pipe 
 #' (e.g. "WndwAgr=001|030|060"). Note that a separate file will be output for each aggregation interval. 
 #' It is assumed that the length of the file is one day. The aggregation interval must divide one day into 
-#' complete intervals.
+#' complete intervals. No uncertainty data will be output if both "WndwAgr" and "WndwInst" are NULL.
 #' 
-#' 5. "WndwInst=value"
+#' 4. "WndwInst=value", (optional) where value is the instantaneous data ouptut frequency, formatted as a 3 character 
+#' sequence. For example, "WndwInst=001" refers to a 1-minute sampling frequency. "WndwInst" must be included if instantaneous
+#' uncertainty data output is desired. No uncertainty data will be output if both "WndwAgr" and "WndwInst" are NULL.
+#' 
+#' 5. "FileSchmData=value" (optional), where values is the full path to the avro schema for the output data 
+#' file. If this input is not provided, the output schema for the data will be the same as the input data
+#' file. If a schema is provided, ENSURE THAT ANY PROVIDED OUTPUT SCHEMA FOR THE DATA MATCHES THE COLUMN ORDER OF 
+#' THE INPUT DATA. Note that you will need to distinguish between the aquatroll200 (outputs conductivity) and the 
+#' leveltroll500 (does not output conductivity) in your schema.
+#' 
+#' 6. "FileSchmUcrt=value" (optional), where values is the full path to the avro schema for the output uncertainty data 
+#' file. If this input is not provided, the output schema for the data will be the same as the input data
+#' file. If a schema is provided, ENSURE THAT ANY PROVIDED OUTPUT SCHEMA FOR THE DATA MATCHES THE COLUMN ORDER OF 
+#' THE INPUT DATA. Note that you will need to distinguish between the aquatroll200 (outputs conductivity) and the 
+#' leveltroll500 (does not output conductivity) in your schema.
+#' 
+#' 7. "DirSubCopy=value" (optional), where value is the names of additional subfolders, separated by 
+#' pipes, at the same level as the flags folder in the input path that are to be copied with a 
+#' symbolic link to the output path. 
+#'
+#' Note: This script implements logging described in \code{\link[NEONprocIS.base]{def.log.init}},
+#' which uses system environment variables if available.
+#' @return water table elevation calculated from calibrated pressure, density of water, gravity, and sensor elevation.
+#' Data and uncertainty values will be output in Parquet format in DirOut, where the terminal directory 
+#' of DirOut replaces BASE_REPO but otherwise retains the child directory structure of the input path. 
+#' Any other folders specified in argument DirSubCopy will be copied over unmodified with a symbolic link. 
+#'  
+#' If no output schema is provided for the data, the output column/variable names will be determined by the 
+#' sensor type (leveltroll500 or aquatroll200). Output column/variable names for the leveltroll500 will be
+#' readout_time, pressure, pressure_data_quality, temperature, temperature_data quality, elevation, in that order. 
+#' Output column/variable names for the aquatroll200 will be readout_time, pressure, pressure_data_quality, 
+#' temperature, temperature_data quality, conductivity, conductivity_data_quality, elevation, in that order.
+#' ENSURE THAT ANY PROVIDED OUTPUT SCHEMA MATCHES THIS ORDER. Otherwise, they will be labeled incorrectly.
+#' 
+#' @references
+#' License: (example) GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+#' @keywords Currently none
+#' 
+#' @examples
+#' Stepping through the code in Rstudio 
+#' Sys.setenv(DIR_IN='/home/NEON/ncatolico/pfs/swPhysical_leveltroll500_qaqc_data_group')
+#' log <- NEONprocIS.base::def.log.init(Lvl = "debug")
+#' arg <- c("DirIn=$DIR_IN","DirOut=~/pfs/out","WndwAgr=005|030")
+#' rm(list=setdiff(ls(),c('arg','log')))
+#' 
+#' @seealso None currently
+#' changelog and author contributions / copyrights
+#'   Nora Catolico (2021-02-02)
+#'     original creation
+#'
+##############################################################################################
+# Start logging
+log <- NEONprocIS.base::def.log.init()
 
-################################################
+# Pull in command line arguments (parameters)
+arg <- base::commandArgs(trailingOnly = TRUE)
 
 # Parse the input arguments into parameters
 Para <- NEONprocIS.base::def.arg.pars(arg = arg,NameParaReqd = c("DirIn", "DirOut"),NameParaOptn = c("DirSubCopy","FileSchmData","FileSchmUcrt","WndwInst","WndwAgr"),log = log)
@@ -28,8 +103,7 @@ log$debug(base::paste0('Output directory: ',DirOut))
 # Retrieve output schema for data
 FileSchmDataOut <- Para$FileSchmData
 log$debug(base::paste0('Output schema for data: ',base::paste0(FileSchmDataOut,collapse=',')))
-# Retrieve output schema; no need for new data schema
-# Retrieve output schema for data
+# Retrieve output schema
 FileSchmUcrtOut <- Para$FileUcrtUcrt
 log$debug(base::paste0('Output schema for uncertainty: ',base::paste0(FileSchmUcrtOut,collapse=',')))
 
@@ -58,15 +132,13 @@ if(base::is.null(Para$WndwAgr) || Para$WndwAgr == 'NA'){
   WndwAgr <- NULL
 }else{
   WndwAgr <- base::as.difftime(base::as.numeric(Para$WndwAgr),units="mins")
-  log$debug(base::paste0('Aggregation interval(s), in minutes: ',base::paste0(WndwInst,collapse=',')))
+  log$debug(base::paste0('Aggregation interval(s), in minutes: ',base::paste0(WndwAgr,collapse=',')))
 }
 
 # Retrieve optional sensor subdirectories to copy over
 nameDirSubCopy <- c('location','uncertainty_coef')
 DirLocationCopy <- base::unique(base::setdiff(Para$DirIn,nameDirSubCopy[1]))
 DirUncertCoefCopy <- base::unique(base::setdiff(Para$DirIn,nameDirSubCopy[2]))
-
-
 
 #what are the expected subdirectories of each input path
 nameDirSub <- c('data','location','uncertainty_coef','uncertainty_data')
@@ -85,18 +157,21 @@ if(base::length(DirIn) < 1){
 }
 
 # Create the binning for each aggregation interval
-timeBgnDiff <- list()
-timeEndDiff <- list()
-for(idxWndwAgr in base::seq_len(base::length(WndwAgr))){
-  timeBinDiff <- NEONprocIS.base::def.time.bin.diff(WndwBin=WndwAgr[idxWndwAgr],WndwTime=base::as.difftime(1,units='days'))
-  timeBgnDiff[[idxWndwAgr]] <- timeBinDiff$timeBgnDiff # Add to timeBgn of each day to represent the starting time sequence
-  timeEndDiff[[idxWndwAgr]] <- timeBinDiff$timeEndDiff # Add to timeBgn of each day to represent the end time sequence
-} # End loop around aggregation intervals
+if(length(WndwAgr)>0){
+  timeBgnDiff <- list()
+  timeEndDiff <- list()
+  for(idxWndwAgr in base::seq_len(base::length(WndwAgr))){
+    timeBinDiff <- NEONprocIS.base::def.time.bin.diff(WndwBin=WndwAgr[idxWndwAgr],WndwTime=base::as.difftime(1,units='days'))
+    timeBgnDiff[[idxWndwAgr]] <- timeBinDiff$timeBgnDiff # Add to timeBgn of each day to represent the starting time sequence
+    timeEndDiff[[idxWndwAgr]] <- timeBinDiff$timeEndDiff # Add to timeBgn of each day to represent the end time sequence
+  } # End loop around aggregation intervals
+}
+
 
 # Process each datum
 for (idxDirIn in DirIn){
   ##### Logging and initializing #####
-  #idxDirIn<-DirIn[1] #for testing
+  #idxDirIn<-DirIn[4] #for testing
   log$info(base::paste0('Processing path to datum: ',idxDirIn))
   
   # Gather info about the input directory (including date), and create base output directory
@@ -212,6 +287,16 @@ for (idxDirIn in DirIn){
     dataCol <- c("readout_time","pressure","temperature","elevation") 
   }
   dataOut <- dataOut[,dataCol]
+  #Write out data
+  rptDataOut <- try(NEONprocIS.base::def.wrte.parq(data = dataOut, 
+                                                   NameFile = base::paste0(idxDirOutData,"/",context,"_",sensor,"_",CFGLOC,"_",format(timeBgn,format = "%Y-%m-%d"),".parquet"), 
+                                                   Schm = SchmDataOut),silent=FALSE)
+  if(any(grepl('try-error',class(rptDataOut)))){
+    log$error(base::paste0('Writing the output data failed: ',attr(rptDataOut,"condition")))
+    stop()
+  } else {
+    log$info("Data written out.")
+  }
   
   
   ##### Read in uncertainty data #####
@@ -298,14 +383,24 @@ for (idxDirIn in DirIn){
     ucrtOut_inst <- uncertaintyData[,ucrtCol_inst]
     #standardize naming
     names(ucrtOut_inst)<- c("readout_time","groundwaterTempExpUncert","groundwaterPressureExpUncert","groundwaterElevExpUncert","groundwaterCondExpUncert")
+    
+    #write out instantaneous uncertainty data
+    rptUcrtOut_Inst <- try(NEONprocIS.base::def.wrte.parq(data = ucrtOut_inst, 
+                                                          NameFile = base::paste0(idxDirOutUcrt,"/",context,"_",sensor,"_",CFGLOC,"_",format(timeBgn,format = "%Y-%m-%d"),"_",WndwInst[1],"_minUcrt.parquet"), 
+                                                          Schm = SchmUcrtOut),silent=FALSE)
+    
+    if(any(grepl('try-error',class(ucrtOut_inst)))){
+      log$error(base::paste0('Writing the output data failed: ',attr(ucrtOut_inst,"condition")))
+      stop()
+    } else {
+      log$info("Data written out.")
+    }
   }
   
   
-  
+  ######## Uncertainty for L1 mean 5 and 30 minute outputs ########
+  #the repeatability and reproducibility of the sensor and  uncertainty of the calibration procedures and coefficients including uncertainty in the standard
   if(length(WndwAgr)>0){
-    ######## Uncertainty for L1 mean 5 and 30 minute outputs ########
-    #the repeatability and reproducibility of the sensor and  uncertainty of the calibration procedures and coefficients including uncertainty in the standard
-    
     #determine averaging window
     timeMeas <- base::as.POSIXlt(trollData$readout_time)# Pull out time variable
     # Run through each aggregation interval, creating the daily time series of windows
@@ -339,13 +434,17 @@ for (idxDirIn in DirIn){
         if(length(uncertaintyCoef)>0){
           #Temperature Uncertainty
           #combined uncertainty of temperature is equal to the standard uncertainty values provided by CVAL
-          #TemperatureExpUncert<-2*(UNat_temp^2+U_CVALA3_temp^2)^0.5
-          ucrtDataWndwTime$temperature_ucrtExpn_L1<-NEONprocIS.stat::wrap.ucrt.dp01.cal.mult(data=dataWndwTime,VarUcrt='temperature',ucrtCoef=uncertaintyCoef)
+          #numPts <- base::sum(x=!base::is.na(dataWndwTime$temperature),na.rm=FALSE)
+          #se <- stats::sd(dataWndwTime$temperature,na.rm=TRUE)/base::sqrt(numPts)
+          #TemperatureExpUncert<-2*(se^2+U_CVALA3_temp^2)^0.5
+          ucrtDataWndwTime$temperature_ucrtExpn_L1<-NEONprocIS.stat::wrap.ucrt.dp01.cal.cnst(data=dataWndwTime,VarUcrt='temperature',ucrtCoef=uncertaintyCoef)
           
           #Pressure Uncertainty
           #combined uncertainty of pressure is equal to the standard uncertainty values provided by CVAL
-          #pressure_ucrtExpn_L1<-2*(UNat_pressure^2+U_CVALA3_pressure^2)^0.5
-          ucrtDataWndwTime$pressure_ucrtExpn_L1<-NEONprocIS.stat::wrap.ucrt.dp01.cal.mult(data=dataWndwTime,VarUcrt='pressure',ucrtCoef=uncertaintyCoef)
+          #numPts <- base::sum(x=!base::is.na(dataWndwTime$pressure),na.rm=FALSE)
+          #se <- stats::sd(dataWndwTime$pressure,na.rm=TRUE)/base::sqrt(numPts)
+          #pressure_ucrtExpn_L1<-2*(se^2+U_CVALA3_pressure^2)^0.5
+          ucrtDataWndwTime$pressure_ucrtExpn_L1<-NEONprocIS.stat::wrap.ucrt.dp01.cal.cnst(data=dataWndwTime,VarUcrt='pressure',ucrtCoef=uncertaintyCoef)
           ucrtDataWndwTime$pressure_ucrtComb_L1<-ucrtDataWndwTime$pressure_ucrtExpn_L1/2
           
           #Elevation Uncertainty
@@ -356,34 +455,23 @@ for (idxDirIn in DirIn){
           if(sensor=='aquatroll200'){
             #Raw Conductivity Uncertainty
             #combined uncertainty of actual conductivity (not published) is equal to the standard uncertainty values provided by CVAL
-            #rawConductivity_ucrtExpn_L1<-2*(UNat_cond^2+U_CVALA3_cond^2)^0.5
-            ucrtDataWndwTime$rawConductivity_ucrtExpn_L1<-NEONprocIS.stat::wrap.ucrt.dp01.cal.mult(data=dataWndwTime,VarUcrt='conductivity',ucrtCoef=uncertaintyCoef)
+            #numPts <- base::sum(x=!base::is.na(dataWndwTime$raw_conductivity),na.rm=FALSE)
+            #se <- stats::sd(dataWndwTime$raw_conductivity,na.rm=TRUE)/base::sqrt(numPts)
+            #rawConductivity_ucrtExpn_L1<-2*(se^2+U_CVALA3_cond^2)^0.5
+            ucrtDataWndwTime$rawConductivity_ucrtExpn_L1<-NEONprocIS.stat::wrap.ucrt.dp01.cal.cnst(data=dataWndwTime,VarUcrt='conductivity',ucrtCoef=uncertaintyCoef)
             
             #Specific Conductivity Uncertainty
             #grab U_CVALA3 values
-            U_CVALA3_cond<-NULL
-            ucrtCoef<-NULL
-            for(i in 1:length(uncertaintyCoef)){
-              if(uncertaintyCoef[[i]]$term =='conductivity' & uncertaintyCoef[[i]]$Name == 'U_CVALA3'){
-                ucrtCoef<-as.double(uncertaintyCoef[[i]]$Value)
-                U_CVALA3_cond<-append(U_CVALA3_cond,ucrtCoef)
-              }
-            }
-            if(length(U_CVALA3_cond)>1){
-              log$warn("Multiple conductivity U_CVALA3 coefficients")
-              stop()
-            }
-            U_CVALA3_temp<-NULL
-            for(i in 1:length(uncertaintyCoef)){
-              if(uncertaintyCoef[[i]]$term =='temperature' & uncertaintyCoef[[i]]$Name == 'U_CVALA3'){
-                ucrtCoef<-as.double(uncertaintyCoef[[i]]$Value)
-                U_CVALA3_temp<-append(U_CVALA3_temp,ucrtCoef)
-              }
-            }
-            if(length(U_CVALA3_temp)>1){
-              log$warn("Multiple temperature U_CVALA3 coefficients")
-              stop()
-            }
+            U_CVALA3_cond<-NEONprocIS.stat::def.ucrt.dp01.cal.cnst(ucrtCoef=uncertaintyCoef,
+                                                    NameCoef='U_CVALA3',
+                                                    VarUcrt='conductivity',
+                                                    TimeAgrBgn=dataWndwTime$readout_time[1],
+                                                    TimeAgrEnd=dataWndwTime$readout_time[base::nrow(dataWndwTime)]+as.difftime(.001,units='secs'))
+            U_CVALA3_temp<-NEONprocIS.stat::def.ucrt.dp01.cal.cnst(ucrtCoef=uncertaintyCoef,
+                                                                     NameCoef='U_CVALA3',
+                                                                     VarUcrt='temperature',
+                                                                     TimeAgrBgn=dataWndwTime$readout_time[1],
+                                                                     TimeAgrEnd=dataWndwTime$readout_time[base::nrow(dataWndwTime)]+as.difftime(.001,units='secs'))
             
             # Compute uncertainty of the mean due to natural variation, represented by the standard error of the mean
             #log$debug(base::paste0('Computing L1 uncertainty due to natural variation (standard error)'))
@@ -430,49 +518,24 @@ for (idxDirIn in DirIn){
       }else{
         names(ucrtOut_agr)<- c("readout_time","surfacewaterTempExpUncert","surfacewaterPressureExpUncert","surfacewaterElevExpUncert")
       }
-    }
-  }
-  
-  
-  
-  
-  
-  
-  #Write out data
-  rptDataOut <- try(NEONprocIS.base::def.wrte.parq(data = dataOut, 
-                                                   NameFile = base::paste0(idxDirOutData,"/",context,"_",sensor,"_",CFGLOC,"_",format(timeBgn,format = "%Y-%m-%d"),".parquet"), 
-                                                   Schm = SchmDataOut),silent=FALSE)
-  if(any(grepl('try-error',class(rptDataOut)))){
-    log$error(base::paste0('Writing the output data failed: ',attr(rptDataOut,"condition")))
-    stop()
-  } else {
-    log$info("Data written out.")
-  }
-  
-  #Write out uncertainty data
-  if(length(WndwInst)>0){
-    rptUcrtOut_Inst <- try(NEONprocIS.base::def.wrte.parq(data = ucrtOut_inst, 
-                                                          NameFile = base::paste0(idxDirOutUcrt,"/",context,"_",sensor,"_",CFGLOC,"_",format(timeBgn,format = "%Y-%m-%d"),"_",WndwInst[1],"_minUcrt.parquet"), 
-                                                          Schm = SchmUcrtOut),silent=FALSE)
-    
-    if(any(grepl('try-error',class(ucrtOut_inst)))){
-      log$error(base::paste0('Writing the output data failed: ',attr(ucrtOut_inst,"condition")))
-      stop()
-    } else {
-      log$info("Data written out.")
-    }
-  }
-  
-  if(length(WndwAgr)>0){
-    rptUcrtOut_Agr <- try(NEONprocIS.base::def.wrte.parq(data = ucrtOut_agr, 
-                                                         NameFile = base::paste0(idxDirOutUcrt,"/",context,"_",sensor,"_",CFGLOC,"_",format(timeBgn,format = "%Y-%m-%d"),"_",WndwAgr[idxWndwAgr],"_minUcrt.parquet"), 
-                                                         Schm = SchmUcrtOut),silent=FALSE)
-    
-    if(any(grepl('try-error',class(rptUcrtOut_Agr)))){
-      log$error(base::paste0('Writing the output data failed: ',attr(rptUcrtOut_Agr,"condition")))
-      stop()
-    } else {
-      log$info("Data written out.")
+      
+      #Write out aggregate uncertainty data
+      rptUcrtOut_Agr <- try(NEONprocIS.base::def.wrte.parq(data = ucrtOut_agr, 
+                                                           NameFile = base::paste0(idxDirOutUcrt,"/",context,"_",sensor,"_",CFGLOC,"_",format(timeBgn,format = "%Y-%m-%d"),"_",WndwAgr[idxWndwAgr],"_minUcrt.parquet"), 
+                                                           Schm = SchmUcrtOut),silent=FALSE)
+      
+      if(any(grepl('try-error',class(rptUcrtOut_Agr)))){
+        log$error(base::paste0('Writing the output data failed: ',attr(rptUcrtOut_Agr,"condition")))
+        stop()
+      } else {
+        log$info("Data written out.")
+      }
     }
   }
 }
+  
+  
+  
+  
+  
+ 
