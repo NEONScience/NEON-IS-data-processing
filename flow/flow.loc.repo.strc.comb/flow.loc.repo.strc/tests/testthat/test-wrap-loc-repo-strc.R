87d0afb757fbbef99ba183117b54a2a1ea4dc9bd
Mija Choi <choim@battelleecology.org>
2021-05-11 20:09:01 -0600

    cleanup the test

--- flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
+++ flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/test-wrap-loc-repo-strc.R
@@ -53,7 +53,7 @@ test_that("Unit test of wrap.loc.repo.strc.R", {
   library(stringr)
   
   wk_dir <- getwd()
-  
+
   testInputDir <- base::paste0(wk_dir, '/', 'pfs/proc_group/prt/2019/01/01/3119')
   
   dirInLoc <- base::paste0(testInputDir, '/location')
@@ -63,30 +63,25 @@ test_that("Unit test of wrap.loc.repo.strc.R", {
   
   # Load in the location json and get the location name to verify the test
   loc <- NEONprocIS.base::def.loc.meta(NameFile = base::paste0(dirInLoc, '/', fileLoc))
-  nameLoc <- loc$name
-  sourceId <- loc$source_id
-  install_date <- loc$install_date
-  
+ 
   testOutputDir = "pfs/out"
-  installdate <- str_replace_all(install_date, "-", "/")
+  installdate <- str_replace_all(loc$install_date, "-", "/")
   testOutputDirPath <- base::paste0(testOutputDir, "/",loc$source_type,"/",installdate,collapse = '/')
   #
-  # Test scenario 1::
-  # if Comb = FALSE, default when Comb is not passed in to wrap.loc.repo.strc.
-  # then pfs/proc_group/source_type/2019/01/01/source_id/ copied to 
-  #      pfs/out/source_type/2019/01/01/LOCATION_ID/source_id/
-  #                                                         location/
-  #                                                         data/
-  # for example, /pfs/out/prt/2019/01/01/CFGLOC100241/3119/location/ & 
-  #                                                       /data/
   # Remove the test output dirs and file recursively
   if (dir.exists(testOutputDir)) {
     unlink(testOutputDir, recursive = TRUE)
   }
   
+  # Test scenario 1::
+  # if Comb = FALSE, default when Comb is not passed in to wrap.loc.repo.strc.
+  # for example, pfs/proc_group/prt/2019/01/01/3119/ copied to 
+  #              pfs/out/prt/2019/01/01/CFGLOC100241/3119/location/
+  #                                                      / data/
+  #
   wrap.loc.repo.strc(DirIn = testInputDir, DirOutBase = testOutputDir)
-  testOutputDirnamedLoc <- base::paste0(testOutputDirPath, "/", nameLoc, "/", sourceId, "/location")
-  testOutputDirData <- base::paste0(testOutputDirPath, "/", nameLoc, "/", sourceId, "/data")
+  testOutputDirnamedLoc <- base::paste0(testOutputDirPath, "/", loc$name, "/", loc$source_id, "/location")
+  testOutputDirData <- base::paste0(testOutputDirPath, "/",loc$name, "/", loc$source_id, "/data")
   expect_true ((file.exists(testOutputDirnamedLoc,fileLoc, recursive = TRUE)) && 
                (file.exists(testOutputDirData,fileData, recursive = TRUE)))
  
@@ -105,14 +100,13 @@ test_that("Unit test of wrap.loc.repo.strc.R", {
   
   wrap.loc.repo.strc(DirIn = testInputDir,DirOutBase = testOutputDir,Comb = TRUE)
   
-  testOutputDirnamedLoc <- base::paste0(testOutputDirPath, "/", nameLoc, "/", "/location")
-  testOutputDirData <- base::paste0(testOutputDirPath, "/", nameLoc, "/", "/data")
+  testOutputDirnamedLoc <- base::paste0(testOutputDirPath, "/", loc$name, "/", "/location")
+  testOutputDirData <- base::paste0(testOutputDirPath, "/", loc$name, "/", "/data")
   expect_true ((file.exists(testOutputDirnamedLoc,fileLoc, recursive = TRUE)) && 
                  (file.exists(testOutputDirData,fileData, recursive = TRUE)))
   # Test scenario 3::
   # If there is no location file, skip
-  #  testInputDir = "C:/projects/NEON-IS-data-processing/flow/flow.loc.repo.strc.comb/flow.loc.repo.strc/tests/testthat/pfs/prt_noFiles/2019/01/01/3119"
-  
+
   testInputDir <- base::paste0(wk_dir, '/', 'pfs/proc_group/prt_noFiles/2019/01/01/3119')
   # Remove the test output dirs and file recursively
   #
@@ -124,14 +118,11 @@ test_that("Unit test of wrap.loc.repo.strc.R", {
   expect_true (!dir.exists(testOutputDir))
   
   # Test scenario 4::
-  # If there is more than one location file, use the first
-  # AND Comb is not passed in to wrap.loc.repo.strc 
+  # If there are more than one location file AND Comb is not passed in to wrap.loc.repo.strc 
+  # use the first and copy all in the output directory
   # then pfs/proc_group/source_type/2019/01/01/source_id/ copied to 
-  #      pfs/out/source_type/2019/01/01/LOCATION_ID/source_id/
-  #                                                         location/
-  #                                                         data/
-  # for example, /pfs/out/prt_moreThanOneFile/2019/01/01/CFGLOC100241/3119/location/ & 
-  #                                                       /data/
+  #      pfs/out/source_type/2019/01/01/CFGLOC100241/3119/location/ & 
+  #                                                       data/
   testInputDir <- base::paste0(wk_dir, '/', 'pfs/proc_group/prt_moreThanOneFile/2019/01/01/16247')
   dirInLoc <- base::paste0(testInputDir, '/location')
   fileLoc <- base::dir(dirInLoc)
@@ -140,21 +131,14 @@ test_that("Unit test of wrap.loc.repo.strc.R", {
 
   # Load in the location json and get the location name to verify the test
   loc <- NEONprocIS.base::def.loc.meta(NameFile = base::paste0(dirInLoc, '/', fileLoc[1]))
-  nameLoc <- loc$name
-  sourceId <- loc$source_id
-
   testOutputDir = "pfs/out"
-  testOutputDirPath <- base::paste0(testOutputDir, "/","prt_moreThanOneFile/", installdate, collapse = '/')
-  # Remove the test output dirs and file recursively
   #
   if (dir.exists(testOutputDir))  {
     unlink(testOutputDir, recursive = TRUE)
   }
   wrap.loc.repo.strc(DirIn = testInputDir, DirOutBase = testOutputDir)
-  testOutputDirnamedLoc <- base::paste0(testOutputDirPath, "/", nameLoc, "/", sourceId, "/location")
-  testOutputDirData <- base::paste0(testOutputDirPath, "/", nameLoc, "/", sourceId, "/data")
-  
-  expect_true ((file.exists(testOutputDirnamedLoc,fileLoc, recursive = TRUE)) && 
-                 (file.exists(testOutputDirData,fileData, recursive = TRUE)))
+
+  expect_true (file.exists(testOutputDir,fileLoc, recursive = TRUE) && 
+                 file.exists(testOutputDir,fileData, recursive = TRUE))
   
 })
