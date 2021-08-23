5d516e3fa8d6e822c1cc512393efa82e7da818c9
Mija Choi <choim@battelleecology.org>
2021-08-23 12:18:46 -0600

    add def.generate.err.msg.R to base package

--- pack/NEONprocIS.base/R/def.generate.err.msg.R
+++ pack/NEONprocIS.base/R/def.generate.err.msg.R
@@ -1,23 +1,34 @@
-###################################################################################################
-#' @title validate if the JSON content is strictly valid
+##################################################################################################################
+#' @title Generate the error message. It includes error message, function called, line number and function calling
 
 #' @author
 #' Mija Choi \email{choim@battelleecology.org}
 
 #' @description
+#' Generate the error message including error message, function called, line number and function calling.
+#' And return the error message 
 
-#' @param 
+#' @param Errmsg String. Can be NULL if no specific error. 
+#' 
+#' @param fun_calling String.
+#' @param fun_called String. 
+#' @param lineNum Number. 
 
-#' @return String. \cr
+#' @return Message with error message, function called, line number and function calling
 
-#' @references Currently none
+#' @references 
+#' License: (example) GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
 
 #' @keywords Currently none
 
-#' @examples NEONprocIS.base::def.validate.json (NameFileIn)
+#' @examples 
+#' msg <- NEONprocIS.base::def.generate.err.msg(errmsg="Bad input", fun_calling=rlang::call_frame(n = 2)$fn_name, fun_called=rlang::call_frame(n = 1)$fn_name, lineNum=getSrcLocation(function() {}, "line"))
+#' log$error(msg)
+#' stop(msg)
+#' An example output, Error in check(x) : "Bad input" in check near line 4, called by test 
 
-#' @seealso \link[NEONprocIS.cal]{def.read.cal.xml}
-#'
+#' @seealso Currently none
+
 #' @export
 
 # changelog and author contributions / copyrights
