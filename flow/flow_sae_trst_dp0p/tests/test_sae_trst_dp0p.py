990d0acb32daaf312975d1baf75376c903629122
Liangying Zhang <lzhang@battelleecology.org>
2021-12-16 10:44:48 -0700

    more files for li7200

--- flow/flow_sae_trst_dp0p/tests/test_sae_trst_dp0p.py
+++ flow/flow_sae_trst_dp0p/tests/test_sae_trst_dp0p.py
@@ -2,7 +2,7 @@
 
 from unittest import TestCase
 
-from flow_sae_trst_dp0p.csat3 import get_nth_bit, get_range_bits
+from flow_sae_trst_dp0p.shared_functions import get_nth_bit, get_range_bits, get_nth_bit_opposite
 
 
 class SaeTrstDp0pTest(TestCase):
@@ -10,16 +10,19 @@ class SaeTrstDp0pTest(TestCase):
     def setUp(self):
         self.number = 61503  #1111 0000 0011 1111
 
-
     def test_get_nth_bit(self):
         message = "nth bit was not retrieved correctly"
         self.assertEqual(get_nth_bit(self.number, 4), 1, message)
         self.assertEqual(get_nth_bit(self.number, 6), 0, message)
 
-
     def test_get_range_bits(self):
         message = "data from range bits were not retrieved correctly"
         self.assertEqual(get_range_bits(self.number, 4, 5), 3, message)
+
+    def test_get_nth_bit_opposite(self):
+        message = "nth bit should be retrieved and reversed"
+        self.assertEqual(get_nth_bit_opposite(self.number, 4), 0, message)
+        self.assertEqual(get_nth_bit_opposite(self.number, 6), 1, message)
 
 
 
