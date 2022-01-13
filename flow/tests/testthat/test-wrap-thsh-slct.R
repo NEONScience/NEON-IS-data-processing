b372d0ef78bf9c38a7b8e4436a8a8ee214cdbf71
choim <choim@battelleecology.org>
2022-01-12 17:57:05 -0700

    add more test location jsons for neg test

--- flow/tests/testthat/test-wrap-thsh-slct.R
+++ flow/tests/testthat/test-wrap-thsh-slct.R
@@ -5,10 +5,10 @@
 #' Mija Choi \email{choim@batelleEcology.org}
 #'
 #' @description Wrapper function. Select thresholds for each processing day and location for sets of term and context(s)
-#' among all available thresholds. 
-#' The choice of constraint/threshold to use is determined by moving up the following hierarchy 
-#' from finer to coarser constraints until one applies. Thus, the finest applicable level of constraint 
-#' is chosen. Threshold selection order is as follows (1 being the finest possible constraint): 
+#' among all available thresholds.
+#' The choice of constraint/threshold to use is determined by moving up the following hierarchy
+#' from finer to coarser constraints until one applies. Thus, the finest applicable level of constraint
+#' is chosen. Threshold selection order is as follows (1 being the finest possible constraint):
 #' 6. Realm, annual
 #' 5. Realm, seasonal
 #' 4. Site-specific, annual
@@ -17,39 +17,39 @@
 #' 1. Sensor-specific, seasonal
 #'
 #'
-#' @param DirIn Character value. The input path to the data from a single sensor or location, structured as follows: 
-#' #/pfs/BASE_REPO/#/yyyy/mm/dd/#/id, where # indicates any number of parent and child directories 
-#' of any name, so long as they are not 'pfs' or recognizable as the 'yyyy/mm/dd' structure which indicates 
+#' @param DirIn Character value. The input path to the data from a single sensor or location, structured as follows:
+#' #/pfs/BASE_REPO/#/yyyy/mm/dd/#/id, where # indicates any number of parent and child directories
+#' of any name, so long as they are not 'pfs' or recognizable as the 'yyyy/mm/dd' structure which indicates
 #' the 4-digit year, 2-digit month, and' 2-digit day. The id is the unique identifier of the sensor or location. \cr
 #'
 #' Nested within this path are the folders:
 #'         /threshold
-#'         /location 
-#' The location folder holds at least 1 json file with location data/properties specific to the location 
-#' identifier in the directory path. If there is more than one file in this directory, only the first will 
-#' be used, since the properties of the named location (i.e. site) should be the same across files. 
-#' The threshold folder contains a json file with available thresholds from which the threshold selection 
+#'         /location
+#' The location folder holds at least 1 json file with location data/properties specific to the location
+#' identifier in the directory path. If there is more than one file in this directory, only the first will
+#' be used, since the properties of the named location (i.e. site) should be the same across files.
+#' The threshold folder contains a json file with available thresholds from which the threshold selection
 #' order will be used to choose from.
 #'
-#' @param DirOutBase Character value. The output path that will replace the #/pfs/BASE_REPO portion of DirIn. 
+#' @param DirOutBase Character value. The output path that will replace the #/pfs/BASE_REPO portion of DirIn.
 #'
-#' @param thshRaw List of thresholds to select from, as produced by reading a json formatted threshold file with 
+#' @param thshRaw List of thresholds to select from, as produced by reading a json formatted threshold file with
 #' rjson::fromJSON (see example)
-#' 
-#' @param thshPosx The list of thresholds thshRaw after passing it through NEONprocIS.qaqc::def.read.thsh.qaqc.list 
+#'
+#' @param thshPosx The list of thresholds thshRaw after passing it through NEONprocIS.qaqc::def.read.thsh.qaqc.list
 #' in order to turn the timestamps to POSIXct (see example)
-#' 
-#' @param ParaThsh A list of lists, each specifying a combination of term and context(s) to select thresholds for 
-#' the day and location. Applicable thresholds will be selected separately for each sub-list and combined when written to file. 
+#'
+#' @param ParaThsh A list of lists, each specifying a combination of term and context(s) to select thresholds for
+#' the day and location. Applicable thresholds will be selected separately for each sub-list and combined when written to file.
 #' Each sub-list contains two named elements:\cr
 #' \code{Term} Character value. The term (i.e. variable) to select thresholds for. For example: 'temp'. \cr
-#' \code{Ctxt} Character vector. Any number of contexts that apply to the term in order to define its application. 
-#' For example, c('soil','deep') would indicate to select thresholds for term 'temp' matching both contexts 'soil', and 
-#' 'deep'. Enter NULL if there are no applicable contexts, meaning that the term stands alone. 
+#' \code{Ctxt} Character vector. Any number of contexts that apply to the term in order to define its application.
+#' For example, c('soil','deep') would indicate to select thresholds for term 'temp' matching both contexts 'soil', and
+#' 'deep'. Enter NULL if there are no applicable contexts, meaning that the term stands alone.
 #' Note that AND logic applies within each sub-list (matching term and all contexts).
-#' 
-#' @param DirSubCopy (optional) Character vector. The names of additional subfolders at 
-#' the same level as the location folder in the input path that are to be copied with a symbolic link to the 
+#'
+#' @param DirSubCopy (optional) Character vector. The names of additional subfolders at
+#' the same level as the location folder in the input path that are to be copied with a symbolic link to the
 #' output path (i.e. not combined but carried through as-is).
 
 #' @param log A logger object as produced by NEONprocIS.base::def.log.init to produce structured log
@@ -57,12 +57,12 @@
 #' for more details.
 
 #' @return A directory structure created in DirOutBase, where the terminal directory of DirOutBase
-#' replaces BASE_REPO of DirIn but otherwise retains the child directory structure of the input path. The filtered 
-#' thresholds containing the thresholds applicable to each location, day, and sets of term and context(s) 
-#' will be written to thresholds.json file in the same format as input thshRaw and placed in an additional subdirectory 
-#' called 'thresholds' at the same level as the 
+#' replaces BASE_REPO of DirIn but otherwise retains the child directory structure of the input path. The filtered
+#' thresholds containing the thresholds applicable to each location, day, and sets of term and context(s)
+#' will be written to thresholds.json file in the same format as input thshRaw and placed in an additional subdirectory
+#' called 'thresholds' at the same level as the
 #' location directory. Be sure to specify "DirSubCopy=location" to pass the location directory through
-#' to the output (along with any other desired subdirectories to copy over. Otherwise, they will be dropped. 
+#' to the output (along with any other desired subdirectories to copy over. Otherwise, they will be dropped.
 
 #' @references
 #' License: (example) GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
@@ -75,7 +75,7 @@
 #' FileThsh <- "~/pfs/tempSoil_threshold_filter/thresholds.json"
 #' thshRaw <- rjson::fromJSON(file=FileThsh,simplify=TRUE),silent=FALSE)
 #' thsh <- NEONprocIS.qaqc::def.read.thsh.qaqc.list(listThsh=thshRaw) # This turns dates to POSIXct, which is required
-#' ParaThsh <- list(list(Term='temp',Ctxt='soil')) 
+#' ParaThsh <- list(list(Term='temp',Ctxt='soil'))
 #' wrap.thsh.slct(DirIn="~/pfs/tempSoil_locations/prt/2020/01/01/CFGLOC12345",
 #'                DirOutBase="~/pfs/out",
 #'                thsh=thsh,
@@ -95,14 +95,19 @@ test_that("Unit test of wrap.thsh.slct.R", {
   library(stringr)
   
   FileThsh <- "pfs/thresholds.json"
-  thshRaw <- rjson::fromJSON(file=FileThsh,simplify=TRUE)
+  thshRaw <- rjson::fromJSON(file = FileThsh, simplify = TRUE)
   # This turns dates to POSIXct, which is required
-  thshPosx <- NEONprocIS.qaqc::def.read.thsh.qaqc.list(listThsh=thshRaw) 
-  ParaThsh <- list(list(Term='relativeHumidity',Ctxt=c('relative-humidity','terrestrial'))) 
+  thshPosx <-
+    NEONprocIS.qaqc::def.read.thsh.qaqc.list(listThsh = thshRaw)
+  ParaThsh <-
+    list(list(
+      Term = 'relativeHumidity',
+      Ctxt = c('relative-humidity', 'terrestrial')
+    ))
   
-  DirIn="pfs/hmp_locations/2020/01/02/CFGLOC101252"
+  DirIn = "pfs/hmp_locations/2020/01/02/CFGLOC101252"
   
-  DirOutBase="pfs/out"
+  DirOutBase = "pfs/out"
   
   if (dir.exists(DirOutBase)) {
     unlink(DirOutBase, recursive = TRUE)
@@ -115,12 +120,59 @@ test_that("Unit test of wrap.thsh.slct.R", {
     cmdSymbLink <- base::paste0('rm ', base::paste0(DirSrc))
     rmSymbLink <- base::lapply(cmdSymbLink, base::system)
   }
-  wrap.thsh.slct(DirIn=DirIn,
-                 DirOutBase=DirOutBase,
-                 thshRaw=thshRaw,
-                 thshPosx=thshPosx,
-                 ParaThsh=ParaThsh,
-                 DirSubCopy='location'
-   )
+  # Test 1, Happy path
+  wrap.thsh.slct(
+    DirIn = DirIn,
+    DirOutBase = DirOutBase,
+    thshRaw = thshRaw,
+    thshPosx = thshPosx,
+    ParaThsh = ParaThsh,
+    DirSubCopy = 'location'
+  )
+  # Test 2 no location files
   
- })
+  if (dir.exists(DirOutBase)) {
+    unlink(DirOutBase, recursive = TRUE)
+  }
+  
+  DirIn_noLoc = "pfs/locations_nofiles/prt/10312"
+  try(wrap.thsh.slct(
+    DirIn = DirIn_noLoc,
+    DirOutBase = DirOutBase,
+    thshRaw = thshRaw,
+    thshPosx = thshPosx,
+    ParaThsh = ParaThsh
+  ),
+  silent = TRUE)
+  
+  DirSrc = "10312"
+  exstDirSrc <- base::unlist(base::lapply(DirSrc, base::dir.exists))
+  
+  if (exstDirSrc) {
+    cmdSymbLink <- base::paste0('rm ', base::paste0(DirSrc))
+    rmSymbLink <- base::lapply(cmdSymbLink, base::system)
+  }
+  
+  # Test 3, 2 location files
+  
+  if (dir.exists(DirOutBase)) {
+    unlink(DirOutBase, recursive = TRUE)
+  }
+  
+  DirIn_2Locs = "pfs/hmp_locations_2files/hmp155/10267"
+  try(wrap.thsh.slct(
+    DirIn = DirIn_2Locs,
+    DirOutBase = DirOutBase,
+    thshRaw = thshRaw,
+    thshPosx = thshPosx,
+    ParaThsh = ParaThsh
+  ),
+  silent = TRUE)
+  DirSrc = "10267"
+  exstDirSrc <- base::unlist(base::lapply(DirSrc, base::dir.exists))
+  
+  if (exstDirSrc) {
+    cmdSymbLink <- base::paste0('rm ', base::paste0(DirSrc))
+    rmSymbLink <- base::lapply(cmdSymbLink, base::system)
+  }
+})
