72b6c6528e212c0f7b59b1482a1f39486b2f20fd
Liangying Zhang <lzhang@battelleecology.org>
2022-03-14 10:40:50 -0600

    pipelines for mti300ahrs

--- flow/flow_sae_trst_dp0p/csat3.py
+++ flow/flow_sae_trst_dp0p/csat3.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-from pathlib import Path
 from structlog import get_logger
 
-import environs
 import math
-import os
 import pandas as pd
 
 from flow_sae_trst_dp0p.l0tol0p import L0toL0p
-from flow_sae_trst_dp0p.shared_functions import get_temp_kelvin, get_pressure_pa, mmol_to_mol, umol_to_mol
 from flow_sae_trst_dp0p.shared_functions import get_nth_bit, get_range_bits
-from flow_sae_trst_dp0p.cal_flags import get_cal_val_flags
-from flow_sae_trst_dp0p import log_config
 
 log = get_logger()
 
@@ -22,9 +16,7 @@ class Csat3(L0toL0p):
     cpdry = 1004.64
     mdry = 28.9645
     r = 8314.4621
-    data_columns = ['readout_time', 'veloXaxs', 'veloYaxs', 'veloZaxs', 'veloSoni', 'tempSoni', 'idx',
-                    'qfSoniUnrs', 'qfSoniData', 'qfSoniTrig', 'qfSoniComm', 'qfSoniCode',
-                    'qfSoniTemp', 'qfSoniSgnlPoor', 'qfSoniSgnlHigh', 'qfSoniSgnlLow']
+    data_columns = ['readout_time', 'veloXaxs', 'veloYaxs', 'veloZaxs', 'veloSoni']
 
     def data_conversion(self, filename: str) -> pd.DataFrame:
         df = pd.read_parquet(filename)
@@ -32,21 +24,22 @@ class Csat3(L0toL0p):
         log.debug(f'{outputdf.columns}')
         log.info(df['site_id'][0])
         log.info(df['source_id'][0])
-        outputdf['tempSoni'] = outputdf['speed_of_sound'].apply(lambda x: math.nan if math.isnan(x) else self.get_temp_soni(x))
-        outputdf['index'] = outputdf['diagnostic_word'].apply(lambda x: math.nan if math.isnan(x) else get_range_bits(x, 0, 5))
-        outputdf['qfSoniUnrs'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == -99999 else 0)
-        outputdf['qfSoniData'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == 61503 else 0)
-        outputdf['qfSoniTrig'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == 61440 else 0)
-        outputdf['qfSoniComm'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == 61441 else 0)
-        outputdf['qfSoniCode'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == 61442 else 0)
-        outputdf['qfSoniTemp'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else get_nth_bit(x, 15))
-        outputdf['qfSoniSgnlPoor'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else get_nth_bit(x, 14))
-        outputdf['qfSoniSgnlHigh'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else get_nth_bit(x, 13))
-        outputdf['qfSoniSgnlLow'] = outputdf['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else get_nth_bit(x, 12))
         del outputdf['source_id']
         del outputdf['site_id']
         del outputdf['diagnostic_word']
         outputdf.columns = self.data_columns
+
+        outputdf['tempSoni'] = df['speed_of_sound'].apply(lambda x: math.nan if math.isnan(x) else self.get_temp_soni(x))
+        outputdf['index'] = df['diagnostic_word'].apply(lambda x: math.nan if math.isnan(x) else get_range_bits(x, 0, 5))
+        outputdf['qfSoniUnrs'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == -99999 else 0)
+        outputdf['qfSoniData'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == 61503 else 0)
+        outputdf['qfSoniTrig'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == 61440 else 0)
+        outputdf['qfSoniComm'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == 61441 else 0)
+        outputdf['qfSoniCode'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else 1 if x == 61442 else 0)
+        outputdf['qfSoniTemp'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else get_nth_bit(x, 15))
+        outputdf['qfSoniSgnlPoor'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else get_nth_bit(x, 14))
+        outputdf['qfSoniSgnlHigh'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else get_nth_bit(x, 13))
+        outputdf['qfSoniSgnlLow'] = df['diagnostic_word'].apply(lambda x: -1 if math.isnan(x) else get_nth_bit(x, 12))
         return outputdf
 
     def get_temp_soni(self, veloSoni: float) -> float:
