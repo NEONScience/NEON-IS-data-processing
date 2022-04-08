b06a7f5e81ce8907025c9ae4fe7089b1fa7968d9
choim <choim@battelleecology.org>
2022-04-08 15:43:27 -0600

    added NULL tests to def.schm.avro.pars.R unit test

--- pack/NEONprocIS.base/tests/testthat/test-def.schm.avro.pars.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.schm.avro.pars.R
@@ -43,3 +43,16 @@ test_that("when avro schm is pass, return all the elements in the schema",
             expect_true (typeof(rpt$var[3]$doc[3]) == "character")
           })
 
+test_that("when avro schm has NULL values",
+          {
+            FileSchm <- "def.schm.avro.pars/prt_calibrated_NULL.avsc"
+            rpt <- NEONprocIS.base::def.schm.avro.pars(FileSchm = FileSchm)
+            # testthat::expect_true(is.list(rpt))
+            # expect_true (length(rpt$schmJson) == 1)
+            # expect_true (length(rpt$schmList) == 5)
+            # expect_true (length(rpt$var) == 3)
+            # expect_true (rpt$var[1]$name[1] == 'source_id')
+            # expect_true (rpt$var[2]$type[3] == "long|timestamp-millis")
+            # expect_true (typeof(rpt$var[3]$doc[3]) == "character")
+          })
+
