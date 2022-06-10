9820d123c759f3a5c8b2d38fc4a30dfe0b382eb0
visalachundru <vchundru@den-devissom-1.ci.neoninternal.org>
2022-06-10 11:35:20 -0600

    added new pipeline for assetuid_macaddress_mapping

--- modules/assetuid_macaddress_mapping_loader/assetuid_macaddress_loader.py
+++ modules/assetuid_macaddress_mapping_loader/assetuid_macaddress_loader.py
@@ -3,42 +3,48 @@ import os
 import logging
 from pathlib import Path
 from contextlib import closing
-
+from structlog import get_logger
 import environs
-from botocore.exceptions import ClientError
-import psycopg2
 import requests
 import json
 import argparse
+import common.log_config as log_config
 
-log = logging.getLogger(__name__)
+
+# log = get_logger()
 
 
-def load(outputpath, region, sourcetype) -> None:
+def load() -> None:
+    env = environs.Env
+    out_path: Path = env.path('OUTPUT_PATH')
+    region: str = env.path('REGION')
+    log_level: str = env.log_level('LOG_LEVEL', 'DEBUG')
+    log_config.configure(log_level)
+    log.debug(f'out_path: {out_path}')
+    source_type: str = env.str('SOURCE_TYPE')
     urlpath = ''
+
     if (region == "int"):
         urlpath = f"http://den-intcdsllb-1.ci.neoninternal.org/cdsWebApp/assets?sensor-type-name={sourcetype}"
-    elif(region == "cert"):
+    elif (region == "cert"):
         urlpath = f"http://den-certcdsllb-1.ci.neoninternal.org/cdsWebApp/assets?sensor-type-name={sourcetype}"
-    elif(region == "prod"):
+    elif (region == "prod"):
         urlpath = f"http://den-prodcdsllb-1.ci.neoninternal.org/cdsWebApp/assets?sensor-type-name={sourcetype}"
     else:
         urlpath = f"http://den-intcdsllb-1.ci.neoninternal.org/cdsWebApp/assets?sensor-type-name={sourcetype}"
-    print(f"urlpath is {urlpath}")
-
-
+    log.debug(f"urlpath is {urlpath}")
     response = requests.get(urlpath, headers={'Accept': 'application/json'})
     if response.status_code == 200:
-        print("sucessfully fetched the data")
+        log.debug("sucessfully fetched the data")
         assets = []
         assets = response.json()['assets']
-        print('assets details are:')
+        log.debug('assets details are:')
         for asset in assets:
             try:
                 asset_uid = asset.get("assetUid")
                 mac_address = asset.get("macAddress")
                 if (mac_address is None):
-                    print(f'Empty mac_address for assetuid {asset.get("assetUid")}')
+                    log.debug(f'Empty mac_address for assetuid {asset.get("assetUid")}')
                     continue
 
                 file_name = f'{sourcetype}_{mac_address}.txt'
@@ -56,12 +62,13 @@ def load(outputpath, region, sourcetype) -> None:
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--output_path')
-    parser.add_argument('--region')
-    parser.add_argument('--source_type')
-    args = parser.parse_args()
-    outputpath = args.output_path
-    region = args.region
-    sourcetype = args.source_type
-    load(outputpath, region, sourcetype)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--output_path')
+    # parser.add_argument('--region')
+    # parser.add_argument('--source_type')
+    # args = parser.parse_args()
+    # outputpath = args.output_path
+    # region = args.region
+    # sourcetype = args.source_type
+
+    load()
