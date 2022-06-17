35775ec1d6771d71f6f988884f43abbbb812a611
visalachundru <vchundru@den-devissom-1.ci.neoninternal.org>
2022-06-17 17:07:37 -0600

    creating new pipeline

--- modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
+++ modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
@@ -9,47 +9,38 @@ import requests
 import json
 import argparse
 import common.log_config as log_config
-
+import os
 
 log = get_logger()
 
 
 def load() -> None:
     env = environs.Env
-    data_path: Path = env.path('DATA_PATH')
-    map_path: Path = env.path(MAP_PATH)
-    out_path: Path = os.environ['OUTPUT_PATH']
+    data_path: Path = os.environ['DATA_PATH']
+    map_path: Path = os.environ['MAP_PATH']
+    out_path: Path = os.environ['OUT_PATH']
     log_level: str = os.environ['LOG_LEVEL']
     log_config.configure(log_level)
     log.debug(f'out_path: {out_path}')
+
     
-    response = requests.get(urlpath, headers={'Accept': 'application/json'})
-    if response.status_code == 200:
-        log.debug("sucessfully fetched the data")
-        assets = []
-        assets = response.json()['assets']
-        log.debug('assets details are:')
-        for asset in assets:
-            try:
-                asset_uid = asset.get("assetUid")
-                mac_address = asset.get("macAddress")
-                if (mac_address is None):
-                    log.debug(f'Empty mac_address for assetuid {asset.get("assetUid")}')
-                    continue
-
-                file_name = f'{source_type}_{mac_address}.txt'
-                file_path = Path(out_path, source_type, mac_address, file_name)
-                # print(f"filePath to write is: {file_path}")
-                file_path.parent.mkdir(parents=True, exist_ok=True)
-                with open(file_path, 'w') as asset_macaddress_file:
-                    asset_macaddress_file.write(str(asset_uid))
-            except BaseException as err:
-                print(f"Unexpected {asset_uid}, {mac_address}")
-                raise
-
-    else:
-        print(f"Asset call failed with error code: {response.status_code} error with your request")
-
+   # for path in map_path.rglob('*'):
+    for path in map_path:
+        print(f'map path value is: {path}')
+        if path.is_file():
+            mac_address = os.path.basename(os.path.dirname(path))
+            with open(path, 'r') as f:
+                asset_uid = f.readlines()
+        for dpath in data_path.rglob('*'):
+            if dpath.is_file():
+                    if (Path(dpath).stem.split("_")[1] == mac_address):
+                        new_file_path = Path(out_path, dpath)
+                        new_file_path.replace(mac_address, asset_uid)
+                        new_file_path.parent.mkdir(parents=True, exist_ok=True)
+                        if not new_file_path.exists():
+                          log.debug(f'original path: {dpath} new_path: {new_file_path}')
+                          new_file_path.symlink_to(new_file_path)
+                          
 
 if __name__ == '__main__':
     load()
