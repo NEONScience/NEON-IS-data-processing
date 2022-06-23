d394959f3565197f8c299eaafb9e5aa0c15df2e7
visalachundru <vchundru@den-devissom-1.ci.neoninternal.org>
2022-06-23 09:56:10 -0600

    bug fixes

--- modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
+++ modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
@@ -22,25 +22,33 @@ def load() -> None:
     log_level: str = os.environ['LOG_LEVEL']
     log_config.configure(log_level)
     log.debug(f'out_path: {out_path}')
-
-    
-   # for path in map_path.rglob('*'):
-    for path in map_path:
-        print(f'map path value is: {path}')
+    print(f'final output path is: {Path(out_path)}')
+    for path in Path(map_path).rglob('*'):
+        path = Path(path)
         if path.is_file():
+            print(f'map path file name is: {path}')
             mac_address = os.path.basename(os.path.dirname(path))
+            print(f'Mac_Address name is: {mac_address}')
             with open(path, 'r') as f:
-                asset_uid = f.readlines()
-        for dpath in data_path.rglob('*'):
-            if dpath.is_file():
-                    if (Path(dpath).stem.split("_")[1] == mac_address):
-                        new_file_path = Path(out_path, dpath)
-                        new_file_path.replace(mac_address, asset_uid)
+                asset_uid = f.read().split()[0]
+                print(f'Asset_UID name is: {asset_uid}')
+            for dpath in Path(data_path).rglob('*'):
+                if dpath.is_file():
+                    new_dpath = os.path.normpath(dpath)
+                    print(f'data path file name is: {new_dpath}')
+                    path_list = new_dpath.split(os.sep)
+                    
+                    if (dpath.stem.split("_")[1] == mac_address):
+                        new_file_path = os.path.join(Path(out_path), os.sep.join(path_list[3:]))
+                        print(f'Final output path is {new_file_path}')
+                        str(new_file_path).replace(str(mac_address), str(asset_uid))
+                        print(f'New file path after converting mac address to assetuid is: {new_file_path}')
                         new_file_path.parent.mkdir(parents=True, exist_ok=True)
                         if not new_file_path.exists():
-                          log.debug(f'original path: {dpath} new_path: {new_file_path}')
-                          new_file_path.symlink_to(new_file_path)
-                          
+                            print(f'original path: {dpath} new_path: {new_file_path}')
+                            log.debug(f'original path: {dpath} new_path: {new_file_path}')
+                            new_file_path.symlink_to(new_file_path)
+
 
 if __name__ == '__main__':
     load()
