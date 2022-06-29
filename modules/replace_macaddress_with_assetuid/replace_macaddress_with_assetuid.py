62367bb64c14ea7e80d7ca18b17f4360b0f0fd4c
visalachundru <vchundru@den-devissom-1.ci.neoninternal.org>
2022-06-28 19:32:22 -0600

    removed some of the print statements

--- modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
+++ modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
@@ -25,10 +25,10 @@ def load() -> None:
 
     for dpath in Path("/pfs/DATA_PATH/").rglob('*'):
         if Path(dpath).is_file():
-            print(f'you are inside file and the file name is : {dpath}')
+            # print(f'you are inside file and the file name is : {dpath}')
             mac_address = Path(dpath).stem.split("_")[1]
             new_dpath = os.path.normpath(dpath)
-            print(f'data path file name is: {new_dpath}')
+            # print(f'data path file name is: {new_dpath}')
             print(f'Mac_Address name is: {mac_address}')
             path_list = new_dpath.split(os.sep)
             for mpath in Path(map_path).rglob('*'):
@@ -40,9 +40,8 @@ def load() -> None:
                             print(f'Asset_UID name is: {asset_uid}')
                         f.close()
                         extended_path = os.sep.join(path_list[3:]).replace(str(mac_address), str(asset_uid))
-                        print(f'Extended Path is: {extended_path}')
+                        # print(f'Extended Path is: {extended_path}')
                         new_file_path = Path(os.path.join(Path(out_path), extended_path))
-                        print(f'New file path after converting mac address to assetuid is: {new_file_path}')
                         Path(new_file_path).parent.mkdir(parents=True, exist_ok=True)
                         if not new_file_path.exists():
                             print(f'original path: {dpath} new_path: {new_file_path}')
