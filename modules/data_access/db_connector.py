907eddd6f31082f086b2d1f0ac706c4da7366e3a
Robert Markel <rmarkel@battelleecology.org>
2022-07-20 15:05:01 -0600

    Fixed documentation strings.

--- modules/data_access/db_connector.py
+++ modules/data_access/db_connector.py
@@ -1,15 +1,13 @@
-from typing import Any
-
 import psycopg2
-from psycopg2.extensions import parse_dsn
+from psycopg2.extensions import parse_dsn, connection
 
 
-def connect(db_url: str) -> Any:
+def connect(db_url: str) -> connection:
     """
-    Return a database connection based on the input database URL.
+    Return a database connection based on the input URL.
 
-    @param db_url: The database URL.
-    @returns: The connection.
+    :param db_url: The database URL.
+    :returns: The connection.
     """
     db_params = parse_dsn(db_url)
     return psycopg2.connect(**db_params)
