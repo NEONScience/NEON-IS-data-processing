08f43164d20a8fee84c41d4ad0f2e8046a7c0ee1
choim <choim@battelleecology.org>
2022-10-18 17:53:18 -0600

    group_loader and group_loader_main updated

--- modules/group_loader/group_loader.py
+++ modules/group_loader/group_loader.py
@@ -5,28 +5,29 @@ from typing import Callable, Iterator, Set
 import geojson
 import structlog
 
-from data_access.types.named_location import NamedLocation
+from data_access.types.group import Group
 import data_access.types.geojson_converter as geojson_converter
 
 log = structlog.get_logger()
 
 
-def load_locations(out_path: Path, get_locations: Callable[[str], Iterator[NamedLocation]], source_type: str) -> None:
+def load_groups(out_path: Path, get_group: Callable[[str], Iterator[Group]], group_prefix: str) -> None:
     """
     Write location files into the output path.
 
     :param out_path: The path for writing files.
-    :param get_locations: A function yielding named locations.
-    :param source_type: sensor type.
+    :param get_group: A function yielding groups.
+    :param group_prefix: group_prefix.
     """
-    for named_location in get_locations(source_type=source_type):
-        schema_names: Set = named_location.schema_names
-        location_name: str = named_location.name
+    for group in get_group(group_prefix=group_prefix):
+#        schema_names: Set = get_named_location_schema_name()
+        location_name: str = group.name
         for schema_name in schema_names:
-            path = Path(out_path, schema_name, location_name, f'{location_name}.json')
+            path = Path(out_path, schema_name, f'group_loader', group_prefix[:-1], location_name, f'{location_name}.json')
             path.parent.mkdir(parents=True, exist_ok=True)
             geojson_data = geojson_converter.convert_named_location(named_location)
             file_data = geojson.dumps(geojson_data, indent=4, sort_keys=False, default=str)
             with open(path, 'w') as file:
                 log.debug(f'writing file: {path}')
-                file.write(file_data)
+                file.write(file_data)   
+     
--- modules/group_loader/group_loader_main.py
+++ modules/group_loader/group_loader_main.py
@@ -7,25 +7,23 @@ from functools import partial
 
 import common.log_config as log_config
 from data_access.db_connector import connect
-from data_access.get_named_locations import get_named_locations
-from location_loader.location_loader import load_locations
+from data_access.get_groups import get_groups
+from group_loader.group_loader import load_groups
 
 log = structlog.get_logger()
 
 
 def main() -> None:
     env = environs.Env()
-    location_type: str = env.str('LOCATION_TYPE')
-    source_type: str = env.str('SOURCE_TYPE')
+    group_prefix: str = env.str('GROUP_PREFIX')
     db_url: str = env.str('DATABASE_URL')
     out_path: Path = env.path('OUT_PATH')
     log_level: str = env.log_level('LOG_LEVEL', 'INFO')
     log_config.configure(log_level)
 
     with closing(connect(db_url)) as connection:
-        get_named_locations_partial = partial(get_named_locations, connection=connection, location_type=location_type)
-        load_locations(out_path=out_path, get_locations=get_named_locations_partial, source_type=source_type)
-
+        get_groups_partial = partial(get_groups, connection=connection)
+        load_groups(out_path=out_path, get_group=get_groups_partial, group_prefix=group_prefix)
 
 if __name__ == "__main__":
     main()
