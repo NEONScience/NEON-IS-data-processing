59ad527d30a46cd151c6fff3889351aabb814367
choim <choim@battelleecology.org>
2022-10-18 09:21:37 -0600

    initial checkin for group_loader

---
+++ modules/group_loader/group_loader_main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import environs
+import structlog
+from pathlib import Path
+from contextlib import closing
+from functools import partial
+
+import common.log_config as log_config
+from data_access.db_connector import connect
+from data_access.get_named_locations import get_named_locations
+from location_loader.location_loader import load_locations
+
+log = structlog.get_logger()
+
+
+def main() -> None:
+    env = environs.Env()
+    location_type: str = env.str('LOCATION_TYPE')
+    source_type: str = env.str('SOURCE_TYPE')
+    db_url: str = env.str('DATABASE_URL')
+    out_path: Path = env.path('OUT_PATH')
+    log_level: str = env.log_level('LOG_LEVEL', 'INFO')
+    log_config.configure(log_level)
+
+    with closing(connect(db_url)) as connection:
+        get_named_locations_partial = partial(get_named_locations, connection=connection, location_type=location_type)
+        load_locations(out_path=out_path, get_locations=get_named_locations_partial, source_type=source_type)
+
+
+if __name__ == "__main__":
+    main()
