477ab6145628cd3a9d8d3e34e5acc8022a8c3ccf
choim <choim@battelleecology.org>
2022-10-24 12:33:58 -0600

    add get_groups.py back

--- modules/group_loader/group_loader.py
+++ modules/group_loader/group_loader.py
@@ -11,7 +11,7 @@ import data_access.types.geojson_converter as geojson_converter
 log = structlog.get_logger()
 
 
-def load_groups(out_path: Path, get_groups: Callable[[str], Iterator[Group]], group_prefix: str) -> None:
+def load_groups(out_path: Path, get_group: Callable[[str], Iterator[Group]], group_prefix: str) -> None:
     """
     Write location files into the output path.
 
@@ -20,14 +20,14 @@ def load_groups(out_path: Path, get_groups: Callable[[str], Iterator[Group]], gr
     :param group_prefix: group_prefix.
     """
     group_prefix_1 = group_prefix[:-1]
-    for group_name in get_group_names(group_prefix=group_prefix):
-        group_name: str = group.name
-        for member_name in get_member_group_names(group_prefix=group_prefix):
-            path = Path(out_path, group_prefix_1, member_name, f'{member_name}.json')
-            path.parent.mkdir(parents=True, exist_ok=True)
-            geojson_data = geojson_converter.convert_group(group_name)
-            file_data = geojson.dumps(geojson_data, indent=4, sort_keys=False, default=str)
-            with open(path, 'w') as file:
-                log.debug(f'writing file: {path}')
-                file.write(file_data)   
+    for group in get_group(group_prefix=group_prefix):
+        member_name: str = group.name
+        group_name str = group.group
+        path = Path(out_path, group_prefix_1, member_name, f'{member_name}.json')
+        path.parent.mkdir(parents=True, exist_ok=True)
+        geojson_data = geojson_converter.convert_group(group)
+        file_data = geojson.dumps(geojson_data, indent=4, sort_keys=False, default=str)
+        with open(path, 'w') as file:
+            log.debug(f'writing file: {path}')
+            file.write(file_data)   
      
