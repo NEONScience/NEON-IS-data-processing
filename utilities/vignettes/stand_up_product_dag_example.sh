36720471e7e43857e2b34004b5ce12db378a5958
covesturtevant <csturtevant@battelleecology.org>
2022-11-29 14:32:40 -0700

    update vignettes for combined module approach

--- utilities/vignettes/stand_up_product_dag_example.sh
+++ utilities/vignettes/stand_up_product_dag_example.sh
@@ -16,27 +16,20 @@ source_type='hmp155'
 product='relHumidity'
 
 # Define paths based on base paths and product information above 
-spec_path_l0=$git_path_pipelines/l0_data_loader
 spec_path_source_type=$git_path_pipelines/$source_type
 spec_path_product=$git_path_pipelines/$product
 
 # Create sensor-specific import_trigger with a small amount of data
-pachctl create repo import_trigger_$source_type
-pachctl start commit import_trigger_$source_type@master
+pachctl create repo $source_type'_import_trigger'
+pachctl start commit $source_type'_import_trigger'@master
 sites=(CPER HARV BARC ARIK) # Edit as desired
 days=(01 02 03) # Edit as desired. Jan 2020 is hardcoded throughout this code
 for site in $(echo ${sites[*]}); do 
 for day in $(echo ${days[*]}); do 
-pachctl put file import_trigger_$source_type@master:/2020/01/$day/$site -f $data_path/import_trigger_FULL/2020/01/$day/$site
+pachctl put file $source_type'_import_trigger'@master:/2020/01/$day/$site -f $data_path/import_trigger_FULL/2020/01/$day/$site
 done
 done
-pachctl finish commit import_trigger_$source_type@master
-
-# Load L0 data
-# *** First, create/edit/update all pipeline specs ***
-# Note, if your data_source_SOURCETYPE_site pipeline spec is still json, use the converter at: https://www.json2yaml.com/
-pachctl create pipeline -f $spec_path_l0/'data_source_'$source_type'_site.yaml'
-pachctl create pipeline -f $spec_path_l0/'data_source_'$source_type'_linkmerge.yaml'
+pachctl finish commit $source_type'_import_trigger'@master
 
 # Load in calibrations (must be stored locally. Ideally, set up the python loader instead.
 pachctl create repo calibration_$source_type
@@ -81,9 +74,9 @@ done
 
 # Now run the daily cron pipeline to initialize it.
 # Then stop it almost immediately after (so it doesn't run every day until we want it to)
-pachctl run cron cron_daily_$source_type
+pachctl run cron $source_type'_cron_daily_and_date_control'
 # Wait a few minutes and ensure the job runs, then...
-pachctl stop pipeline cron_daily_$source_type
+pachctl stop pipeline $source_type'_cron_daily_and_date_control'
 
 
 # Set up product pipeline
@@ -103,25 +96,26 @@ done
 
 # Bump to full scale for a few days
 # *** First - edits the date_gap_filler pipeline spec to remove restriction on particular CFGLOCs. ***
-# *** Also make any desired edits to the date_gap_filler_limiter pipeline spec to adjust date range to the dates of the import trigger 
+# *** Also make any desired edits to the cron_daily_and_date_control pipeline spec to adjust date range to the dates of the import trigger 
 pachctl start transaction
-pachctl update pipeline --reprocess -f $spec_path_source_type/$source_type'_date_gap_filler_limiter.yaml'
-pachctl update pipeline -f $spec_path_source_type/$source_type'_date_gap_filler.yaml'
-pachctl start commit import_trigger_$source_type@master
+pachctl update pipeline --reprocess -f $spec_path_source_type/$source_type'_cron_daily_and_date_control.yaml'
+pachctl update pipeline -f $spec_path_source_type/$source_type'_fill_date_gaps_and_regularize.yaml'
+pachctl start commit $source_type'_import_trigger'@master
 pachctl finish transaction
 for day in $(echo ${days[*]}); do 
-pachctl put file -r import_trigger_$source_type@master:/2020/01/$day -f $data_path/import_trigger_FULL/2020/01/$day
+pachctl put file -r $source_type'_import_trigger'@master:/2020/01/$day -f $data_path/import_trigger_FULL/2020/01/$day
 done
-pachctl finish commit import_trigger_$source_type@master
+pachctl finish commit $source_type'_import_trigger'@master
 
 # Add 1 day at full scale to evaluate resource requests in normal operations
 # *** First - edit the date_gap_filler_limiter pipeline spec to add 1 day to the end date ***
 pachctl start transaction
-pachctl update pipeline --reprocess -f $spec_path_source_type/$source_type'_date_gap_filler_limiter.yaml'
-pachctl start commit import_trigger_$source_type@master
+pachctl update pipeline --reprocess -f $spec_path_source_type/$source_type'_cron_daily_and_date_control.yaml'
+pachctl start commit $source_type'_import_trigger'@master
 pachctl finish transaction
 days=(04) # Edit as desired
 for day in $(echo ${days[*]}); do 
-pachctl put file -r import_trigger_$source_type@master:/2020/01/$day -f $data_path/import_trigger_FULL/2020/01/$day
+pachctl put file -r $source_type'_import_trigger'@master:/2020/01/$day -f $data_path/import_trigger_FULL/2020/01/$day
 done
-pachctl finish commit import_trigger_$source_type@master
+pachctl finish commit $source_type'_import_trigger'@master
+
