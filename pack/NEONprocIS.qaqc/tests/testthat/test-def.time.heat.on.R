9e8112391b212574840ac2d970c0fe8429a090cc
choim <choim@battelleecology.org>
2022-12-07 19:06:15 -0700

    remove unused codes and update some comments

--- pack/NEONprocIS.qaqc/tests/testthat/test-def.time.heat.on.R
+++ pack/NEONprocIS.qaqc/tests/testthat/test-def.time.heat.on.R
@@ -36,7 +36,7 @@
 
 # changelog and author contributions / copyrights
 #   Mija Choi (2022-12-02)
-#     added to the original test
+#     modified the original test
 ###############################################################################################
 # Define test context
 context("\n   | Unit test of def.time.heat.on.R, determining discrete periods a heater was turned on based on heater events\n")
@@ -53,11 +53,9 @@ test_that("calculate the time on dataHeat State is always false",
             testthat::expect_true(is.list(rpt))
             expect_true(length(rpt) == 2)
             testthat::expect_equal(rpt$timeOff[1], as.POSIXct("2019-05-01 00:10:20",tz="GMT"))
-            #  testthat::expect_that(rpt[1]$timeOn[1], equals(base::as.POSIXct("NA")))
-
           }
-
 )
+
 test_that("calculate the time on dataHeat when TimeOffAuto is not null",
           {
             time1 <- base::as.POSIXct('2019-05-01 00:10:20', tz = 'GMT')
@@ -72,10 +70,9 @@ test_that("calculate the time on dataHeat when TimeOffAuto is not null",
             expect_true(length(rpt) == 2)
             testthat::expect_equal(rpt$timeOff[1], as.POSIXct("2019-05-02 00:48:28",tz="GMT"))
             testthat::expect_equal(rpt$timeOn[1],  as.POSIXct("2019-05-02 00:18:28",tz="GMT"))
-
           }
-
 )
+
 test_that("calculate the time on dataHeat when TimeOffAuto is null",
           {
             time1 <- base::as.POSIXct('2019-05-01 00:10:20', tz = 'GMT')
@@ -89,5 +86,4 @@ test_that("calculate the time on dataHeat when TimeOffAuto is null",
             expect_true(length(rpt) == 2)
             testthat::expect_equal(rpt$timeOn[1], as.POSIXct("2019-05-02 00:18:28",tz="GMT"))
           }
-
 )
