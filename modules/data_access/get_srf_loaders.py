c1cb035bfa96455a149a598dc959fa3230e3ff7a
choim <choim@battelleecology.org>
2023-01-24 11:10:48 -0700

    srf query is updated

--- modules/data_access/get_srf_loaders.py
+++ modules/data_access/get_srf_loaders.py
@@ -11,27 +11,27 @@ def get_srf_loaders(connector: DbConnector, group_prefix: str) -> Iterator[Srf]:
     Get science_review data for a group prefix, i.e., pressure-air_.
 
     :param connector: A database connector.
-    :param group_prefix: A group prefix.
+    :param group_prefix: A group prefix, i.e., 'surfacewater-physical_PRLA130100'.
     :return: The Srf.
     """
 
     sql = f'''
-         select 
+        select 
             g.group_name, sr.id, sr.start_date, sr.end_date, sr.meas_strm_name, 
             ms.term_name as "srfTermName", sr.srf, sr.user_comment, sr.create_date, sr.last_update
         from 
-            science_review sr, 
-            data_product_group dpg , 
-            "group" g, nam_locn nl, 
-            meas_strm ms  
-        where
-            g.named_location_id = nl.nam_locn_id 
+            "group" g , data_product_group dpg, nam_locn nl, meas_strm ms , science_review sr
+        where 
+            g.group_name like %s
         and 
-            ms.nam_locn_id = nl.nam_locn_id 
+            nl.nam_locn_id = g.named_location_id
         and 
-            substring  (sr.meas_strm_name from 15 for 13)  = substring (dpg.dp_idq  from 15 for 13 )
-         and 
-             g.group_name like %s
+            ms.nam_locn_id = nl.nam_locn_id
+        and 
+            sr.meas_strm_name = ms.meas_strm_name 
+        and 
+            substring (sr.meas_strm_name from 15 for 13) = substring (dpg.dp_idq  from 15 for 13 ) 
+             
      '''
     group_prefix_1 = group_prefix + '%'
     if group_prefix[-1] == "_":
@@ -45,19 +45,20 @@ def get_srf_loaders(connector: DbConnector, group_prefix: str) -> Iterator[Srf]:
             sr_id = row[1]
             start_date = row[2]
             end_date = row[3]
-            srfTermName = row[4]
-            srf = row[5]
-            user_comment = row[6]
-            create_date = row[7]
-            last_update = row[8]
+            meas_strm_name = row[4]
+            srfTermName = row[5]
+            srf = row[6]
+            user_comment = row[7]
+            create_date = row[8]
+            last_update = row[9]
             srf = Srf(group_name=group_name,
                       id=sr_id,
                       start_date=start_date,
                       end_date=end_date,
-                      measurement_stream_name=srfTermName,
+                      measurement_stream_name=meas_strm_name,
                       srf_term_name=srfTermName,
                       srf=srf,
                       user_comment=user_comment,
                       create_date=create_date,
                       last_update_date=last_update)
-            yield threshold
+            yield srf
