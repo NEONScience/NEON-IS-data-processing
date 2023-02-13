de9013079c29ae1f442ad1b16800a5d2b8d8d592
Robert Markel <rmarkel@battelleecology.org>
2023-02-13 14:33:50 -0700

    Minor cleanup.

--- modules/packager/packager.py
+++ modules/packager/packager.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 import pandas as pd
-import environs
 from sortedcontainers import SortedSet
 import datetime
 import hashlib
@@ -12,39 +11,32 @@ from structlog import get_logger
 log = get_logger()
 
 
-def package(*, prefix_index: int, prefix_length: int, sort_index: int) -> None:
+def package(*, data_path, out_path, prefix_index: int, prefix_length: int, sort_index: int) -> None:
     """
-    :param prefix_index: start index of prefix (the prefix typically corresponds to the glob, e.g. CPER/2019/01)
+    Bundles the required files into a package suitable for publication. Note the prefix_index
+    typically corresponds to the glob pattern in the pipeline specification file, e.g. CPER/2019/01.)
+
+    :param data_path: The input data path.
+    :param out_path: The output path for writing the package files.
+    :param prefix_index: start index of prefix
     :param prefix_length: number of indices in prefix
     :param sort_index: index of filename field to sort on (e.g. the day)
     """
-
-    data_path = os.environ['DATA_PATH']
-    out_path = os.environ['OUT_PATH']
-
-    # get the package prefix
-    dataPath = environs.Env().path('DATA_PATH')
-    dataPath_parts = dataPath.parts
-    prefix = os.path.join(*dataPath_parts[prefix_index:prefix_index+prefix_length])
-    prefix_field = '-'.join(dataPath_parts[prefix_index+1:prefix_index+prefix_length])
-
-    # store set of files to be collated into each package file
-    package_files = {}
-
-    # hasData by package file
-    hasDataByFile = {}
-
-    # package path by package file
-    packagePathByFile = {}
+    package_files = {}  # the set of files to be collated into each package file
+    has_data_by_file = {}  # has_data by package file
+    package_path_by_file = {}  # package path by package file
 
     # processing timestamp
     timestamp = datetime.datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
+
+    # get the package prefix
+    (prefix, prefix_field) = get_package_prefix(data_path, prefix_index, prefix_length)
 
     for root, dirs, files in os.walk(data_path):
         for file in files:
             # if this is the manifest, parse it
             if 'manifest' in file:
-                parse_manifest(os.path.join(root, file), hasDataByFile, sort_index, prefix_field, timestamp)
+                parse_manifest(os.path.join(root, file), has_data_by_file, sort_index, prefix_field, timestamp)
                 continue
             # get the package filename
             package_file = get_package_filename(file, sort_index, prefix_field, timestamp)
@@ -56,34 +48,20 @@ def package(*, prefix_index: int, prefix_length: int, sort_index: int) -> None:
 
     for package_file in package_files.keys():
         output_file = os.path.join(out_path, prefix, package_file)
-        packagePathByFile[package_file] = output_file
+        package_path_by_file[package_file] = output_file
         os.makedirs(os.path.join(out_path, prefix), exist_ok=True)
-        isFirstFile = True
+        is_first_file = True
         for file in package_files[package_file]:
             data = pd.read_csv(file)
             mode = 'a'
-            writeHeader = False
-            if isFirstFile:
+            write_header = False
+            if is_first_file:
                 mode = 'w'
-                writeHeader = True
-                isFirstFile = False
-            data.to_csv(output_file, mode=mode, header=writeHeader, index=False)
+                write_header = True
+                is_first_file = False
+            data.to_csv(output_file, mode=mode, header=write_header, index=False)
 
-    # write manifest
-    manifest_filepath = os.path.join(out_path, prefix, 'manifest.csv')
-    with open(manifest_filepath, 'w') as manifest_csv:
-        writer = csv.writer(manifest_csv)
-        writer.writerow(['file','hasData','size','checksum'])
-        for key in hasDataByFile.keys():
-            # get file size and checksum
-            package_path = packagePathByFile[key]
-            file_size = os.stat(package_path).st_size
-            md5_hash = hashlib.md5()
-            with open(package_path,"rb") as f:
-                for byte_block in iter(lambda: f.read(4096),b""):
-                    md5_hash.update(byte_block)
-            checksum = md5_hash.hexdigest()
-            writer.writerow([key, hasDataByFile[key], file_size, checksum])
+    write_manifest(out_path, prefix, has_data_by_file, package_path_by_file)
 
 
 def get_package_filename(file, sort_index, prefix_field, timestamp):
@@ -94,11 +72,35 @@ def get_package_filename(file, sort_index, prefix_field, timestamp):
     return package_file
 
 
-def parse_manifest(manifest_file, hasDataByFile, sort_index, prefix_field, timestamp):
+def get_package_prefix(data_path, prefix_index, prefix_length):
+    data_path_parts = data_path.parts
+    prefix = os.path.join(*data_path_parts[prefix_index: prefix_index + prefix_length])
+    prefix_field = '-'.join(data_path_parts[prefix_index + 1: prefix_index + prefix_length])
+    return prefix, prefix_field
+
+
+def write_manifest(out_path, prefix, has_data_by_file, package_path_by_file):
+    manifest_filepath = os.path.join(out_path, prefix, 'manifest.csv')
+    with open(manifest_filepath, 'w') as manifest_csv:
+        writer = csv.writer(manifest_csv)
+        writer.writerow(['file', 'hasData', 'size', 'checksum'])
+        for key in has_data_by_file.keys():
+            # get file size and checksum
+            package_path = package_path_by_file[key]
+            file_size = os.stat(package_path).st_size
+            md5_hash = hashlib.md5()
+            with open(package_path, "rb") as f:
+                for byte_block in iter(lambda: f.read(4096), b""):
+                    md5_hash.update(byte_block)
+            checksum = md5_hash.hexdigest()
+            writer.writerow([key, has_data_by_file[key], file_size, checksum])
+
+
+def parse_manifest(manifest_file, has_data_by_file, sort_index, prefix_field, timestamp):
     manifest = pd.read_csv(manifest_file, header=0, squeeze=True, index_col=0).to_dict()
     for key in manifest.keys():
         package_file = get_package_filename(key, sort_index, prefix_field, timestamp)
-        if package_file in hasDataByFile.keys():
-            hasDataByFile[package_file] = hasDataByFile[package_file] | manifest[key]
+        if package_file in has_data_by_file.keys():
+            has_data_by_file[package_file] = has_data_by_file[package_file] | manifest[key]
         else:
-            hasDataByFile[package_file] = manifest[key]
+            has_data_by_file[package_file] = manifest[key]
--- modules/packager/tests/test_packager.py
+++ modules/packager/tests/test_packager.py
@@ -14,51 +14,52 @@ import logging
 class Packager(TestCase):
 
     def setUp(self):
-
-        self.log = logging.getLogger("testlog")
+        self.log = logging.getLogger('testlog')
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
-        # create temporary dir
-        self.temp_dir = TempDirectory()
+        self.temp_dir = TempDirectory()  # create temporary dir
         self.temp_dir_name = self.temp_dir.path
-        self.input_path = Path(self.temp_dir_name, "repo/inputs")
-        self.data_path = Path(self.input_path, "CPER/2019/05")
-        self.out_path = Path(self.temp_dir_name, "outputs")
-        self.output_path = Path(self.out_path, "CPER/2019/05")
-        self.data_file_1 = Path(self.data_path, '24/NEON.D10.CPER.DP1.00041.001.001.501.001.ST_1_minute.2019-05-24.basic.csv')
-        self.data_file_2 = Path(self.data_path, '25/NEON.D10.CPER.DP1.00041.001.001.501.001.ST_1_minute.2019-05-25.basic.csv')
+        self.input_path = Path(self.temp_dir_name, 'repo/inputs')
+        self.data_path = Path(self.input_path, 'CPER/2019/05')
+        self.out_path = Path(self.temp_dir_name, 'outputs')
+        self.output_path = Path(self.out_path, 'CPER/2019/05')
+        self.data_file_1 = Path(self.data_path,
+                                '24/NEON.D10.CPER.DP1.00041.001.001.501.001.ST_1_minute.2019-05-24.basic.csv')
+        self.data_file_2 = Path(self.data_path,
+                                '25/NEON.D10.CPER.DP1.00041.001.001.501.001.ST_1_minute.2019-05-25.basic.csv')
         os.makedirs(Path(self.data_path, '24'))
         os.makedirs(Path(self.data_path, '25'))
         with open(self.data_file_1, 'w') as f:
-            f.write("file 1 content")
+            f.write('file 1 content')
         with open(self.data_file_2, 'w') as f:
-            f.write("file 2 content")
-        self.prefix_index = self.data_path.parts.index("CPER")
+            f.write('file 2 content')
+        self.prefix_index = self.data_path.parts.index('CPER')
         self.prefix_length = 3
         self.sort_index = 10
 
     def test_package(self):
-        os.environ["DATA_PATH"] = str(self.data_path)
-        os.environ["OUT_PATH"] = str(self.out_path)
-        package(prefix_index=self.prefix_index, prefix_length=self.prefix_length, sort_index=self.sort_index)
+        package(data_path=self.data_path,
+                out_path=self.out_path,
+                prefix_index=self.prefix_index,
+                prefix_length=self.prefix_length,
+                sort_index=self.sort_index)
         self.check_output()
 
     def test_main(self):
-        os.environ["DATA_PATH"] = str(self.data_path)
-        os.environ["OUT_PATH"] = str(self.out_path)
-        os.environ["PREFIX_INDEX"] = str(self.prefix_index)
-        os.environ["PREFIX_LENGTH"]= str(self.prefix_length)
-        os.environ["SORT_INDEX"] = str(self.sort_index)
+        os.environ['DATA_PATH'] = str(self.data_path)
+        os.environ['OUT_PATH'] = str(self.out_path)
+        os.environ['PREFIX_INDEX'] = str(self.prefix_index)
+        os.environ['PREFIX_LENGTH'] = str(self.prefix_length)
+        os.environ['SORT_INDEX'] = str(self.sort_index)
         packager_main.main()
         self.check_output()
 
     def check_output(self):
         os.chdir(self.output_path)
-        out_files = glob.glob("*.csv")
-        self.log.debug("INPUT PATH = " + str(self.input_path))
-        self.log.debug("OUTPUT PATH = " + str(self.out_path))
-        self.log.debug("NUMBER OF OUTPUT FILES = " + str(len(out_files)))
-        self.log.debug("OUTPUT FILES = " + str(out_files))
+        out_files = glob.glob('*.csv')
+        self.log.debug(f'INPUT PATH = {str(self.input_path)}')
+        self.log.debug(f'OUTPUT PATH = {str(self.out_path)}')
+        self.log.debug(f'NUMBER OF OUTPUT FILES {str(len(out_files))}')
+        self.log.debug(f'OUTPUT FILES {str(out_files)}')
         basic_pattern = 'NEON.D10.CPER.DP1.00041.001.001.501.001.ST_1_minute.2019-05.basic.*.csv'
         self.assertTrue(len(out_files) == 2)
         self.assertTrue(fnmatch.fnmatch(out_files[0], basic_pattern))
