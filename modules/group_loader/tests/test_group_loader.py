2f93c3bf493de274c2ecb8de2275d326e8414c9a
choim <choim@battelleecology.org>
2023-02-15 15:45:06 -0700

    fix a type error in test_group_loader.py

--- modules/group_loader/tests/test_group_loader.py
+++ modules/group_loader/tests/test_group_loader.py
@@ -36,6 +36,7 @@ class GroupLoaderTest(DatabaseBackedTest):
         s_date = '2022-11-07T11:56:11Z'
         e_date = '2022-11-08T12:57:12Z'
         data_product_id = 'DP1.20008.001'
+        groups: List[Group] = []
 
         def get_groups(group_prefix) -> List[Group]:
             print(f'prefix: {group_prefix}')
@@ -43,7 +44,8 @@ class GroupLoaderTest(DatabaseBackedTest):
             periods = [ActivePeriod(start_date=to_datetime(s_date), end_date=to_datetime(e_date))]
             props = [Property('HOR', '000'), Property('VER', '000')]
             group = Group(name='test-group_2', group='test-group_1', active_periods=periods, data_product_ID=data_product_id, properties=props)
-            return [group]
+            groups.append(group)
+            return groups
 
         # test the function
         group_loader.load_groups(out_path=self.out_path, get_groups=get_groups, group_prefix='test-')
