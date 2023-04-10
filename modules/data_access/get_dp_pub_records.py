a98224a4484b7fda8c435271e5949438275af0b0
choim <choim@battelleecology.org>
2023-04-10 11:22:02 -0600

    more added to the get_dp_pub_rocords.py

--- modules/data_access/get_dp_pub_records.py
+++ modules/data_access/get_dp_pub_records.py
@@ -10,14 +10,20 @@ from data_access.db_connector import DbConnector
 from data_access.types.dp_pub import DpPub
 
 
-def get_dp_pub_records(connector: DbConnector,dp_id: List[str],data_begin: datetime,data_cutoff: datetime,
-                    site: List[str]) -> Iterator[DpPub]:
+def get_dp_pub_records(connector: DbConnector,dp_ids: List[str],data_begin: datetime,data_cutoff: datetime,
+                       sites: List[str]) -> Iterator[DpPub]:
     """
-    Get dp pub records for a dp_id, dataIntervalStart, dataIntervalEnd and any site.
+    Get dp pub records for a dp_id(s), dataIntervalStart, dataIntervalEnd and any site(s).
 
     :param connector: A database connector.
     :param dp_id, dataIntervalStart, dataIntervalEnd and any site: A data product id.
     :return: data product records.
+
+    select * from dp_pub
+    where site in ('STER', 'ABBY')
+    and dp_idq  in ('NEON.DOM.SITE.DP1.00017.001', 'NEON.DOM.SITE.DP1.00040.001')
+    and data_interval_start >= '2013-10-01T00:00:00Z'
+    and data_interval_end <= '2013-11-01T00:00:00Z';
     """
 
     sql = f'''
@@ -36,11 +42,40 @@ def get_dp_pub_records(connector: DbConnector,dp_id: List[str],data_begin: datet
          from 
              dp_pub 
          where 
-             dp_idq  = %s
+             dp_idq  = ANY (%s)
          and 
              data_interval_start >= %s
          and 
              data_interval_start < %s
          and 
-             site = %s
+             site = ANY (%s)
      '''
+
+    connection = connector.get_connection()
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(sql,(dp_ids,data_begin,data_cutoff,sites))
+        rows = cursor.fetchall()
+        for row in rows:
+            dataProductId = row[0]
+            site = row[1]
+            dataIntervalStart = row[2]
+            dataIntervalEnd = row[3]
+            packageType = row[4]
+            hasData = row[5]
+            status = row[6]
+            create_date = row[7]
+            updateDate = row[8]
+            releaseStatus = row[9]
+            id = row[10]
+            dppub = DpPub(dataProductI=ddataProductId,
+                          site=site,
+                          dataIntervalStart=date_formatter.to_string(dataIntervalStart),
+                          dataIntervalEnd=date_formatter.to_string(dataIntervalEnd),
+                          packageType=packageType,
+                          hasData=hasData,
+                          status=status,
+                          create_date=date_formatter.to_string(create_date),
+                          updateDate=date_formatter.to_string(updateDate),
+                          releaseStatus=releaseStatus,
+                          id=id)
+            yield dppub
