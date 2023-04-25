18bd28a953f52306634274cb6c1b074a17a54183
visalachundru <vchundru@den-devissom-1.ci.neoninternal.org>
2023-04-25 09:56:45 -0600

    moved calibration repo loader from omnibus tools project

---
+++ modules/calval_loader/pachyderm_script.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+
+pachctl delete repo g2131i_stcdd_calibration
+pachctl delete repo csat3_calibration 
+pachctl delete repo li191r_calibration
+pachctl delete repo ptb330a_calibration
+pachctl delete repo leveltroll500_calibration
+pachctl delete repo tchain_calibration
+pachctl delete repo aquatroll200_calibration
+pachctl delete repo li7200_calibration
+pachctl delete repo metone370380_calibration
+pachctl delete repo hivol3000_calibration
+pachctl delete repo ce318_raw_calibration
+pachctl delete repo l2130i_stcdd_calibration
+pachctl delete repo windobserverii_calibration
+pachctl delete repo li840a_calibration
+pachctl delete repo drx8533ep_calibration
+pachctl delete repo spn1_calibration
+pachctl delete repo prt_calibration
+pachctl delete repo hmp155_calibration
+pachctl delete repo windobserverii_calibration
+pachctl delete repo pqs1_calibration
+pachctl delete repo exophorp_calibration
+pachctl delete repo exototalalgae_calibration
+pachctl delete repo enviroscan_calibration
+pachctl delete repo gmp343_calibration
+pachctl delete repo hfp01sc_calibration
+pachctl delete repo nr01_calibration
+pachctl delete repo exoconductivity_calibration
+pachctl delete repo sunav2_raw_calibration
+pachctl delete repo si111_calibration
+pachctl delete repo exoturbidity_calibration
+pachctl delete repo exodissolvedoxygen_calibration
+
+pachctl create repo g2131i_stcdd_calibration
+pachctl create repo csat3_calibration 
+pachctl create repo li191r_calibration
+pachctl create repo ptb330a_calibration
+pachctl create repo leveltroll500_calibration
+pachctl create repo tchain_calibration
+pachctl create repo aquatroll200_calibration
+pachctl create repo li7200_calibration
+pachctl create repo metone370380_calibration
+pachctl create repo hivol3000_calibration
+pachctl create repo ce318_raw_calibration
+pachctl create repo l2130i_stcdd_calibration
+pachctl create repo windobserverii_calibration
+pachctl create repo li840a_calibration
+pachctl create repo drx8533ep_calibration
+pachctl create repo spn1_calibration
+pachctl create repo prt_calibration
+pachctl create repo hmp155_calibration
+pachctl create repo windobserverii_calibration
+pachctl create repo pqs1_calibration
+pachctl create repo exophorp_calibration
+pachctl create repo exototalalgae_calibration
+pachctl create repo enviroscan_calibration
+pachctl create repo gmp343_calibration
+pachctl create repo hfp01sc_calibration
+pachctl create repo nr01_calibration
+pachctl create repo exoconductivity_calibration
+pachctl create repo sunav2_raw_calibration
+pachctl create repo si111_calibration
+pachctl create repo exoturbidity_calibration
+pachctl create repo exodissolvedoxygen_calibration
+pachctl create repo gascylinder_calibration
+
+
+pachctl auth set repo g2131i_stcdd_calibration repoWriter robot:li191r-upload-sbx-vchundru 
+pachctl auth set repo csat3_calibration repoWriter robot:li191r-upload-sbx-vchundru 
+pachctl auth set repo li191r_calibration repoWriter robot:li191r-upload-sbx-vchundru 
+pachctl auth set repo ptb330a_calibration repoWriter robot:li191r-upload-sbx-vchundru 
+pachctl auth set repo leveltroll500_calibration repoWriter robot:li191r-upload-sbx-vchundru 
+pachctl auth set repo tchain_calibration repoWriter robot:li191r-upload-sbx-vchundru 
+pachctl auth set repo aquatroll200_calibration repoWriter robot:li191r-upload-sbx-vchundru 
+pachctl auth set repo li7200_calibration repoWriter robot:li191r-upload-sbx-vchundru 
+pachctl auth set repo metone370380_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo hivol3000_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo ce318_raw_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo l2130i_stcdd_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo windobserverii_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo li840a_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo drx8533ep_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo spn1_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo prt_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo hmp155_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo windobserverii_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo pqs1_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo exophorp_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo exototalalgae_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo enviroscan_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo gmp343_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo hfp01sc_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo nr01_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo exoconductivity_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo sunav2_raw_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo si111_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo exoturbidity_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo exodissolvedoxygen_calibration repoWriter robot:li191r-upload-sbx-vchundru
+pachctl auth set repo gascylinder_calibration repoWriter robot:li191r-upload-sbx-vchundru
