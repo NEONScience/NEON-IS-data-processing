e9dab754ab620972d314e8a0c8d4ba86123fe088
visalachundru <vchundru@den-devissom-1.ci.neoninternal.org>
2023-05-11 11:38:45 -0600

    Cleaned up the code and added calibration pipelines for "li191r" and "gascylinder" sensor types

--- modules/calval_loader/calval_loader.py
+++ modules/calval_loader/calval_loader.py
@@ -17,13 +17,15 @@ from google.cloud import storage
 
 def calval_loader() -> None:
 
-    ingest_bucket_name = os.environ['CVAL_INGEST_BUCKET']
+    env = environs.Env()
+    ingest_bucket_name = env.str('CVAL_INGEST_BUCKET')
+    output_directory: Path = env.path('OUT_PATH')
     storage_client = storage.Client()
     ingest_bucket = storage_client.bucket(ingest_bucket_name)
     path_names = storage_client.list_blobs(ingest_bucket)
     files_to_be_uploaded = list()
     for blob in path_names:
-        output_directory: Path = env.path('OUT_PATH')
+        
         file_name = os.path.splitext(blob.name)[0]
         file_path = os.path.join(output_directory, file_name+".txt")         
 
--- modules/calval_loader/load_all_calval_files.py
+++ modules/calval_loader/load_all_calval_files.py
@@ -31,12 +31,12 @@ def load() -> None:
     env = environs.Env()
     ingest_bucket_name = env.str('CVAL_INGEST_BUCKET')
     in_path: Path = env.path('IN_PATH')
+    output_directory: Path = env.path('OUT_PATH')
+    sensor_type = env.str('SOURCE_TYPE')
     db_config = read_from_mount(Path('/var/db_secret'))
     storage_client = storage.Client()
     ingest_bucket = storage_client.bucket(ingest_bucket_name)
     print("Bucet name is : ", ingest_bucket)
-      pachyderm_client = python_pachyderm.Client
-    print("Pachyderm client is: ", pachyderm_client)
     with closing(DbConnector(db_config)) as connector:
         now = datetime.datetime.now()
         try:
@@ -50,13 +50,12 @@ def load() -> None:
                 root = ET.fromstring(blob.download_as_string())
                 asset_id = root.find('SensorID').find('MxAssetID').text
                 avro_schema_name = get_avro_schema_name(connector.get_connection(), asset_id)
-                if (avro_schema_name != None):
+                if ((avro_schema_name != None) and (avro_schema_name == sensor_type)):
                     stream_id = root.find('StreamCalVal').find('StreamID').text
                     stream_name = get_calibration_stream_name(connector.get_connection(), avro_schema_name, stream_id)
                     print('repo name , asset_id, stream_name, filename are :', avro_schema_name, "  ", asset_id, "  ",
                           stream_name, " ", filename)
                     try:
-                        # file_url = f'https://storage.cloud.google.com/{ingest_bucket_name}/{filename}'
                         output_path = Path(output_directory, avro_schema_name, asset_id, stream_name, filename)
                         output_path.parent.mkdir(parents=True, exist_ok=True)
                         # print('Output Path is:', output_path)
