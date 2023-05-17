d79c1d862f500f54bc7f8c66babe9bd93a7c618e
visalachundru <vchundru@den-devissom-1.ci.neoninternal.org>
2023-05-17 11:41:53 -0600

    Committing the changes made to group the datums

--- modules/calval_loader/load_all_calval_files.py
+++ modules/calval_loader/load_all_calval_files.py
@@ -23,34 +23,37 @@ def load() -> None:
     db_config = read_from_mount(Path('/var/db_secret'))
     storage_client = storage.Client()
     ingest_bucket = storage_client.bucket(ingest_bucket_name)
-    print("Bucet name is : ", ingest_bucket)
+    starting_path_index: int = env.int('STARTING_PATH_INDEX')
     with closing(DbConnector(db_config)) as connector:
         now = datetime.datetime.now()
         try:
-            pathname, extension = os.path.splitext(in_path)
-            filename = pathname.split('/')
-            filename = filename[-1] + ".xml"
-            # print("FileName is: ", filename)
-
-            blob = ingest_bucket.get_blob(filename)
-            with blob.open("r") as f:
-                root = ET.fromstring(blob.download_as_string())
-                asset_id = root.find('SensorID').find('MxAssetID').text
-                avro_schema_name = get_avro_schema_name(connector.get_connection(), asset_id)
-                if ((avro_schema_name != None) and (avro_schema_name == sensor_type)):
-                    stream_id = root.find('StreamCalVal').find('StreamID').text
-                    stream_name = get_calibration_stream_name(connector.get_connection(), avro_schema_name, stream_id)
-                    print('repo name , asset_id, stream_name, filename are :', avro_schema_name, "  ", asset_id, "  ",
-                          stream_name, " ", filename)
-                    try:
-                        output_path = Path(output_directory, avro_schema_name, asset_id, stream_name, filename)
-                        output_path.parent.mkdir(parents=True, exist_ok=True)
-                        # print('Output Path is:', output_path)
-                        with open(output_path, "wb") as output_file:
-                            output_file.write(blob.download_as_string())
-                    except Exception:
-                        exc_type, exc_obj, exc_tb = sys.exc_info()
-                        print("Exception at line " + str(exc_tb.tb_lineno) + ": " + str(sys.exc_info()))
+            data_path_start = Path(*in_path.parts[0:starting_path_index+1]) # starting index
+            print("Starting New Datum in the load_al_calval_files pipeline ")
+            for path in data_path_start.rglob('*'):
+                pathname, extension = os.path.splitext(path)
+                filename = pathname.split('/')
+                filename = filename[-1] + ".xml"
+                print("FileName is: ", filename)
+                blob = ingest_bucket.get_blob(filename)
+    
+                with blob.open("r") as f:
+                    root = ET.fromstring(blob.download_as_string())
+                    asset_id = root.find('SensorID').find('MxAssetID').text
+                    avro_schema_name = get_avro_schema_name(connector.get_connection(), asset_id)
+                    if ((avro_schema_name != None) and (avro_schema_name == sensor_type)):
+                        stream_id = root.find('StreamCalVal').find('StreamID').text
+                        stream_name = get_calibration_stream_name(connector.get_connection(), avro_schema_name, stream_id)
+                        print('repo name , asset_id, stream_name, filename are :', avro_schema_name, "  ", asset_id, "  ",
+                              stream_name, " ", filename)
+                        try:
+                            output_path = Path(output_directory, avro_schema_name, asset_id, stream_name, filename)
+                            output_path.parent.mkdir(parents=True, exist_ok=True)
+                            # print('Output Path is:', output_path)
+                            with open(output_path, "wb") as output_file:
+                                output_file.write(blob.download_as_string())
+                        except Exception:
+                            exc_type, exc_obj, exc_tb = sys.exc_info()
+                            print("Exception at line " + str(exc_tb.tb_lineno) + ": " + str(sys.exc_info()))
         except Exception:
             exception_type, exception_obj, exception_tb = sys.exc_info()
             print("Exception at line " + str(exception_tb.tb_lineno) + ": " + str(sys.exc_info()))
