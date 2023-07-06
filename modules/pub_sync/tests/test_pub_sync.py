de81fcf602160cf94213b8dd3c069909a63b57b7
Mija Choi <choim@battelleecology.org>
2023-07-05 19:35:02 -0600

    pub_sync test updated and the test data added

--- modules/pub_sync/tests/test_pub_sync.py
+++ modules/pub_sync/tests/test_pub_sync.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
-from typing import List, Dict
+from typing import List,Dict, NamedTuple
 import unittest
 
 from pathlib import Path
 from unittest import TestCase
 
-#from data_access.tests.database_test import DatabaseBackedTest
 from data_access.types.dp_pub import DpPub
+from typing import Callable, Iterator, List, Dict
+
+from dateutil.relativedelta import relativedelta
+from common.get_path_key import get_path_key
+
+from data_access.types.dp_pub import DpPub
+
+from pub_sync.tests.dp_pub_data import get_dp_pub_data
 from testfixtures import TempDirectory
 
-from pub_sync import pub_sync_main, pub_sync
+from pub_sync import pub_sync_main,pub_sync
 
-#class PubSyncTest(DatabaseBackedTest):
+
 class PubSyncTest(TestCase):
-    #
-    # def setUp(self):
-    #     self.setUpPyfakefs()
-    #     self.out_path = Path('/out')
-    #     self.fs.create_dir(self.out_path)
-
 
     def test_pub_sync(self):
         temp_dir = TempDirectory()
@@ -39,28 +40,56 @@ class PubSyncTest(TestCase):
         date_path_start = Path(*date_path.parts[0:date_path_min_index])  # Parent of the min index
         change_by = 'pachyderm1'
 
-
-        def get_sync_pubs(pub_dates: List[Dict], dp_ids: List[str], sites: List[str], psmp_pachy: List[Dict], change_by: str) -> List[DpPub]:
+        def get_sync_pubs(pub_dates: List[Dict],dp_ids: List[str],sites: List[str],psmp_pachy: List[Dict],
+                          change_by: str) -> List[DpPub]:
             """Mock function"""
 
             date_key = '202301'
             cutoff_date = '202304'
             pub_dates[date_key] = [date_key + '01T00:00:00Z',
                                    cutoff_date + '01T00:00:00Z']
+            pubs: List[DpPub] = []
+            pubs = get_dp_pub_data()
+            psmp_portal_remove = {}
+            #
+            for pub in pubs:
+             #   Form the key for matching existing portal pubs to pachy pubs
+                dataIntervalStartKey = pub.dataIntervalStart.replace('Z','').replace(':','').replace('-','')
+                dataIntervalEndKey = pub.dataIntervalEnd.replace('Z','').replace(':','').replace('-','')
+                pub_key = pub.dataProductId + pub.site + dataIntervalStartKey + '--' + dataIntervalEndKey + pub.packageType
 
+            #    If an existing portal pub is not the list of current pubs, mark it for further investigation & possible removal
+                if pub_key not in psmp_pachy.keys():
+                    if pub_key in psmp_portal_remove.keys():
+                        pub_list = psmp_portal_remove[pub_key]
+                        pub_list.append(pub)
+                        psmp_portal_remove[pub_key] = pub_list
+                    else:
+                        psmp_portal_remove[pub_key] = [pub]
+                        # log.debug(
+                        #     f'Found pub records for package [{pub.dataProductId} {pub.site} {pub.dataIntervalStart} {pub.packageType}] not output by current processing. Marked for investigation.')
 
-        pub_sync.sync_pubs(get_sync_pubs = get_sync_pubs,
-                           data_path = data_path,
-                           date_path = date_path,
-                           date_path_year_index = date_path_year_index,
-                           date_path_month_index = date_path_month_index,
-                           data_path_product_index = data_path_product_index,
-                           data_path_site_index = data_path_site_index,
-                           data_path_date_index = data_path_date_index,
-                           data_path_package_index = data_path_package_index,
-                           dp_ids = dp_ids,
-                           sites = sites,
-                           change_by = change_by)
+        # Check or set the relevant portal records to inactive
+        #   log.info(f'Found {len(psmp_portal_remove.keys())} product-site-month-packages to check/set to inactive')
+        #    remove_pub(connector,psmp_portal_remove,change_by)
+            pub_dates = {}
+            date_path_indices = (date_path_year_index,date_path_month_index)
+            date_path_min_index = int(min(date_path_indices))
+            date_path_max_index = int(max(date_path_indices))
+            date_path_start = Path(*date_path.parts[0:date_path_min_index])  # Parent of the min index
+
+        pub_sync.sync_pubs(get_sync_pubs=get_sync_pubs,
+                           data_path=data_path,
+                           date_path=date_path,
+                           date_path_year_index=date_path_year_index,
+                           date_path_month_index=date_path_month_index,
+                           data_path_product_index=data_path_product_index,
+                           data_path_site_index=data_path_site_index,
+                           data_path_date_index=data_path_date_index,
+                           data_path_package_index=data_path_package_index,
+                           dp_ids=dp_ids,
+                           sites=sites,
+                           change_by=change_by)
 
         self.assertTrue(change_by == 'pachyderm1')
 
