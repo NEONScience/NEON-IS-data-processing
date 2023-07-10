52c98db800326d27484afd999588f1a8b2619775
covesturtevant <csturtevant@battelleecology.org>
2023-07-10 12:08:52 -0600

    remove data directories in combined module images. Update pipeline specs.

--- modules_combined/level1_group_and_consolidate/build_tag_push_update.sh
+++
@@ -1,9 +0,0 @@
-# Run from root repository (NEON-IS-data-processing)
-#!/usr/bin/env bash
-image_name=level1_group_and_consolidate
-tag=$(git rev-parse HEAD)
-docker build -t $image_name:latest -f ./modules_combined/level1_group_and_consolidate/Dockerfile .
-docker tag $image_name quay.io/battelleecology/$image_name:$tag
-docker push quay.io/battelleecology/$image_name:$tag
-
-Rscript ./utilities/flow.img.updt.R "./pipe" ".yaml" "quay.io/battelleecology/$image_name" "$tag"
