0ed236bd40da0ba96c24c0caa71e5bf8a382cbdd
Mija Choi <choim@battelleecology.org>
2023-08-11 16:24:57 -0600

    remove print lines

--- modules/test_module/test.py
+++ modules/test_module/test.py
@@ -16,10 +16,10 @@ def test_err_datum_path () -> None:
         err_datum_path(err = 'error testing bad input path #1',DirDatm = 'qfs/proc_group/prt/2019/01/01/27134',
                                                      DirErrBase = 'pfs/proc_group_out/errored_datums',
                                                     RmvDatmOut = True, DirOutBase = 'pfs/proc_group_out')
-        err_datum_path(err='error testing correct input path #2',DirDatm='pfs/proc_group/prt/2019/01/01/27134',
+        err_datum_path(err='error testing correct input path #2',DirDatm='pfs/proc_group/prt/2019/01/01/27134/prt_16247_location.json',
                        DirErrBase='pfs/out/errored_datums',
                        RmvDatmOut=True)
-        err_datum_path(err='error testing correct input path group focus #3',DirDatm='pfs/proc_group/2019/01/01/temp-air-single-114/prt/CFGLOC101255',
+        err_datum_path(err='error testing correct input path GROUP focus #3',DirDatm='pfs/proc_group/2019/01/01/temp-air-single-114/prt/CFGLOC101255',
                        DirErrBase='pfs/out/errored_datums',
                        RmvDatmOut=True)
 
