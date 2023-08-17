05d35ac33d3cdee4833d62e7044dad209056fae8
Mija Choi <choim@battelleecology.org>
2023-08-17 16:04:08 -0600

    type change

--- modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid_main.py
+++ modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid_main.py
@@ -12,12 +12,12 @@ def main() -> None:
     data_path: Path = os.environ['DATA_PATH']
     map_path: Path = os.environ['MAP_PATH']
     out_path: Path = os.environ['OUT_PATH']
-    source_type: Path = os.environ['SOURCE_TYPE']
+    source_type: str = os.environ['SOURCE_TYPE']
     log_level: str = os.environ['LOG_LEVEL']
     log_config.configure(log_level)
     log.debug(f'out_path: {out_path}')
     print(f'final output path is: {Path(out_path)}')
-    load_assetuid(data_path = data_path, map_path = map_path, out_path = out_path, source_type=source_type)
+    load_assetuid(data_path = data_path, map_path = map_path, out_path = out_path, source_type = source_type)
 
 
 if __name__ == '__main__':
