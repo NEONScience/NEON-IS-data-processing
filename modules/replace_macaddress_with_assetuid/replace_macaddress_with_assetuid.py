763c515c68529970a554976955552432009e3365
Liangying Zhang <lzhang@battelleecology.org>
2023-08-25 15:53:11 -0600

    rewrite macaddress replace module

--- modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
+++ modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
@@ -9,29 +9,34 @@ import os
 log = get_logger()
 
 
-def load_assetuid(data_path: Path, map_path: Path, out_path: Path, source_type: str) -> None:
+def load_assetuid(data_path: Path, map_path: Path, out_path: Path, source_type: str, relative_path_index: int) -> None:
 
-    for dpath in Path("/pfs/DATA_PATH/").rglob('*'):
-        if Path(dpath).is_file():
-            mac_address = Path(dpath).stem.split("_")[1]
-            new_dpath = os.path.normpath(dpath)
-            print(f'Mac_Address name is: {mac_address}')
-            path_list = new_dpath.split(os.sep)
-            source_type = source_type.split("_")[0]
-            IdxSourceType = path_list.index(source_type)
-            for mpath in Path(map_path).rglob('*'):
-                if Path(mpath).is_file():
-                    if (Path(mpath).stem.split("_")[2] == mac_address):
-                        with open(Path(mpath), 'r') as f:
-                            asset_uid = f.read().split()[0]
-                            print(f'Asset_UID name is: {asset_uid}')
-                        f.close()
-                        extended_path = os.sep.join(path_list[IdxSourceType:]).replace(str(mac_address), str(asset_uid))
-                        print(f'Extended Path is: {extended_path}')
-                        new_file_path = Path(os.path.join(Path(out_path), extended_path))
-                        Path(new_file_path).parent.mkdir(parents=True, exist_ok=True)
-                        if not new_file_path.exists():
-                            print(f'original path: {dpath} new_path: {new_file_path}')
-                            log.debug(f'original path: {dpath} new_path: {new_file_path}')
-                            new_file_path.symlink_to(new_dpath)
+    mac_asset_map = {}
+    for root, direcotories, files in os.walk(str(map_path)):
+        if files:
+            for file in files:
+                mac = root.split(os.sep)[-1]
+                with open(os.path.join(root, file), 'r') as f:
+                    asset = f.read().split()[0]
+                mac_asset_map[mac] = asset
+
+    for root, direcotories, files in os.walk(str(data_path)):
+        if files:
+            for file in files:
+                mac_address = file.split("_")[1]
+                asset_uid = mac_asset_map.get(mac_address)
+                log.debug(f'Mac_Address name is: {mac_address}; asset_uid is: {asset_uid}')
+
+                old_path = os.path.join(root, file)
+                path_list = old_path.split(os.sep)
+                extended_path = os.sep.join(path_list[relative_path_index:]).replace(str(mac_address), str(asset_uid))
+                new_path = Path(os.path.join(Path(out_path), extended_path))
+
+                Path(new_path).parent.mkdir(parents=True, exist_ok=True)
+                if not new_path.exists():
+                    log.debug(f'original path: {file}; new_path: {new_path}')
+                    new_path.symlink_to(old_path)
+
+
+
 
--- modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid_main.py
+++ modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid_main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from structlog import get_logger
 import common.log_config as log_config
 import os
-from replace_macaddress_with_assetuid.replace_macaddress_with_assetuid import load_assetuid
+from replace_macaddress_with_assetuid import load_assetuid
 
 log = get_logger()
 
@@ -13,10 +13,11 @@ def main() -> None:
     map_path: Path = os.environ['MAP_PATH']
     out_path: Path = os.environ['OUT_PATH']
     source_type: str = os.environ['SOURCE_TYPE']
+    relative_path_index: int = int(os.environ['RELATIVE_PATH_INDEX'])
     log_level: str = os.environ['LOG_LEVEL']
     log_config.configure(log_level)
     log.debug(f'out_path: {out_path}')
-    load_assetuid(data_path = data_path, map_path = map_path, out_path = out_path, source_type = source_type)
+    load_assetuid(data_path = data_path, map_path = map_path, out_path = out_path, source_type = source_type, relative_path_index = relative_path_index)
 
 
 if __name__ == '__main__':
