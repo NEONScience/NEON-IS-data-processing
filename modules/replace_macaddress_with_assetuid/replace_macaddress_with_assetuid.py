c0914e1f32437a27fbb8edfb1b441d294f867a9c
Liangying Zhang <lzhang@battelleecology.org>
2023-08-28 15:11:58 -0600

    more code clean up

--- modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
+++ modules/replace_macaddress_with_assetuid/replace_macaddress_with_assetuid.py
@@ -1,10 +1,7 @@
 #!usr/bin/env python3
 import os
-import logging
 from pathlib import Path
-from contextlib import closing
 from structlog import get_logger
-import os
 
 log = get_logger()
 
@@ -12,28 +9,25 @@ log = get_logger()
 def load_assetuid(data_path: Path, map_path: Path, out_path: Path, relative_path_index: int) -> None:
 
     mac_asset_map = {}
-    for root, directories, files in os.walk(str(map_path)):
-        if files:
-            for file in files:
-                mac = root.split(os.sep)[-1]
-                with open(Path(root, file), 'r') as f:
-                    asset = f.read().split()[0]
-                mac_asset_map[mac] = asset
+    for path in map_path.rglob('*'):
+        if path.is_file():
+            mac = path.parent.name
+            with open(path, 'r') as f:
+                asset = f.read().split()[0]
+            mac_asset_map[mac] = asset
 
-    for root, directories, files in os.walk(str(data_path)):
-        if files:
-            for file in files:
-                mac_address = file.split("_")[1]
-                asset_uid = mac_asset_map.get(mac_address)
-                log.debug(f'Mac_Address name is: {mac_address}; asset_uid is: {asset_uid}')
+    for path in data_path.rglob('*'):
+        if path.is_file():
+            mac_address = path.parent.parent.name
+            asset_uid = mac_asset_map.get(mac_address)
+            log.debug(f'Mac_Address name is: {mac_address}; asset_uid is: {asset_uid}')
 
-                old_path = Path(root, file)
-                new_path = Path(out_path, os.sep.join(old_path.parts[relative_path_index:]).replace(mac_address, asset_uid))
+            new_path = Path(out_path, os.sep.join(path.parts[relative_path_index:]).replace(mac_address, asset_uid))
 
-                Path(new_path).parent.mkdir(parents=True, exist_ok=True)
-                if not new_path.exists():
-                    log.debug(f'original path: {file}; new_path: {new_path}')
-                    new_path.symlink_to(old_path)
+            Path(new_path).parent.mkdir(parents=True, exist_ok=True)
+            if not new_path.exists():
+                log.debug(f'original path: {path}; new_path: {new_path}')
+                new_path.symlink_to(path)
 
 
 
