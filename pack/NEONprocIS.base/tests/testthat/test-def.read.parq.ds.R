c9a1f2fc7f812efc3a4c6bb54fd6b4f72443be41
choim <choim@battelleecology.org>
2023-09-12 14:41:37 -0600

    = added

--- pack/NEONprocIS.base/tests/testthat/test-def.read.parq.ds.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.read.parq.ds.R
@@ -15,7 +15,7 @@ test_that("Read parquet dataset",
             testthat::expect_true(names(ds)[3]=='readout_time')
             readout_time_tail <- base::as.POSIXct(utils::tail(ds$readout_time,1),format="%Y-%m-%dT%H:%M",tz='GMT')
             readout_time_1 <- base::as.POSIXct(ds$readout_time[1],format="%Y-%m-%dT%H:%M",tz='GMT')
-            testthat::expect_true(readout_time_tail < readout_time_1)
+            testthat::expect_true(readout_time_tail <= readout_time_1)
             
             # Successful: read in dataset as data frame and sort time
             ds <- NEONprocIS.base::def.read.parq.ds(fileIn = inputPath,
