0be5d1c7d17d589e1e98f307a1b0965bfd38f102
choim <choim@battelleecology.org>
2023-10-23 16:20:58 -0600

    add ERR_PATH as env. var. in pub_packager

--- modules/pub_packager/pub_packager.py
+++ modules/pub_packager/pub_packager.py
@@ -12,11 +12,12 @@ from structlog import get_logger
 log = get_logger()
 
 
-def pub_package(*, data_path, out_path, product_index: int, publoc_index: int, date_index: int, date_index_length: int, sort_index: int) -> None:
+def pub_package(*, data_path, out_path, err_path, product_index: int, publoc_index: int, date_index: int, date_index_length: int, sort_index: int) -> None:
     """
     Bundles the required files into a package suitable for publication. 
 
     :param data_path: The input data path. Should end at whatever aggregation interval is a publication package (i.e. monthly)
+        :param err_path: The error directory, i.e., errored.
     :param out_path: The output path for writing the package files.
     :param product_index: input path index of the data product identifier
     :param publoc_index: input path index of the pub package location (typically the site)
