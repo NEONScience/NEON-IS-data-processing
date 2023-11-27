3b752cd9b0f6d33d42f06abf09f5dd3575eb7c3e
Mija Choi <choim@battelleecology.org>
2023-11-27 16:16:36 -0700

    a minor change in the unit test

--- modules/group_loader/tests/test_group_loader.py
+++ modules/group_loader/tests/test_group_loader.py
@@ -20,7 +20,7 @@ class GroupLoaderTest(DatabaseBackedTest):
     def setUp(self):
         self.setUpPyfakefs()
         self.out_path = Path('/out')
-        self.err_path = Path('/tmp/out/errored')
+        self.err_path = Path('/pfs/out/errored')
         self.fs.create_dir(self.out_path)
 
     @unittest.skip('Integration test skipped due to long process time.')
