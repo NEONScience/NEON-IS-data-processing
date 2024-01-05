23f2e4f4a200660e2f195b54bb21c29d6acf5924
ncatolico <ncatolico@battelleecology.org>
2024-01-04 17:06:07 -0700

    updates for logs

--- modules/logjam_loader/load_all_logjam_files.py
+++ modules/logjam_loader/load_all_logjam_files.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import environs
 import xml.etree.ElementTree as ET
 import sys
-from logjam_loader.get_avro_schema_name import get_avro_schema_name
-from logjam_loader.get_logjam_stream_name import get_logjam_stream_name
 from google.cloud import storage
 from data_access.db_config_reader import read_from_mount
 from data_access.db_connector import DbConnector
@@ -17,49 +15,56 @@ def load() -> None:
     env = environs.Env()
     ingest_bucket_name = env.str('LOGJAM_INGEST_BUCKET')
     in_path: Path = env.path('IN_PATH')
-    # print("IN_PATH value is:", in_path)
+    print("IN_PATH value is:", in_path)
     output_directory: Path = env.path('OUT_PATH')
-    sensor_type = env.str('SOURCE_TYPE')
     db_config = read_from_mount(Path('/var/db_secret'))
     storage_client = storage.Client()
     ingest_bucket = storage_client.bucket(ingest_bucket_name)
     starting_path_index: int = env.int('STARTING_PATH_INDEX')
+    print("starting_path_index value is:", starting_path_index)
+    
     with closing(DbConnector(db_config)) as connector:
         now = datetime.datetime.now()
         try:
             data_path_start = Path(*in_path.parts[0:starting_path_index + 1])  # starting index
-            print("Starting New Datum in the load_all_logjam_files pipeline ")
+            print("Starting New Datum in the load_all_logjam_files pipeline ", data_path_start)
             for path in data_path_start.rglob('*'):
                 if path.is_file():
                     print("Path value is: ", path)
                     pathname, extension = os.path.splitext(path)
+                    print("pathname is: ", pathname)
 
-                    filename = pathname.split('/')
+                    path_split = pathname.split('/')
+                    print("path_split is: ", path_split)
+                    
+                    folder = path_split[-3]
+                    print("folder is: ", folder)
+                    
+                    asset = path_split[-2]
+                    print("asset is: ", asset)
 
-                    filename = filename[-1] + ".xml"
+                    filename = path_split[-1] + ".csv"
                     print("FileName is: ", filename)
-                    blob = ingest_bucket.get_blob(filename)
+                    
+                    gcs_path = os.path.join(folder, asset,filename)
+                    print("gcs_path is: ", gcs_path)
+                    
+                    blob = ingest_bucket.get_blob(gcs_path)
+                    #blob = ingest_bucket.blob(gcs_path)
+                    print("ingest bucket is: ", ingest_bucket)
+                    print("blob is: ", blob)
 
                     with blob.open("r") as f:
-                        root = ET.fromstring(blob.download_as_string())
-                        asset_id = root.find('SensorID').find('MxAssetID').text
-                        avro_schema_name = get_avro_schema_name(connector.get_connection(), asset_id)
-                        if ((avro_schema_name != None) and (avro_schema_name == sensor_type)):
-                            stream_id = root.find('Streamlogjam').find('StreamID').text
-                            stream_name = get_logjam_stream_name(connector.get_connection(), avro_schema_name,
-                                                                      stream_id)
-                            print('repo name , asset_id, stream_name, filename are :', avro_schema_name, "  ", asset_id,
-                                  "  ",
-                                  stream_name, " ", filename)
-                            try:
-                                output_path = Path(output_directory, avro_schema_name, asset_id, stream_name, filename)
-                                output_path.parent.mkdir(parents=True, exist_ok=True)
-                                # print('Output Path is:', output_path)
-                                with open(output_path, "wb") as output_file:
-                                    output_file.write(blob.download_as_string())
-                            except Exception:
-                                exc_type, exc_obj, exc_tb = sys.exc_info()
-                                print("Exception at line " + str(exc_tb.tb_lineno) + ": " + str(sys.exc_info()))
+                        print(f.read())
+                        try:
+                            output_path = Path(output_directory, filename)
+                            output_path.parent.mkdir(parents=True, exist_ok=True)
+                            print('Output Path is:', output_path)
+                            with open(output_path, "wb") as output_file:
+                                output_file.write(blob.download_as_string())
+                        except Exception:
+                            exc_type, exc_obj, exc_tb = sys.exc_info()
+                            print("Exception at line " + str(exc_tb.tb_lineno) + ": " + str(sys.exc_info()))
         except Exception:
             exception_type, exception_obj, exception_tb = sys.exc_info()
             print("Exception at line " + str(exception_tb.tb_lineno) + ": " + str(sys.exc_info()))
