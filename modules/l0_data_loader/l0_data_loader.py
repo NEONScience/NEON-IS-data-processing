988ef6b76a7bb5eca748d7e8fb69b0b1384dd744
visalachundru <vchundru@battelleecology.org>
2024-01-17 15:28:16 -0700

    l0_data_loader

--- modules/l0_data_loader/l0_data_loader.py
+++ modules/l0_data_loader/l0_data_loader.py
@@ -11,28 +11,37 @@ from google.cloud import storage
 def l0_data_loader() -> None:
 
     env = environs.Env()
-    ingest_bucket_name = env.str('l0-bucket')
+    ingest_bucket_name = env.str('BUCKET_NAME')
     source_type = env.str('SOURCE_TYPE')
     output_directory: Path = env.path('OUT_PATH')
+    #ingest_bucket_name = 'neon-dev-l0-ingest'
+    #source_type = 'li191r'
+    #output_directory: Path = env.path('/pfs/out')
+    #output_directory: Path = '/pfs/out'
     storage_client = storage.Client()
     ingest_bucket = storage_client.bucket(ingest_bucket_name)
     gen_date = env.str('GEN_DATE')
     download_year = env.str('download_year')
     download_month = env.str('download_month')
     download_day = env.str('download_day')
-    gen_date = env.str('GEN_DATE')
+    
+    #download_year = '2023'
+    #download_month = '10'
+    #download_day = '01'
+   # gen_date = '2023-10-01'
       
     path_names = storage_client.list_blobs(ingest_bucket, prefix="v1/{source_type}/ms={download_year}-{download_month}", delimiter='/')
     for blob in path_names:
         
         file_name = os.path.splitext(blob.name)[0]
+        # print("File name is:  ", file_name)
         source_id =  re.split('_', file_name)[1]
         file_path = Path(output_directory, source_type,download_year, download_month, download_day,source_id, "data", {source_type}+"_"+{source_id}+"_"+{gen_date}+".parquet")         
         file_path.parent.mkdir(parents=True, exist_ok=True)
-        print("File name is:  ", file_path)
-        l0_data_file= open(file_path, "w")
-        l0_data_file.write(blob.updated.strftime("%Y-%m-%d %H:%M:%S"))
-        l0_data_file.close()
+        print("File path is:  ", file_path)
+      #  l0_data_file= open(file_path, "w")
+      #  l0_data_file.write(blob.updated.strftime("%Y-%m-%d %H:%M:%S"))
+      # l0_data_file.close()
         
 if __name__ == '__main__':
     l0_data_loader()
