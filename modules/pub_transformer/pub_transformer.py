f923d5a043be5fb381e1476140bccb3d577ad154
covesturtevant <csturtevant@battelleecology.org>
2024-03-25 15:28:55 -0600

    write basic & expanded files to manifest only if they are created.

--- modules/pub_transformer/pub_transformer.py
+++ modules/pub_transformer/pub_transformer.py
@@ -103,6 +103,18 @@ def pub_transform(*, data_path: Path, out_path: Path, workbook_path: Path, produ
             # Create each table in the workbook if possible
             for table in workbook['table'].unique():
                 
+                # Determine which, if any, packages files will be created for this product
+                basic_file = False
+                expanded_file = False
+                if workbook_table['downloadPkg'].str.contains('basic').any():
+                    basic_file = True
+                if workbook_table['downloadPkg'].str.contains('expanded').any():
+                    expanded_file = True
+                    
+                if basic_file is False & expanded_file is False:
+                    log.debug(f'No download packages indicated for {table}. Skipping.')
+                    continue
+                
                 # Load the data file(s) corresponding to this table (must have already been placed into a parquet file for each table)
                 data = None
                 for file in data_files:
@@ -153,18 +165,22 @@ def pub_transform(*, data_path: Path, out_path: Path, workbook_path: Path, produ
                 # determine data availability
                 if product not in has_data_by_file.keys():
                     has_data_by_file[product]={}
-                has_data_by_file[product][basic_filename] = not data[workbook_table.loc[
-                    (workbook_table['dataCategory'] == 'Y') &
-                    (workbook_table['downloadPkg'] == 'basic')]['fieldName'].values].isnull().values.all()
-                has_data_by_file[product][expanded_filename] = \
-                    not data[workbook_table.loc[(workbook_table['dataCategory'] == 'Y') & ((workbook_table['downloadPkg'] == 'basic') |
-                                        (workbook_table['downloadPkg'] == 'expanded'))]['fieldName'].values].isnull().values.all()
+                if basic_file is True:
+                    has_data_by_file[product][basic_filename] = not data[workbook_table.loc[
+                        (workbook_table['dataCategory'] == 'Y') &
+                        (workbook_table['downloadPkg'] == 'basic')]['fieldName'].values].isnull().values.all()
+                if expanded_file is True:
+                    has_data_by_file[product][expanded_filename] = \
+                        not data[workbook_table.loc[(workbook_table['dataCategory'] == 'Y') & ((workbook_table['downloadPkg'] == 'basic') |
+                                            (workbook_table['downloadPkg'] == 'expanded'))]['fieldName'].values].isnull().values.all()
                 
                 # Record portal visibility
                 if product not in visibility_by_file.keys():
                     visibility_by_file[product]={}
-                visibility_by_file[product][basic_filename] = visibility
-                visibility_by_file[product][expanded_filename] = visibility
+                if basic_file is True:
+                    visibility_by_file[product][basic_filename] = visibility
+                if expanded_file is True:
+                    visibility_by_file[product][expanded_filename] = visibility
                 
                 # extract and write datasets
                 os.makedirs(output_path, exist_ok=True)
