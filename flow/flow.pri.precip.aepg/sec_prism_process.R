7ca67284364532d1b5c86a95cf4a0037d5c4ea7e
burlingamet <tburlingame@battelleecology.org>
2024-04-07 10:41:47 -0600

    change data pat for prism/sec grab

--- flow/flow.pri.precip.aepg/sec_prism_process.R
+++ flow/flow.pri.precip.aepg/sec_prism_process.R
@@ -1,10 +1,7 @@
-
-library(dplyr)
-library(lubridate)
-
-site <- 'osbs'
-prism <- read.csv(paste0('data/', site, '_prism.csv'))
-sec <-  read.csv(paste0('data/', site, '_sec.csv'))
+dat_pat <- '/scratch/PrimaryPrecipAlgo/'
+site <- 'konz'
+prism <- read.csv(paste0(date_pat, site, '_prism.csv'))
+sec <-  read.csv(paste0(dat_pat, site, '_sec.csv'))
 
 sum(sec$value)
 sum(prism$value)
