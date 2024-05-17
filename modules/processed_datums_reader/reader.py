b94dfa1a90c592d266fa3d5a0742e4f19e01a459
visalachundru <vchundru@battelleecology.org>
2024-05-17 11:30:05 -0600

    bug fixes

--- modules/processed_datums_reader/reader.py
+++ modules/processed_datums_reader/reader.py
@@ -1,25 +1,26 @@
-import os
 from pachyderm_sdk import Client
-from pachyderm_sdk.api import pfs
-from pachyderm_sdk.api.pfs import FileType
+from pachyderm_sdk.api import pfs, pps
 from collections import defaultdict
+#from pachyderm_sdk.api.pps import Pipeline
 
 
-def read_processed_files(client: Client) -> defaultdict(lambda: defaultdict(int)):
+def read_processed_files(client: Client) -> defaultdict[lambda: defaultdict[int]]:
     pipeline_name = "level1_pipeline_list"
-    input_repo = f"/{pipeline_name}"
-    date_pattern = "/????/??/??"  # Adjust this to match your date pattern
-    commit_id = "master"
     files_by_pipeline = defaultdict(lambda: defaultdict(int))
     source = (pfs.File.from_uri("level1_pipeline_list@master:/level1_pipeline_list.txt"))
     with (client.pfs.pfs_file(file=source) as pfs_file):
         for pipeline_name in pfs_file:
-            data_pattern_path = f'{pipeline_name}@master:/????/??/??'
-            date_paths = client.pfs.glob_file(data_pattern_path)
-            for processed_date in date_paths:
-                group_path_pattern = f'{pipeline_name}@master:{processed_date}/*'
-                group_paths = client.pfs.glob_file(group_path_pattern)
-                for group in group_paths:
-                        files_by_pipeline[pipeline_name][processed_date] += 1
+            pipeline_name = pipeline_name.decode('UTF-8')
+            pipeline_name = pipeline_name.strip()
+            pipeline_info = client.pps.inspect_pipeline(pipeline=pps.Pipeline(name=pipeline_name), details=True)
+            project_name = pipeline_info.pipeline.project.name
+            pipeline_commit_name = f'{project_name}/{pipeline_name}@master'
+            for file in client.pfs.glob_file(commit=pfs.Commit.from_uri(pipeline_commit_name), pattern='/????/??/??/*'):
+                path = file.file.path
+                path_parts = path.split('/')
+                processed_date = f'{path_parts[0]}/{path_parts[1]}/{path_parts[2]}'
+                group_name = path_parts[3]
+                for file in client.pfs.glob_file(commit=pfs.Commit.from_uri(pipeline_commit_name),
+                                                 pattern=f'{processed_date}/{group_name}/**'):
+                    files_by_pipeline[pipeline_name][processed_date] += 1
     return files_by_pipeline
-
