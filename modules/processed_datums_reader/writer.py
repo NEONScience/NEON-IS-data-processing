1a8e59c305573462ce76e55a622acd0f539673c2
visalachundru <vchundru@battelleecology.org>
2024-05-21 10:00:27 -0600

    Processed datums reader fixes

--- modules/processed_datums_reader/writer.py
+++ modules/processed_datums_reader/writer.py
@@ -17,9 +17,8 @@ def write_to_db(db: Db, files_by_pipeline: defaultdict(lambda: defaultdict(int))
             dag_name = pipeline_name.split('_')[0]
             for processed_date, file_count in processed_date.items():
                 date_object = datetime.strptime(processed_date, '%Y-%m-%d').date()
-                params = dict(dag_name=dag_name, pipeline_name=pipeline_name, processedd_date=date_object,
+                params = dict(dag_name=dag_name, pipeline_name=pipeline_name, processed_date=date_object,
                               processed_group_count=file_count)
-                #print(sql.as_string(db.connection))
                 clear_existing_records(db=db, dag_name=dag_name, pipeline_name=pipeline_name, processed_date=processed_date)
                 cursor.execute(sql, params)
         db.connection.commit()
@@ -34,7 +33,7 @@ def clear_existing_records(db: Db, dag_name: str, pipeline_name: str, processed_
         and 
            pipeline_name = %(pipeline_name)s
         and 
-            processed_date = %(date_object)s
+            processed_date = %(processed_date)s
     '''
     with closing(db.connection.cursor()) as cursor:
        # print(sql.as_string(db.connection))
