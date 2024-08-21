8c42c5a34ba214db8a51bc9ea15f70b7f22f6bd5
choim <choim@battelleecology.org>
2024-08-21 10:43:35 -0600

    update files accordingly after context updated

--- modules/group_loader/build_tag_push_update.sh
+++ modules/group_loader/build_tag_push_update.sh
@@ -3,7 +3,7 @@
 image_name=group_loader
 tag=$(git rev-parse --short HEAD)
 cd ./modules
-docker build -t $image_name:latest -f ./modules/group_loader/Dockerfile .
+docker build -t $image_name:latest -f ./group_loader/Dockerfile .
 docker tag $image_name quay.io/battelleecology/$image_name:$tag
 docker push quay.io/battelleecology/$image_name:$tag
 cd ..
