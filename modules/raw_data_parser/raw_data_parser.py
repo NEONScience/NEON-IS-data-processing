8d82b1b612c814982b567836c33ac57816f65fbf
Liangying Zhang <lzhang@battelleecology.org>
2024-08-27 11:02:25 -0600

    add tests and fix typo

--- modules/raw_data_parser/raw_data_parser.py
+++ modules/raw_data_parser/raw_data_parser.py
@@ -9,7 +9,7 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import re
 
-from raw_data_parser.pass_code import pass_code_dict
+from raw_data_parser.parse_code import parse_code_dict
 
 log = get_logger()
 keep_columns = ['source_id', 'site_id', 'readout_time']
@@ -18,7 +18,7 @@ keep_columns = ['source_id', 'site_id', 'readout_time']
 def parse_raw(source_type: str, parse_field: str, data_path: Path, out_path: Path,
               relative_path_index: int) -> None:
     out_df = pd.DataFrame()
-    out_file = None
+    out_file = ''
 
     for path in data_path.rglob('*'):
         if path.is_file():
@@ -41,7 +41,7 @@ def parse_raw(source_type: str, parse_field: str, data_path: Path, out_path: Pat
 
 
 def sensor_parse(df: pd.DataFrame, source_type: str, parse_field: str) -> pd.DataFrame:
-    parser = pass_code_dict.get(source_type)
+    parser = parse_code_dict.get(source_type)
 
     if source_type.lower() == 'li7200_raw':
         # extract_and_rename(data_string: str, name_mapping: dict):
@@ -76,6 +76,7 @@ def extract_and_rename(data_string: str, name_mapping: dict) -> Dict:
 
 
 def write_to_parquet(out_file: Path, out_df: pd.DataFrame) -> None:
+    log.debug(f'output path: {out_file}.')
     hashable_cols = [x for x in out_df.columns if isinstance(out_df[x].iloc[0], Hashable)]
     dupcols = [x.encode('UTF-8') for x in hashable_cols
                if (out_df[x].duplicated().sum() / (int(out_df[x].size) - 1)) > 0.3]
@@ -86,16 +87,18 @@ def write_to_parquet(out_file: Path, out_df: pd.DataFrame) -> None:
 
 def create_output_path(source_type: str, path: Path) -> Path:
     out_source_type: str = source_type.split('_')[0]
-    new_type = path.stem.replace(source_type, out_source_type)
+    mac = path.parent.parent.name
 
+    path = Path(str(path).replace(mac, mac.upper()))
+
+    new_stem = path.stem.replace(source_type, out_source_type)
     pattern = r"(_\d+_\d+)$"
-    new_type = re.sub(pattern, '', new_type)
+    new_stem = re.sub(pattern, '', new_stem)
 
-    # Combine the new stem with the original suffix (extension)
-    new_filename = new_type + path.suffix
-
-    # Reconstruct the full path with the new filename
+    # Construct the new filename and full path
+    new_filename = new_stem + path.suffix
     new_path = path.with_name(new_filename)
+
     new_path.parent.mkdir(parents=True, exist_ok=True)
 
     return new_path
