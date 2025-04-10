7286dbe489f7bdbcb262181ab7ea9fc8d08aa024
covesturtevant <csturtevant@battelleecology.org>
2025-04-10 10:38:11 -0600

    test logging

--- modules/calval_loader/get_calibration_stream_name.py
+++ modules/calval_loader/get_calibration_stream_name.py
@@ -32,8 +32,9 @@ def get_calibration_stream_name(connection, asset_type: str, stream_number: int)
         cursor.execute(sql, dict(sensor_type_name=asset_type, stream_number=stream_number))
         row = cursor.fetchone()
         if row is None:
+            print(f'Stream name not found for stream ID {stream_number} and asset type {asset_type}.')
             logging.error(f'Stream name not found for stream ID {stream_number} and asset type {asset_type}.')
             return None
         stream_name = row[0]
-       # print(f'stream_name: {stream_name}')
+       print(f'stream_name: {stream_name}')
     return stream_name
