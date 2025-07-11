4aba9963a6c2ce496ec8de6a8ee7d77fdda86629
choim <choim@battelleecology.org>
2025-07-11 08:57:46 -0600

    update test-def.read.avro.deve.R

--- pack/NEONprocIS.base/tests/testthat/test-def.read.avro.deve.R
+++ pack/NEONprocIS.base/tests/testthat/test-def.read.avro.deve.R
@@ -44,7 +44,8 @@ test_that("   Testing def.read.avro.deve.R, definition function. Read AVRO file"
             {
               workingDirPath <- getwd()
               
-              nameLib <- file.path(workingDirPath, "ravro.so")
+             # nameLib <- file.path(workingDirPath, "ravro.so")
+              nameLib='ravro.so'
               col_List = c('source_id', 'site_id', 'readout_time', 'resistance')
               
               nameFile <- file.path(workingDirPath, "testdata/HART_data.avro")
