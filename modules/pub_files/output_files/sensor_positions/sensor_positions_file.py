b3c2f395f50f5a137f90dd11e4e7065c1d8a5329
covesturtevant <csturtevant@battelleecology.org>
2025-07-28 17:18:25 -0600

    auto-detect tchain locations, split out sensor-specific code

--- modules/pub_files/output_files/sensor_positions/sensor_positions_file.py
+++ modules/pub_files/output_files/sensor_positions/sensor_positions_file.py
@@ -7,13 +7,15 @@ import common.date_formatter as date_formatter
 from pub_files.database.geolocation_geometry import Geometry
 from pub_files.input_files.file_metadata import PathElements
 from pub_files.output_files.filename_format import get_filename
-from pub_files.output_files.sensor_positions.sensor_position import get_position, get_property
+from pub_files.output_files.sensor_positions.sensor_position import get_position
 from pub_files.output_files.sensor_positions.sensor_positions_database import SensorPositionsDatabase
+from pub_files.output_files.sensor_positions import sensor_specific_processors
 
 
 def write_file(out_path: Path, location_path: Path, elements: PathElements, timestamp: datetime,
                database: SensorPositionsDatabase) -> Path:
     """Write the sensor positions file to the output path."""
+
     filename = get_filename(elements, timestamp=timestamp, file_type='sensor_positions', extension='csv')
     file_path = Path(out_path, filename)
     with open(file_path, 'w', encoding='UTF8', newline='') as file:
@@ -26,100 +28,114 @@ def write_file(out_path: Path, location_path: Path, elements: PathElements, time
             if path.is_file() and path.name.startswith('CFGLOC'):
                 named_location_name = path.stem
                 location = database.get_named_location(named_location_name)
-                print(location.properties)
                 (row_hor_ver, row_location_id, row_description) = get_named_location_data(database, named_location_name)
                 geolocations = database.get_geolocations(named_location_name)
                 
                 for geolocation in geolocations:
-                    # offset location
-                    offset_name = geolocation.offset_name
-                    offset_geometry: Geometry = database.get_geometry(offset_name)
-                    # set row values with formatting
-                    row_position_start_date: str = format_date(geolocation.start_date)
-                    row_position_end_date: str = format_date(geolocation.end_date)
-                    row_x_offset: float = round(geolocation.x_offset, 2)
-                    row_y_offset: float = round(geolocation.y_offset, 2)
-                    row_z_offset: float = round(geolocation.z_offset, 2)
+                    # Use the specified processing method
+                    if sensor_specific_processors.is_tchain_sensor(location):
+                        rows = sensor_specific_processors.create_tchain_rows(
+                            database, location, geolocation, row_hor_ver, 
+                            row_location_id, row_description,
+                            _create_base_row_data, _add_reference_position_data)
+                    else:
+                        rows = [_create_standard_row(database, geolocation, row_hor_ver, 
+                                                   row_location_id, row_description)]
                     
-                    
-                    # Temp stuff for tchain
-                    thermistor_depths={}
-                    thermistor_depths['501']=get_property(location.properties,'ThermistorDepth501')
-                    thermistor_depths['502']=get_property(location.properties,'ThermistorDepth502')
-                    thermistor_depths['503']=get_property(location.properties,'ThermistorDepth503')
-                    thermistor_depths['504']=get_property(location.properties,'ThermistorDepth504')
-                    thermistor_depths['505']=get_property(location.properties,'ThermistorDepth505')
-                    thermistor_depths['506']=get_property(location.properties,'ThermistorDepth506')
-                    thermistor_depths['507']=get_property(location.properties,'ThermistorDepth507')
-                    thermistor_depths['508']=get_property(location.properties,'ThermistorDepth508')
-                    thermistor_depths['509']=get_property(location.properties,'ThermistorDepth509')
-                    thermistor_depths['510']=get_property(location.properties,'ThermistorDepth510')
-                    thermistor_depths['511']=get_property(location.properties,'ThermistorDepth511')
-
-                    row_pitch: float = round(geolocation.alpha, 2)
-                    row_roll: float = round(geolocation.beta, 2)
-                    row_azimuth: float = round(geolocation.gamma, 2)
-                    row_reference_location_id: str = (database.get_named_location(offset_name)).name
-                    row_reference_location_description: str = geolocation.offset_description
-                    row_reference_location_latitude = None
-                    row_reference_location_longitude = None
-                    row_reference_location_elevation = None
-                    if offset_geometry.latitude is not None:
-                        row_reference_location_latitude: float = round(offset_geometry.latitude, 6)
-                    if offset_geometry.longitude is not None:
-                        row_reference_location_longitude: float = round(offset_geometry.longitude, 6)
-                    if offset_geometry.elevation is not None:
-                        row_reference_location_elevation: float = round(offset_geometry.elevation, 2)
-                    # reference location
-                    for reference_geolocation in database.get_geolocations(offset_name):
-                        reference_position = get_position(reference_geolocation, geolocation.x_offset,
-                                                          geolocation.y_offset)
-                        x_azimuth = reference_position.x_azimuth
-                        y_azimuth = reference_position.y_azimuth
-                        east_offset = reference_position.east_offset
-                        north_offset = reference_position.north_offset
-                        row_x_azimuth: float = round(x_azimuth, 2) if x_azimuth is not None else ''
-                        row_y_azimuth: float = round(y_azimuth, 2) if y_azimuth is not None else ''
-                        row_east_offset: float = round(east_offset, 2) if east_offset is not None else ''
-                        row_north_offset: float = round(north_offset, 2) if north_offset is not None else ''
-                        row_reference_location_start_date = format_date(reference_geolocation.start_date)
-                        row_reference_location_end_date = format_date(reference_geolocation.end_date)
-                        
-                        # Loop around the thermistor depth properties and split out the main
-                        # hor.ver to each depth
-                        for key in thermistor_depths.keys():
-                            if thermistor_depths[key] is not None:
-                                hor_ver_split=row_hor_ver.split('.')
-                                hor_ver_split[1]=key
-                                row_hor_ver_depth=".".join(hor_ver_split)
-                                
-                                # create row
-                                row = [row_hor_ver_depth,
-                                       row_location_id,
-                                       row_description,
-                                       row_position_start_date,
-                                       row_position_end_date,
-                                       row_reference_location_id,
-                                       row_reference_location_description,
-                                       row_reference_location_start_date,
-                                       row_reference_location_end_date,
-                                       row_x_offset,
-                                       row_y_offset,
-                                       round(row_z_offset-thermistor_depths[key], 2),
-                                       row_pitch,
-                                       row_roll,
-                                       row_azimuth,
-                                       row_reference_location_latitude,
-                                       row_reference_location_longitude,
-                                       row_reference_location_elevation,
-                                       row_east_offset,
-                                       row_north_offset,
-                                       row_x_azimuth,
-                                       row_y_azimuth]
-                                if row not in file_rows:  # prevent duplicates
-                                    file_rows.append(row)
+                    # Add rows, preventing duplicates
+                    for row in rows:
+                        if row not in file_rows:
+                            file_rows.append(row)
+        
         writer.writerows(file_rows)
     return file_path
+
+
+def _create_base_row_data(database: SensorPositionsDatabase, geolocation, 
+                         row_hor_ver: str, row_location_id: str, row_description: str) -> Dict:
+    """Create the common row data used by both standard and specific sensors."""
+    offset_name = geolocation.offset_name
+    offset_geometry: Geometry = database.get_geometry(offset_name)
+    
+    # Basic geolocation data
+    base_data = {
+        'row_hor_ver': row_hor_ver,
+        'row_location_id': row_location_id,
+        'row_description': row_description,
+        'row_position_start_date': format_date(geolocation.start_date),
+        'row_position_end_date': format_date(geolocation.end_date),
+        'row_x_offset': round(geolocation.x_offset, 2),
+        'row_y_offset': round(geolocation.y_offset, 2),
+        'row_z_offset': round(geolocation.z_offset, 2),
+        'row_pitch': round(geolocation.alpha, 2),
+        'row_roll': round(geolocation.beta, 2),
+        'row_azimuth': round(geolocation.gamma, 2),
+        'row_reference_location_id': (database.get_named_location(offset_name)).name,
+        'row_reference_location_description': geolocation.offset_description,
+        'row_reference_location_latitude': round(offset_geometry.latitude, 6) if offset_geometry.latitude is not None else None,
+        'row_reference_location_longitude': round(offset_geometry.longitude, 6) if offset_geometry.longitude is not None else None,
+        'row_reference_location_elevation': round(offset_geometry.elevation, 2) if offset_geometry.elevation is not None else None,
+    }
+    
+    return base_data
+
+
+def _add_reference_position_data(database: SensorPositionsDatabase, base_data: Dict, 
+                                geolocation, offset_name: str) -> List[Dict]:
+    """Add reference position data to base row data and return list of complete row data."""
+    complete_rows = []
+    
+    for reference_geolocation in database.get_geolocations(offset_name):
+        reference_position = get_position(reference_geolocation, geolocation.x_offset, geolocation.y_offset)
+        
+        complete_row_data = base_data.copy()
+        complete_row_data.update({
+            'row_x_azimuth': round(reference_position.x_azimuth, 2) if reference_position.x_azimuth is not None else '',
+            'row_y_azimuth': round(reference_position.y_azimuth, 2) if reference_position.y_azimuth is not None else '',
+            'row_east_offset': round(reference_position.east_offset, 2) if reference_position.east_offset is not None else '',
+            'row_north_offset': round(reference_position.north_offset, 2) if reference_position.north_offset is not None else '',
+            'row_reference_location_start_date': format_date(reference_geolocation.start_date),
+            'row_reference_location_end_date': format_date(reference_geolocation.end_date),
+        })
+        complete_rows.append(complete_row_data)
+    
+    return complete_rows
+
+
+def _create_standard_row(database: SensorPositionsDatabase, geolocation, 
+                        row_hor_ver: str, row_location_id: str, row_description: str) -> List:
+    """Create a standard sensor row."""
+    base_data = _create_base_row_data(database, geolocation, row_hor_ver, row_location_id, row_description)
+    complete_rows = _add_reference_position_data(database, base_data, geolocation, geolocation.offset_name)
+    
+    # Convert to list format (assuming we take the first reference location for standard sensors)
+    if complete_rows:
+        row_data = complete_rows[0]
+        return [
+            row_data['row_hor_ver'],
+            row_data['row_location_id'],
+            row_data['row_description'],
+            row_data['row_position_start_date'],
+            row_data['row_position_end_date'],
+            row_data['row_reference_location_id'],
+            row_data['row_reference_location_description'],
+            row_data['row_reference_location_start_date'],
+            row_data['row_reference_location_end_date'],
+            row_data['row_x_offset'],
+            row_data['row_y_offset'],
+            row_data['row_z_offset'],
+            row_data['row_pitch'],
+            row_data['row_roll'],
+            row_data['row_azimuth'],
+            row_data['row_reference_location_latitude'],
+            row_data['row_reference_location_longitude'],
+            row_data['row_reference_location_elevation'],
+            row_data['row_east_offset'],
+            row_data['row_north_offset'],
+            row_data['row_x_azimuth'],
+            row_data['row_y_azimuth']
+        ]
+    return []
 
 
 def get_column_names() -> List[str]:
