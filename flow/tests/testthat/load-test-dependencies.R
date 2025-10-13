d2a35964848c69fb727de13c9d8628db0faae6fd
Robert Markel <rmarkel@battelleecology.org>
2025-10-13 14:29:55 -0600

    Loading all packages for testing.

--- flow/tests/testthat/load-test-dependencies.R
+++ flow/tests/testthat/load-test-dependencies.R
@@ -28,21 +28,50 @@
 
 load_test_dependencies <- function() {
   library(rlang)
-  
   library(devtools)
   library(testthat)
   library(usethis)
   library(covr)
   library(dplyr)
   library(arrow)
-  
   library(stringr)
   
+  # Needed for install_local function.
   library(remotes)
   
-  suppressPackageStartupMessages(
-    if (!require("NEONprocIS.base")) {
+  if (!require("NEONprocIS.base")) {
+    suppressPackageStartupMessages(
       install_local("../../../pack/NEONprocIS.base")
-    }
-  )
+    )
+  }
+
+  if (!require("NEONprocIS.cal")) {
+    suppressPackageStartupMessages(
+      install_local("../../../pack/NEONprocIS.cal")
+    )
+  }
+
+  if (!require("NEONprocIS.pub")) {
+    suppressPackageStartupMessages(
+      install_local("../../../pack/NEONprocIS.pub")
+    )
+  }
+
+  if (!require("NEONprocIS.qaqc")) {
+    suppressPackageStartupMessages(
+      install_local("../../../pack/NEONprocIS.qaqc")
+    )
+  }
+  
+  if (!require("NEONprocIS.stat")) {
+    suppressPackageStartupMessages(
+      install_local("../../../pack/NEONprocIS.stat")
+    )
+  }
+
+  if (!require("NEONprocIS.wq")) {
+    suppressPackageStartupMessages(
+      install_local("../../../pack/NEONprocIS.wq")
+    )
+  }
 }
