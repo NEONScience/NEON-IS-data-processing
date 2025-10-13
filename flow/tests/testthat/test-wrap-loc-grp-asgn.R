8a97365b067b4e6b4ae8dfc4a1b4f98744eec8cc
Robert Markel <rmarkel@battelleecology.org>
2025-10-13 13:42:52 -0600

    Minor formatting.

--- flow/tests/testthat/test-wrap-loc-grp-asgn.R
+++ flow/tests/testthat/test-wrap-loc-grp-asgn.R
@@ -61,21 +61,13 @@
 # Define test context
 context("\n                       Unit test of wrap.loc.grp.asgn.R\n")
 
-
-
-# load_test_dependencies <- function() {
-#   library(remotes)
-#   install_local("/Users/rmarkel/git/NEON-IS-data-processing/pack/NEONprocIS.base")
-# }
-
+source('load-test-dependencies.R')
+load_test_dependencies()
 
 # Unit test of wrap.loc.grp.asgn.R
 test_that("Unit test of wrap.loc.grp.asgn.R", {
   source('../../flow.loc.grp.asgn/wrap.loc.grp.asgn.R')
-  source('load-test-dependencies.R')
-  
-  load_test_dependencies()
-  
+
   library(rlang)
 
   library(devtools)
