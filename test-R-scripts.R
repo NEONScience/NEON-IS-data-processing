bf0dd1499b04f1ce2b4026adf8287719cff80343
Robert Markel <rmarkel@battelleecology.org>
2025-10-14 14:57:00 -0600

    Update to test suite script.

--- test-R-scripts.R
+++ test-R-scripts.R
@@ -1,31 +1,27 @@
-# R script file
-source('neon-package-loader.R')
-load_test_dependencies()
+# Run unit tests.
 
+# Load libraries
 library(rlang)
-library(stringr)
-library(devtools)
 library(testthat)
+library(devtools)
 library(usethis)
 library(covr)
 library(dplyr)
 library(arrow)
 library(here)
 
-# base test
-
 # Get the full path to the project root
 project_root <- here()
 print(project_root)
 
+# base test
+base_dir <- paste0(project_root, "/pack/NEONprocIS.base/tests/testthat")
+devtools::test(pkg=base_dir)
+
 # qaqc test
-project_root <- here()
-test_dir <- paste0(project_root, "/pack/NEONprocIS.qaqc/tests/testthat")
-devtools::test(pkg=test_dir)
+qaqc_dir <- paste0(project_root, "/pack/NEONprocIS.qaqc/tests/testthat")
+devtools::test(pkg=qaqc_dir)
 
 # report
 cov <- covr::package_coverage()
 report(cov)
-
-############
-
