4a3eb66d91c90218ee76dae3419332e8d407e1cc
ncatolico <ncatolico@battelleecology.org>
2025-11-18 12:50:39 -0700

    updates to flow.discharge.os.inputs

---
+++ flow/flow.discharge.parse.os.inputs/flow.discharge.parse.os.inputs.R
@@ -0,0 +1,240 @@
+##############################################################################################
+#' @title Workflow for Continuous Discharge Processing
+
+#' @author
+#' Zachary Nickerson \email{nickerson@battelleecology.org}
+
+#' @description Workflow. Validates, cleans, and formats troll log files into daily parquets.
+#'
+#' The arguments are: 
+#' 
+#' 1. "DirIn=value", The input path to the data from a single source ID, structured as follows: 
+#' #/pfs/BASE_REPO/source-id.The source-id folder may have multiple csv log files. 
+#' The source-id is the unique identifier of the sensor.   
+#' 
+#' 2. "DirInOS=value", The input path to the OS data tables.
+#'        
+#' 3. "DirOut=value", where the value is the output path that will replace the #/pfs/BASE_REPO portion 
+#' of DirIn.
+#' 
+#' 4. "DirErr=value", where the value is the output path to place the path structure of errored datums that will 
+#' replace the #/pfs/BASE_REPO portion of \code{DirIn}. 
+#'
+#' Note: This script implements logging described in \code{\link[NEONprocIS.base]{def.log.init}},
+#' which uses system environment variables if available.
+#' 
+#' @return Cleaned troll log files in daily parquets.
+
+#' @references
+#' License: (example) GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+
+#' @keywords Currently none
+
+#' @examples
+#' Stepping through the code in Rstudio
+# Sys.setenv(DIR_IN_OS='~/pfs/l4discharge_os_table_group',DIR_IN='~/pfs/l4discharge_csd_swe_group/2024/03/20/l4discharge_HOPB132100')
+# log <- NEONprocIS.base::def.log.init(Lvl = "debug")
+# arg <- c("DirIn=$DIR_IN",
+#          "DirInOS=$DIR_IN_OS",
+#          "DirOut=~/pfs/out",
+#          "DirErr=~/pfs/out/errored_datums")
+# rm(list=setdiff(ls(),c('arg','log')))
+
+#' @seealso None currently
+
+# changelog and author contributions / copyrights
+#   Zachary Nickerson (2025-10-15) 
+#     original creation
+#   Nora Catolico(2025-11-18)
+#     reorganized input directories and added error logging
+##############################################################################################
+options(digits.secs = 3)
+library(lubridate)
+library(foreach)
+library(doParallel)
+library(stringr)
+
+
+# Source the wrapper function. Assume it is in the working directory
+source("./wrap.discharge.os.inputs.R")
+source("./def.dir.in.partial.R")
+
+# Pull in command line arguments (parameters)
+arg <- base::commandArgs(trailingOnly = TRUE)
+
+# Start logging
+log <- NEONprocIS.base::def.log.init()
+
+# Use environment variable to specify how many cores to run on
+numCoreUse <- base::as.numeric(Sys.getenv('PARALLELIZATION_INTERNAL'))
+numCoreAvail <- parallel::detectCores()
+if (base::is.na(numCoreUse)){
+  numCoreUse <- 1
+} 
+if(numCoreUse > numCoreAvail){
+  numCoreUse <- numCoreAvail
+}
+log$debug(paste0(numCoreUse, ' of ',numCoreAvail, ' available cores will be used for internal parallelization.'))
+
+# Parse the input arguments into parameters
+Para <- NEONprocIS.base::def.arg.pars(arg = arg,
+                                      NameParaReqd = c("DirIn",
+                                                       "DirInOS",
+                                                       "DirOut",
+                                                       "DirErr"),
+                                      log = log)
+
+# Echo arguments
+log$debug(base::paste0('SWE/CSD Input directory: ', Para$DirIn))
+log$debug(base::paste0('OS Table Input directory: ', Para$DirInOS))
+log$debug(base::paste0('Output directory: ', Para$DirOut))
+log$debug(base::paste0('Error directory: ', Para$DirErr))
+
+#what are the expected subdirectories of each input path
+DirIn <-
+  def.dir.in.partial(DirBgn = DirIn,
+                     nameDirSubPartial = 'l4discharge',
+                     log = log)
+
+# Take stock of our OS tables. 
+OStables <- base::list.files(DirInOS,full.names=FALSE)
+
+# --------- Load the OS data ----------
+# Load in data file in parquet format into data frame 'data'. Grab the first file only, since there should only be one.
+csd_constantBiasShift_pub  <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP1.00133.001.csd_constantBiasShift_pub.csv')))
+if (base::any(base::class(csd_constantBiasShift_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.csd_constantBiasShift_pub.csv is unreadable.'))
+  base::stop()
+}
+
+csd_dataGapToFillMethodMapping_pub  <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP1.00133.001.csd_dataGapToFillMethodMapping_pub.csv')))
+if (base::any(base::class(csd_dataGapToFillMethodMapping_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.csd_dataGapToFillMethodMapping_pub.csv is unreadable.'))
+  base::stop()
+}
+
+csd_gapFillingRegression_pub  <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP1.00133.001.csd_gapFillingRegression_pub.csv')))
+if (base::any(base::class(csd_gapFillingRegression_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.csd_gapFillingRegression_pub.csv is unreadable.'))
+  base::stop()
+}
+
+csd_gaugeWaterColumnRegression_pub  <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP1.00133.001.csd_gaugeWaterColumnRegression_pub.csv')))
+if (base::any(base::class(csd_gaugeWaterColumnRegression_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.csd_gaugeWaterColumnRegression_pub.csv is unreadable.'))
+  base::stop()
+}
+
+sdrc_controlInfo_pub  <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_controlInfo_pub.csv')))
+if (base::any(base::class(sdrc_controlInfo_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_controlInfo_pub.csv is unreadable.'))
+  base::stop()
+}
+
+sdrc_curveIdentification_pub  <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_curveIdentification_pub.csv')))
+if (base::any(base::class(sdrc_curveIdentification_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_curveIdentification_pub.csv is unreadable.'))
+  base::stop()
+}
+
+sdrc_priorParameters_pub  <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_priorParameters_pub.csv')))
+if (base::any(base::class(sdrc_priorParameters_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_priorParameters_pub.csv is unreadable.'))
+  base::stop()
+}
+
+sdrc_gaugeDischargeMeas_pub  <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP4.00133.001.sdrc_gaugeDischargeMeas_pub.csv')))
+if (base::any(base::class(sdrc_gaugeDischargeMeas_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_gaugeDischargeMeas_pub.csv is unreadable.'))
+  base::stop()
+}
+
+sdrc_sampledParameters_pub <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP4.00133.001.sdrc_sampledParameters_pub.csv')))
+if (base::any(base::class(sdrc_sampledParameters_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_sampledParameters_pub.csv is unreadable.'))
+  base::stop()
+}
+
+sdrc_gaugePressureRelationship_pub <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP4.00133.001.sdrc_gaugePressureRelationship_pub.csv')))
+if (base::any(base::class(sdrc_gaugePressureRelationship_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_gaugePressureRelationship_pub.csv is unreadable.'))
+  base::stop()
+}
+
+sdrc_stageDischargeCurveInfo_pub <-
+  base::try(read.csv(paste0(DirInOS, '/NEON.DOM.SITE.DP4.00133.001.sdrc_stageDischargeCurveInfo_pub.csv')))
+if (base::any(base::class(sdrc_stageDischargeCurveInfo_pub) == 'try-error')) {
+  # Generate error and stop execution
+  log$error(base::paste0('File ', DirInOS, '/NEON.DOM.SITE.DP1.00133.001.sdrc_stageDischargeCurveInfo_pub.csv is unreadable.'))
+  base::stop()
+}
+
+
+# Process each datum path
+doParallel::registerDoParallel(numCoreUse)
+foreach::foreach(site = sites) %dopar% {
+  site=sites[1]
+  log$info(base::paste0('Processing path to file: ', idxDirIn))
+  
+  # Run the wrapper function for each datum, with error routing
+  tryCatch(
+    withCallingHandlers(
+      wrap.discharge.os.inputs(
+        DirIn=DirIn,
+        csd_constantBiasShift_pub=csd_constantBiasShift_pub,
+        csd_dataGapToFillMethodMapping_pub=csd_dataGapToFillMethodMapping_pub,
+        csd_gapFillingRegression_pub=csd_gapFillingRegression_pub,
+        csd_gaugeWaterColumnRegression_pub=csd_gaugeWaterColumnRegression_pub,
+        sdrc_controlInfo_pub=sdrc_controlInfo_pub,
+        sdrc_curveIdentification_pub=sdrc_curveIdentification_pub,
+        sdrc_priorParameters_pub=sdrc_priorParameters_pub,
+        sdrc_gaugeDischargeMeas_pub=sdrc_gaugeDischargeMeas_pub,
+        sdrc_sampledParameters_pub=sdrc_sampledParameters_pub,
+        sdrc_gaugePressureRelationship_pub=sdrc_gaugePressureRelationship_pub,
+        sdrc_stageDischargeCurveInfo_pub=sdrc_stageDischargeCurveInfo_pub,
+        DirOutBase=Para$DirOut,
+        log=log
+      ),
+      error = function(err) {
+        call.stack <- base::sys.calls() # is like a traceback within "withCallingHandlers"
+        log$error(err$message)
+        InfoDirIn <- NEONprocIS.base::def.dir.splt.pach.time(idxDirIn, 
+                                                             log = log)
+        DirSub <- strsplit(InfoDirIn$dirRepo,".", fixed = TRUE)[[1]][1]
+        NEONprocIS.base::def.dir.crea(DirBgn = Para$DirErr, DirSub = DirSub, 
+                                      log = log)
+        csvname <- DirSub %>%
+          strsplit( "/" ) %>%
+          sapply( tail, 1 )
+        nameFileErr <- base::paste0(Para$DirErr, DirSub, "/",csvname)
+        log$info(base::paste0("Re-routing failed datum path to ", nameFileErr))
+        con <- base::file(nameFileErr, "w")
+        base::close(con)
+      }
+    ),
+    # This simply to avoid returning the error
+    error=function(err) {}
+  )
+}
+
+# End flow.discharge.os.inputs
---
+++ flow/flow.discharge.parse.os.inputs/wrap.discharge.parse.os.inputs.R
@@ -0,0 +1,278 @@
+##############################################################################################
+#' @title Wrapper for L4 Discharge OS input table processing
+
+#' @author
+#' Zachary Nickerson \email{nickerson@battelleecology.org}
+
+#' @description Wrapper function. An OS input table for L4 discharge is read in
+#' and parsed out by date and site for use in the main L4 discharge processing
+#' module
+
+#' @param DirInBase Character value. The input path that contains repositories
+#' for OS tables.
+#' 
+#' @param TableName Character value. Partial name of the OS publication table. 
+#' Must be one of the following: 
+#' 
+#' controlInfo
+#' curveID
+#' gaugeDsc
+#' gaugePress
+#' prior
+#' sampled
+#' stageDscCurve
+#' 
+#' @param DirOutBase Character value. The output path that will replace the 
+#' #/pfs/BASE_REPO portion of DirInBase.
+
+#' @return 
+
+#' @references
+#' License: (example) GNU AFFERO GENERAL PUBLIC LICENSE Version 3, 19 November 2007
+
+#' @keywords Currently none
+
+#' @examples
+#' Not run
+# dirList <- list.files(Para$DirInTables,pattern = "table_loader")
+# tableNameMap <- list()
+# for(d in 1:length(dirList)){
+#   fileName <- list.files(paste(Para$DirInTables,dirList[d],sep = "/"))
+#   filePath <- list.files(paste(Para$DirInTables,dirList[d],sep = "/"),
+#                          full.names = T)
+#   currFile <- read.csv(filePath,encoding = "UTF-8",header = T)
+#   tableNameMap[[gsub("\\.csv$","",
+#                      gsub("^.*\\.001\\.","",
+#                           fileName))]] <- currFile
+# }
+# ListTables <- tableNameMap
+# DirIn <- "/home/NEON/nickerson/pfs/testing/2024/02/25/l4discharge_HOPB132100/data"
+# DirOutBase <- "/home/NEON/nickerson/pfs/out"
+# log <- NEONprocIS.base::def.log.init(Lvl = "debug")
+# wrap.discharge.os.inputs(ListTables=ListTables,
+#                          DirIn=DirInBase,
+#                          DirOutBase=DirOutBase,
+#                          log=log)
+
+#' @seealso None currently
+
+# changelog and author contributions / copyrights
+#   Zachary Nickerson (2025-10-31)
+#     original creation
+#   Nora Catolico(2025-11-18)
+#     reorganized input directories and added error logging
+##############################################################################################
+wrap.discharge.os.inputs <- function(DirIn,
+                                     csd_constantBiasShift_pub,
+                                     csd_dataGapToFillMethodMapping_pub,
+                                     csd_gapFillingRegression_pub,
+                                     csd_gaugeWaterColumnRegression_pub,
+                                     sdrc_controlInfo_pub,
+                                     sdrc_curveIdentification_pub,
+                                     sdrc_priorParameters_pub,
+                                     sdrc_gaugeDischargeMeas_pub,
+                                     sdrc_sampledParameters_pub,
+                                     sdrc_gaugePressureRelationship_pub,
+                                     sdrc_stageDischargeCurveInfo_pub,
+                                     DirOutBase,
+                                     log=NULL
+){
+  # Start logging if not already
+  if(base::is.null(log)){
+    log <- NEONprocIS.base::def.log.init()
+  } 
+  
+  # Set constants
+  secInDay <- 60*60*24
+  
+  # What is the date and site for the current directory?
+  startDate <- as.POSIXct(stringr::str_extract(DirIn, "\\d{4}/\\d{2}/\\d{2}"),
+                          tz="UTC")
+  endDate <- startDate+secInDay
+  site <- stringr::str_extract(DirIn, "(?<=l4discharge_)[A-Z]{4}")
+  date <- format(startDate,"%Y/%m/%d") #for file naming
+  
+  # Gather info about the input directory (including date), and create base output directory
+  InfoDirIn <- NEONprocIS.base::def.dir.splt.pach.time(DirIn)
+  DirInData <- fs::path(DirIn,'data')
+  
+  # create output 
+  DirOutData <- base::paste0(DirOutBase,InfoDirIn$dirRepo,'/data')
+  base::dir.create(DirOutData,recursive=TRUE)
+  
+  # Copy with a symbolic link the desired subfolders 
+  NEONprocIS.base::def.dir.copy.symb(DirSrc=fs::path(DirIn),
+                                     DirDest=base::paste0(DirOutBase,"/",date),
+                                     LnkSubObj=TRUE,
+                                     log=log) 
+  
+  # The flags folder is already populated from the calibration module. Copy over any existing files.
+  fileCopy <- base::list.files(DirInData,recursive=TRUE) # Files to copy over
+  # Symbolically link each file
+  for(idxFileCopy in fileCopy){
+    cmdCopy <- base::paste0('ln -s ',base::paste0(DirInData,'/',idxFileCopy),' ',base::paste0(DirOutData,'/',idxFileCopy))
+    rptCopy <- base::system(cmdCopy)
+  }
+  
+  
+  # Which curveID(s) is/are active for this site*date?
+  currCurveData <- sdrc_curveIdentification_pub[
+    sdrc_curveIdentification_pub$site==site
+    &((sdrc_curveIdentification_pub$curveStartDate<=startDate
+       &sdrc_curveIdentification_pub$curveEndDate>=endDate)
+      |(sdrc_curveIdentification_pub$curveStartDate<=startDate
+        &sdrc_curveIdentification_pub$curveEndDate>=startDate)
+      |(sdrc_curveIdentification_pub$curveStartDate<=endDate
+        &sdrc_curveIdentification_pub$curveEndDate>=endDate)
+    ),
+  ]
+  
+  # If there are no active curve IDs, do not write out any data
+  # If there are active curve IDs, write out data associated with active curve
+  if(nrow(currCurveData)>0){
+    # Write curveIdentification
+    write_currCurveData<-try(write.csv(currCurveData,
+                                       paste(DirOutData,
+                                             "NEON.DOM.SITE.DP1.00133.001.sdrc_curveIdentification_pub.csv",
+                                             sep = "/"),
+                                       row.names = F))
+    if(any(grepl('try-error',class(write_currCurveData)))){
+      log$error(base::paste0('Writing the currCurveData output data failed: ',attr(write_currCurveData,"condition")))
+      stop()
+    } else {
+      log$info("currCurveData data written out.")
+    }
+    
+    # Write controls data associated with this curve
+    surveyDate <- as.Date(currCurveData$controlSurveyEndDateTime,tz="UTC")
+    write_sdrc_controlInfo<-try(write.csv(
+      sdrc_controlInfo_pub[as.Date(sdrc_controlInfo_pub$endDate,
+                                   tz="UTC",
+                                   format="%Y-%m-%dT%H:%M:%SZ")
+                           %in%surveyDate
+                           &sdrc_controlInfo_pub$site==site,],
+      paste(DirOutData,
+            "NEON.DOM.SITE.DP1.00133.001.sdrc_controlInfo_pub.csv",
+            sep = "/"),
+      row.names = F))
+    if(any(grepl('try-error',class(write_sdrc_controlInfo)))){
+      log$error(base::paste0('Writing the sdrc_controlInfo output data failed: ',attr(write_sdrc_controlInfo,"condition")))
+      stop()
+    } else {
+      log$info("sdrc_controlInfo data written out.")
+    }
+    
+    write_sdrc_priorParameters<-try(write.csv(
+      sdrc_priorParameters_pub[as.Date(sdrc_priorParameters_pub$endDate,
+                                       tz="UTC",
+                                       format="%Y-%m-%dT%H:%M:%SZ")
+                               %in%surveyDate
+                               &sdrc_priorParameters_pub$site==site,],
+      paste(DirOutData,
+            "NEON.DOM.SITE.DP1.00133.001.sdrc_priorParameters_pub.csv",
+            sep = "/"),
+      row.names = F))
+    if(any(grepl('try-error',class(write_sdrc_priorParameters)))){
+      log$error(base::paste0('Writing the sdrc_priorParameters output data failed: ',attr(write_sdrc_priorParameters,"condition")))
+      stop()
+    } else {
+      log$info("sdrc_priorParameters data written out.")
+    }
+    
+    # Write the rating curve data associated with this curveID
+    curveID <- currCurveData$curveID
+    write_sdrc_stageDischargeCurveInfo<-try(write.csv(
+      sdrc_stageDischargeCurveInfo_pub[sdrc_stageDischargeCurveInfo_pub$curveID
+                                       %in%curveID,],
+      paste(DirOutData,
+            "NEON.DOM.SITE.DP4.00133.001.sdrc_stageDischargeCurveInfo_pub.csv",
+            sep = "/"),
+      row.names = F))
+    if(any(grepl('try-error',class(write_sdrc_stageDischargeCurveInfo)))){
+      log$error(base::paste0('Writing the sdrc_stageDischargeCurveInfo output data failed: ',attr(write_sdrc_stageDischargeCurveInfo,"condition")))
+      stop()
+    } else {
+      log$info("sdrc_stageDischargeCurveInfo data written out.")
+    }
+    
+    write_sdrc_gaugeDischargeMeas<-try(write.csv(
+      sdrc_gaugeDischargeMeas_pub[sdrc_gaugeDischargeMeas_pub$curveID
+                                  %in%curveID,],
+      paste(DirOutData,
+            "NEON.DOM.SITE.DP4.00133.001.sdrc_gaugeDischargeMeas_pub.csv",
+            sep = "/"),
+      row.names = F))
+    if(any(grepl('try-error',class(write_sdrc_gaugeDischargeMeas)))){
+      log$error(base::paste0('Writing the sdrc_gaugeDischargeMeas output data failed: ',attr(write_sdrc_gaugeDischargeMeas,"condition")))
+      stop()
+    } else {
+      log$info("sdrc_gaugeDischargeMeas data written out.")
+    }
+    
+    write_sdrc_sampledParameters<-try(write.csv(
+      sdrc_sampledParameters_pub[sdrc_sampledParameters_pub$curveID
+                                 %in%curveID,],
+      paste(DirOutData,
+            "NEON.DOM.SITE.DP4.00133.001.sdrc_sampledParameters_pub.csv",
+            sep = "/"),
+      row.names = F))
+    if(any(grepl('try-error',class(write_sdrc_sampledParameters)))){
+      log$error(base::paste0('Writing the sdrc_sampledParameters output data failed: ',attr(write_sdrc_sampledParameters,"condition")))
+      stop()
+    } else {
+      log$info("sdrc_sampledParameters data written out.")
+    }
+  }else{
+    log$info(paste0("No currCurveData for ",site," on ",date))
+  }
+  
+  # Which regressionID(s) is/are active for this site*date?
+  currRegData <- csd_gaugeWaterColumnRegression_pub[
+    csd_gaugeWaterColumnRegression_pub$site==site
+    &((csd_gaugeWaterColumnRegression_pub$regressionStartDate<=startDate
+       &csd_gaugeWaterColumnRegression_pub$regressionEndDate>=endDate)
+      |(csd_gaugeWaterColumnRegression_pub$regressionStartDate<=startDate
+        &csd_gaugeWaterColumnRegression_pub$regressionEndDate>=startDate)
+      |(csd_gaugeWaterColumnRegression_pub$regressionStartDate<=endDate
+        &csd_gaugeWaterColumnRegression_pub$regressionEndDate>=endDate)
+    ),
+  ]
+  
+  # If there are no active regressionIDs, do not write out any data
+  # If there are active regressionIDs, write out data associated with active curve
+  if(nrow(currRegData)>0){
+    # Write gaugeWaterColumnRegression
+    write_currRegData<-try(write.csv(currRegData,
+                                     paste(DirOutData,
+                                           "NEON.DOM.SITE.DP1.00133.001.csd_gaugeWaterColumnRegression_pub.csv",
+                                           sep = "/"),
+                                     row.names = F))
+    if(any(grepl('try-error',class(write_currRegData)))){
+      log$error(base::paste0('Writing the currRegData output data failed: ',attr(write_currRegData,"condition")))
+      stop()
+    } else {
+      log$info("currRegData data written out.")
+    }
+    
+    # Write the gauge-pressure relationship data associated with this curveID
+    regressionID <- currRegData$regressionID
+    sdrc_gaugePressureRelationship<-try(write.csv(
+      sdrc_gaugePressureRelationship_pub[
+        sdrc_gaugePressureRelationship_pub$regressionID
+        %in%regressionID,],
+      paste(DirOutData,
+            "NEON.DOM.SITE.DP4.00133.001.sdrc_gaugePressureRelationship_pub.csv",
+            sep = "/"),
+      row.names = F))
+    if(any(grepl('try-error',class(sdrc_gaugePressureRelationship)))){
+      log$error(base::paste0('Writing the gaugePressureRelationship output data failed: ',attr(sdrc_gaugePressureRelationship,"condition")))
+      stop()
+    } else {
+      log$info("gaugePressureRelationship data written out.")
+    }
+  }else{
+    log$info(paste0("No currRegData for ",site," on ",date))
+  }
+  
+  return()
+}# End wrap.discharge.os.inputs
