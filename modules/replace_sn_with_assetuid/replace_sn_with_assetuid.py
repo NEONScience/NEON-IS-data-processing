ae8a7aa20b5f41f1940fbbf376a5752a5a8a6915
ncatolico <ncatolico@battelleecology.org>
2026-04-22 15:55:56 -0600

    sn to asset conversion mod

---
+++ modules/replace_sn_with_assetuid/replace_sn_with_assetuid.py
@@ -0,0 +1,35 @@
+#!usr/bin/env python3
+import os
+from pathlib import Path
+from structlog import get_logger
+
+log = get_logger()
+
+
+def load_assetuid(data_path: Path, map_path: Path, out_path: Path, relative_path_index: int) -> None:
+
+    sn_asset_map = {}
+    for path in map_path.rglob('*'):
+        if path.is_file():
+            sn = path.parent.name
+            with open(path, 'r') as f:
+                asset = f.read().split()[0]
+            sn_asset_map[sn.upper()] = asset
+
+    for path in data_path.rglob('*'):
+        if path.is_file():
+            serial_number = path.parent.parent.name
+            serial_number = serial_number.upper()
+            asset_uid = sn_asset_map.get(serial_number)
+            log.debug(f'serial_number is: {serial_number}; asset_uid is: {asset_uid}')
+
+            new_path = Path(out_path, os.sep.join(path.parts[relative_path_index:]).replace(serial_number, asset_uid))
+
+            Path(new_path).parent.mkdir(parents=True, exist_ok=True)
+            if not new_path.exists():
+                log.debug(f'original path: {path}; new_path: {new_path}')
+                new_path.symlink_to(path)
+
+
+
+
