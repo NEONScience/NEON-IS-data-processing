bd728cb0653d5200c7ce70fed35de1dc11e87355
ncatolico <ncatolico@battelleecology.org>
2026-06-02 16:04:44 -0600

    typo

--- modules/os_table_loader/data/lov_values_loader.py
+++ modules/os_table_loader/data/lov_values_loader.py
@@ -13,7 +13,7 @@ def get_lov_values(lov_name: str) -> list[dict[str, str]]:
     base_url = os.environ.get('LOV_BASE_URL', DEFAULT_LOV_BASE_URL).rstrip('/')
     encoded_lov_name = quote(lov_name, safe='')
     url = f'{base_url}/list-of-values/{encoded_lov_name}'
-    log.debug(f"url_path is {url_path}")
+    log.debug(f"url path is {url}")
 
     try:
         response = requests.get(url, headers={'Accept': 'application/json'}, timeout=15)
