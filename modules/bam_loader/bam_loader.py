607241ddc319dc66135cdf6eb621d57ca8422834
Nora Catolico <ncatolico@battelleecology.org>
2026-08-03 10:41:02 -0600

    bam_loader

--- modules/bam_loader/bam_loader.py
+++ modules/bam_loader/bam_loader.py
@@ -1,9 +1,7 @@
 
-from google.cloud import storage
 from pathlib import Path
+
 import environs
-import os
-import sys
 from google.cloud import storage
 
 
@@ -14,18 +12,26 @@ def bam_loader() -> None:
     output_directory: Path = env.path('OUT_PATH')
     storage_client = storage.Client()
     ingest_bucket = storage_client.bucket(ingest_bucket_name)
-    path_names = storage_client.list_blobs(ingest_bucket)
-    path_names = [path for path in path_names if "v2-dev" in path.name]
-    
+    target_prefix = "v2-dev/BaM_beta"
+    path_names = storage_client.list_blobs(ingest_bucket, prefix=target_prefix)
+
     for blob in path_names:
-        file_name = os.path.splitext(blob.name)[0]
-        print("file_name is:  ", file_name)
-        file_path = Path(output_directory, blob.updated.strftime("%Y"), blob.updated.strftime("%m"), blob.updated.strftime("%d"), file_name+".parquet")        
+        # Ignore prefix placeholders that represent directories.
+        if blob.name.endswith("/"):
+            continue
+
+        print("blob_name is:", blob.name)
+        file_path = Path(
+            output_directory,
+            blob.updated.strftime("%Y"),
+            blob.updated.strftime("%m"),
+            blob.updated.strftime("%d"),
+            blob.name,
+        )
         file_path.parent.mkdir(parents=True, exist_ok=True)
-        print("file_path is:  ", file_path)
-        log_file= open(file_path, "w")
-        log_file.write(blob.updated.strftime("%Y-%m-%d %H:%M:%S"))
-        log_file.close()
+        print("file_path is:", file_path)
+        with open(file_path, "w", encoding="utf-8") as log_file:
+            log_file.write(blob.updated.strftime("%Y-%m-%d %H:%M:%S"))
         
 if __name__ == '__main__':
     bam_loader()
